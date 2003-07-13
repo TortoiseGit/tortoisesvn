9d3c7520441f808cf536a7602db803a39207d8ce
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2003-07-13 15:07:40 +0000

    required changes for 0.14 release
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@119 154be670-7969-47af-9ab0-c6fb378d2295

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
-#define FILEVER_INCVERSION        0,12,2,35
-#define PRODUCTVER_INCVERSION     0,12,2,35
-#define STRFILEVER_INCVERSION     "0, 12, 2, 35\0"
-#define STRPRODUCTVER_INCVERSION  "0, 12, 2, 35\0"
+#define FILEVER_INCVERSION        0,14,0,36
+#define PRODUCTVER_INCVERSION     0,14,0,36
+#define STRFILEVER_INCVERSION     "0, 14, 0, 36\0"
+#define STRPRODUCTVER_INCVERSION  "0, 14, 0, 36\0"
 #define TSVN_VERMAJOR             0
-#define TSVN_VERMINOR             12
-#define TSVN_VERMICRO             2
-#define TSVN_VERBUILD_INCVERSION  35
+#define TSVN_VERMINOR             14
+#define TSVN_VERMICRO             0
+#define TSVN_VERBUILD_INCVERSION  36
