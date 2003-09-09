9be7c36e5de46e11bcacb1e579dca0202ba3e4f9
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2003-09-09 10:53:25 +0000

    make the prompting dialog go to foreground
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@245 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/resource.h
+++ src/TortoiseProc/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by f:\Development\Svn\tortoisesvn\src\Languages\TortoiseProcRUS.rc
+// Used by d:\Development\SubVersion\TortoiseSVN\src\Languages\TortoiseProcRUS.rc
 //
 #define IDR_MANIFEST                    1
 #define IDR_MAINFRAME                   1
