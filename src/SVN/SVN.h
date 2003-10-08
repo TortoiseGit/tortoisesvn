60910bf2a09c3e13e3cee7086ec19dbef3e24093
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2003-10-08 20:31:52 +0000

    remove our own caching provider since subversion has finally implemented their own.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@324 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVN.h
+++ src/SVN/SVN.h
@@ -444,25 +444,7 @@ private:
 					apr_pool_t* pool);
 	void SaveAuthentication(BOOL save);
 
-public:
-	const char *m_username, *m_password;
-	void get_simple_provider (svn_auth_provider_object_t **provider, apr_pool_t *pool);
 
 };
-	svn_error_t * tsvn_simple_save_creds (svn_boolean_t *saved,
-		void *credentials,
-		void *provider_baton,
-		apr_hash_t *parameters,
-		apr_pool_t *pool);
-	svn_error_t *
-		tsvn_simple_first_creds (void **credentials,
-		void **iter_baton,
-		void *provider_baton,
-		apr_hash_t *parameters,
-		const char *realmstring,
-		apr_pool_t *pool);
-
-extern const svn_auth_provider_t tsvn_simple_provider;
-
 
 
