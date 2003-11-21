0fae780389452468d0cc96830488047b0bcf7b3a
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2003-11-21 13:21:05 +0000

    react to NM_CLICK instead of LVN_ITEMCHANGED
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@459 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseShell/SVNPropertyPage.cpp
+++ src/TortoiseShell/SVNPropertyPage.cpp
@@ -138,7 +138,7 @@ BOOL CSVNPropertyPage::PageProc (HWND hwnd, UINT uMessage, WPARAM wParam, LPARAM
 			{
 				LPNMHDR point = (LPNMHDR)lParam;
 				int code = point->code;
-				if ((code == LVN_ITEMCHANGED)||(code == LVN_ITEMACTIVATE))
+				if ((code == NM_CLICK)||(code == LVN_ITEMACTIVATE))
 				{
 					//enable the remove button if a row in the listcontrol is selected
 					//if no row is selected then disable the remove button
