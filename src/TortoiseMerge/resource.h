580046ae47af19b5d1d948428beb5e19be7e8451
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2003-12-18 12:10:54 +0000

    Added Toolbar entry for the Options/Settings dialog.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@534 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseMerge/resource.h
+++ src/TortoiseMerge/resource.h
@@ -86,13 +86,14 @@
 #define ID_MERGE_PREVIOUSDIFFERENCE     32780
 #define ID_VIEW_                        32781
 #define ID_VIEW_OPTIONS                 32782
+#define ID_BUTTON32783                  32783
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        137
-#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_COMMAND_VALUE         32784
 #define _APS_NEXT_CONTROL_VALUE         1025
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
