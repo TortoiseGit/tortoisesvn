988e27d0512f541b46a59e8bfdc96726bfe383bb
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2003-12-20 09:17:10 +0000

    Follow the Subversion API changes for their 0.35 tag.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@544 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNPrompt.cpp
+++ src/SVN/SVNPrompt.cpp
@@ -136,7 +136,7 @@ svn_error_t* SVNPrompt::simpleprompt(svn_auth_cred_simple_t **cred, void *baton,
 	return SVN_NO_ERROR;
 }
 
-svn_error_t* SVNPrompt::sslserverprompt(svn_auth_cred_ssl_server_trust_t **cred_p, void *baton, const char *realm, int failures, const svn_auth_ssl_server_cert_info_t *cert_info, apr_pool_t *pool)
+svn_error_t* SVNPrompt::sslserverprompt(svn_auth_cred_ssl_server_trust_t **cred_p, void *baton, const char *realm, apr_uint32_t failures, const svn_auth_ssl_server_cert_info_t *cert_info, apr_pool_t *pool)
 {
 	SVN * svn = (SVN *)baton;
 	int allow_perm_accept = failures & SVN_AUTH_SSL_UNKNOWNCA;
