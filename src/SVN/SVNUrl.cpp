622575535a47317c831a9a6071d55acc6459b37e
tepting <tepting@154be670-7969-47af-9ab0-c6fb378d2295>
2004-04-24 09:27:46 +0000

    Introduced SVNUrl, a specialized CString class which is aware of Subversion URLs (not yet used anywhere)
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@1048 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ src/SVN/SVNUrl.cpp
@@ -0,0 +1,288 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2004 - Tim Kemp and Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+
+#include "StdAfx.h"
+#include "SVNUrl.h"
+
+#include "SVN.h"
+#include "Utils.h"
+#include "UnicodeUtils.h"
+
+
+
+// SVNUrl
+
+SVNUrl::SVNUrl()
+{
+}
+
+SVNUrl::SVNUrl(const CString& svn_url) :
+	CString(Unescape(svn_url))
+{
+}
+
+SVNUrl::SVNUrl(const SVNUrl& other) :
+	CString(other)
+{
+}
+
+SVNUrl& SVNUrl::operator=(const CString& svn_url)
+{
+	CString::operator=(Unescape(svn_url));
+	return *this;
+}
+
+SVNUrl& SVNUrl::operator=(const SVNUrl& svn_url)
+{
+	CString::operator=(svn_url);
+	return *this;
+}
+
+
+
+// SVNUrl public interface
+
+void SVNUrl::SetPath(const CString& path)
+{
+	CString new_path = Unescape(path);
+
+	int rev_pos = ReverseFind(_T('@'));
+	if (rev_pos >= 0)
+		*this = new_path + Mid(rev_pos);
+	else
+		*this = new_path;
+}
+
+CString SVNUrl::GetPath(bool escaped) const
+{
+	CString path = *this;
+
+	int rev_pos = path.ReverseFind(_T('@'));
+	if (rev_pos >= 0)
+		path = path.Left(rev_pos);
+
+	if (escaped)
+		return Escape(path);
+	else
+		return path;
+}
+
+void SVNUrl::SetRevision(LONG revision)
+{
+	CString svn_url;
+
+	if (revision == SVN::REV_HEAD)
+	{
+		svn_url = GetPath();
+	}
+	else if (revision == SVN::REV_BASE)
+	{
+		svn_url = GetPath() + _T("@BASE");
+	}
+	else if (revision == SVN::REV_WC)
+	{
+		svn_url = GetPath() + _T("@WC");
+	}
+	else
+	{
+		svn_url.Format(_T("@%u"), revision);
+		svn_url= GetPath() + svn_url;
+	}
+
+	*this = svn_url;
+}
+
+LONG SVNUrl::GetRevision() const
+{
+	int rev_pos = ReverseFind(_T('@'));
+
+	if (rev_pos < 0)
+	{
+		return SVN::REV_HEAD;
+	}
+	else
+	{
+		CString revision = Mid(rev_pos + 1);
+		if (revision.CompareNoCase(_T("HEAD")) == 0)
+		{
+			return SVN::REV_HEAD;
+		}
+		else if (revision.CompareNoCase(_T("BASE")) == 0)
+		{
+			return SVN::REV_BASE;
+		}
+		else if (revision.CompareNoCase(_T("WC")) == 0)
+		{
+			return SVN::REV_WC;
+		}
+		return _tcstol(revision, 0, 10);
+	}
+}
+
+CString SVNUrl::GetRevisionText() const
+{
+	int rev_pos = ReverseFind(_T('@'));
+
+	if (rev_pos < 0)
+		return _T("HEAD");
+	else
+		return Mid(rev_pos + 1).MakeUpper();
+}
+
+bool SVNUrl::IsRoot() const
+{
+	return GetParentPath().IsEmpty();
+}
+
+CString SVNUrl::GetRootPath() const
+{
+	SVNUrl url = *this;
+
+	while (!url.IsRoot())
+		url = url.GetParentPath();
+
+	return url;
+}
+
+CString SVNUrl::GetParentPath() const
+{
+	CString path = GetPath();
+
+	int rev_pos = path.ReverseFind('/');
+
+	if (rev_pos <= 0)
+	{
+		path = _T("");
+	}
+	else
+	{
+		path = path.Left(rev_pos);
+		if (path.Left(rev_pos-1).ReverseFind('/') < 0)
+			path = _T("");
+	}
+
+	return path;
+}
+
+CString SVNUrl::GetName() const
+{
+	CString path = GetPath();
+
+	int rev_pos = path.ReverseFind('/');
+
+	if (rev_pos > 0)
+	{
+		path = path.Left(rev_pos - 1);
+		if (path.ReverseFind('/') < 0)
+			path = GetPath();
+	}
+
+	return path;
+}
+
+
+
+// SVNUrl static helpers
+
+CString SVNUrl::Escape(const CString& url)
+{
+	return CUtils::PathEscape(url);
+}
+
+CString SVNUrl::Unescape(const CString& url)
+{
+	CString new_url = url;
+	new_url.Replace('\\', '/');
+	new_url.TrimRight('/');
+
+	CStringA temp = CUnicodeUtils::GetUTF8(new_url);
+	CUtils::Unescape(temp.GetBuffer());
+	temp.ReleaseBuffer();
+	return CUnicodeUtils::GetUnicode(temp);
+}
+ 
+
+#ifdef _DEBUG
+
+// This test can easily be integrated in a CPPUNIT framework
+
+#ifndef CPPUNIT_ASSERT
+#define CPPUNIT_ASSERT(x) ASSERT(x)
+#endif
+
+static class CSVNUrlTest {
+public:
+	CSVNUrlTest()
+	{
+		// Constructor tests
+		{
+			CPPUNIT_ASSERT( SVNUrl().IsEmpty() );
+			CPPUNIT_ASSERT( SVNUrl() == _T(""));
+			CPPUNIT_ASSERT( SVNUrl(_T("")).IsEmpty() );
+			CPPUNIT_ASSERT( SVNUrl(_T("")) == _T(""));
+			CPPUNIT_ASSERT( SVNUrl(_T("x")) == _T("x"));
+			CPPUNIT_ASSERT( SVNUrl(_T("/x")) == _T("/x"));
+			CPPUNIT_ASSERT( SVNUrl(_T("/x/")) == _T("/x"));
+			CPPUNIT_ASSERT( SVNUrl(_T("\\x")) == _T("/x"));
+			CPPUNIT_ASSERT( SVNUrl(_T("\\x\\")) == _T("/x"));
+//			CPPUNIT_ASSERT( SVNUrl(_T("M%FCller")) == _T("Müller"));
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b/c")) == _T("http://a/b/c"));
+			CPPUNIT_ASSERT( SVNUrl(_T("http:\\\\a\\b\\c")) == _T("http://a/b/c"));
+		}
+
+		// GetPath/SetPath tests
+		{
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b")).GetPath() == _T("http://a/b") );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b/")).GetPath() == _T("http://a/b") );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b@22")).GetPath() == _T("http://a/b") );
+//			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b/@22")).GetPath() == _T("http://a/b") );
+		}
+
+		// GetRevision/SetRevision tests
+		{
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b")).GetRevision() == SVN::REV_HEAD );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b/")).GetRevision() == SVN::REV_HEAD );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b@42")).GetRevision() == 42 );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b/@42")).GetRevision() == 42 );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b@HEAD")).GetRevision() == SVN::REV_HEAD );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b@BASE")).GetRevision() == SVN::REV_BASE );
+
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b")).GetRevisionText() == _T("HEAD") );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b@42")).GetRevisionText() == _T("42") );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b@HEAD")).GetRevisionText() == _T("HEAD") );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b@bAsE")).GetRevisionText() == _T("BASE") );
+		}
+
+		// URL root tests
+		{
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a")).IsRoot() == true );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a")).GetRootPath() == _T("http://a") );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/")).IsRoot() == true );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/")).GetRootPath() == _T("http://a") );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b")).IsRoot() == false );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a/b")).GetRootPath() == _T("http://a") );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a.b.com")).IsRoot() == true );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a.b.com")).GetRootPath() == _T("http://a.b.com") );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a.b.com/d/e")).IsRoot() == false );
+			CPPUNIT_ASSERT( SVNUrl(_T("http://a.b.com/d/e")).GetRootPath() == _T("http://a.b.com") );
+		}
+	}
+} SVNUrlTest;
+
+#endif
---
+++ src/SVN/SVNUrl.h
@@ -0,0 +1,124 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2004 - Tim Kemp and Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef __SVNUrl_h
+#define __SVNUrl_h
+
+#pragma once
+
+
+/**
+ * \ingroup TortoiseProc
+ * SVNUrl is a specialized CString class which is aware of Subversion URLs.
+ * In addition to the normal conventions for URLs, SVNUrls may include a
+ * revision information in the form "PATH@REV".
+ *
+ * This class accepts both the unescaped or escaped form of a SVNUrl.
+ * However, it always stores the unescaped form. To get the escaped form
+ * of the path part of the SVNUrl, use GetPath() with the \a escaped
+ * parameter set to TRUE.
+ *
+ * \par requirements
+ * win95 or later
+ * winNT4 or later
+ * MFC
+ *
+ * \version 1.0
+ * first version
+ *
+ * \date MAR-2004
+ *
+ * \author Thomas Epting
+ *
+ * \par license
+ * This code is absolutely free to use and modify. The code is provided "as is" with
+ * no expressed or implied warranty. The author accepts no liability if it causes
+ * any damage to your computer, causes your pet to fall ill, increases baldness
+ * or makes your car start emitting strange noises when you start it up.
+ * This code has no bugs, just undocumented features!
+ */
+class SVNUrl : public CString
+{
+public:
+	SVNUrl();
+	SVNUrl(const CString& svn_url);
+	SVNUrl(const SVNUrl& other);
+
+	SVNUrl& operator=(const CString& svn_url);
+	SVNUrl& operator=(const SVNUrl& svn_url);
+
+public:
+	/**
+	 *  Sets the \a path part of the SVNUrl.
+	 */
+	void SetPath(const CString& path);
+	/**
+	 * Returns the path part of the SVNUrl.
+	 */
+	CString GetPath(bool escaped = false) const;
+
+	/**
+	 * Sets the \a revision part of the pathname. If \a escaped is set to TRUE,
+	 * the escaped form of the URL is returned. Otherwise, the unescaped form
+	 * of the URL is returned.
+	 */
+	void SetRevision(LONG revision);
+	/**
+	 * Returns the revision part of the pathname.
+	 */
+	LONG GetRevision() const;
+	/**
+	 * Returns the textual representation of the revision part of the SVNUrl.
+	 */
+	CString GetRevisionText() const;
+
+	/**
+	 * Returns true if the SVNUrl is a root URL, e.g. http://svn.collab.net
+	 * Note: A SVN repository's root can \b not be determined with this function!
+	 */
+	bool IsRoot() const;
+	/**
+	 * Returns the root of the current SVNUrl's path,
+	 */
+	CString GetRootPath() const;
+	/**
+	 * Returns the parent of the current SVNUrl's path. If the SVNUrl is a
+	 * root URL, this function returns an empty string.
+	 */
+	CString GetParentPath() const;
+	/**
+	 * Returns the name part of the current SVNUrl's path. If the SVNUrl is a
+	 * root URL, this function returns the full path.
+	 */
+	CString GetName() const;
+
+public:
+	/**
+	 * Returns the escaped form of \a url (this may also be a SVNUrl).
+	 */
+	static CString Escape(const CString& url);
+	/**
+	 * Returns the unescaped form of \a url (this may also be a SVNUrl).
+	 * Backslashes are converted to forward slashes, and trailing slashes
+	 * are removed.
+	 */
+	static CString Unescape(const CString& url);
+
+};
+
+#endif /*__SVNUrl_h*/
