3bca0cb4a46528e883a326d3691f54742b84532c
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2004-06-17 07:40:48 +0000

    improve the build script
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@1237 154be670-7969-47af-9ab0-c6fb378d2295

--- build.bat
+++ build.bat
@@ -28,7 +28,11 @@ rem Subversion
 echo ================================================================================
 echo building Subversion
 cd ..\..\Subversion
+copy build\generator\vcnet_sln.ezt build\generator\vcnet_sln7.ezt
+copy ..\TortoiseSVN\vcnet_sln.ezt build\generator\vcnet_sln.ezt
 call python gen-make.py -t vcproj --with-openssl=..\Common\openssl --with-zlib=..\Common\zlib
+copy build\generator\vcnet_sln7.ezt build\generator\vcnet_sln.ezt /Y
+del build\generator\vcnet_sln7.ezt
 devenv subversion_vcnet.sln /rebuild debug /project "__ALL__" > NUL
 devenv subversion_vcnet.sln /rebuild release /project "__ALL__" > NUL
 @echo off
@@ -42,11 +46,27 @@ devenv TortoiseSVN.sln /rebuild release_mbcs > NUL
 devenv TortoiseSVN.sln /rebuild debug > NUL
 cd Languages
 FOR %%V In (*.po) Do ..\..\bin\release\ResText.exe extract TortoiseMergeLang.dll TortoiseProcLang.dll %%V -quiet
+cd ..\..
+if EXIST bin\debug\iconv rmdir bin\debug\iconv > NUL
+if EXIST bin\release\iconv rmdir bin\release\iconv > NUL
+if EXIST bin\release_mbcs\iconv rmdir bin\release_mbcs\iconv > NUL
+mkdir bin\debug\iconv > NUL
+mkdir bin\release\iconv > NUL
+mkdir bin\release_mbcs\iconv > NUL
+copy ..\Subversion\apr-iconv\LibD\iconv\*.so bin\debug\iconv > NUL
+copy ..\Subversion\apr-iconv\LibR\iconv\*.so bin\release\iconv > NUL
+copy ..\Subversion\apr-iconv\LibR\iconv\*.so bin\release_mbcs\iconv > NUL
+copy ..\Common\openssl\out32dll\*.dll bin\debug /Y > NUL
+copy ..\Common\openssl\out32dll\*.dll bin\release /Y > NUL
+copy ..\Common\openssl\out32dll\*.dll bin\release_mbcs /Y > NUL
+copy ..\Subversion\db4-win32\bin\libdb42d.dll bin\debug /Y > NUL
+copy ..\Subversion\db4-win32\bin\libdb42.dll bin\release /Y > NUL
+copy ..\Subversion\db4-win32\bin\libdb42.dll bin\release_mbcs /Y > NUL
 @echo off
 
 echo ================================================================================
 echo generating docs
-cd ..\..\doc
+cd doc
 call GenDoc.bat > NUL
 @echo off
 
