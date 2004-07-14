f70c7ea50ecd1ed73127e658b3b2ab97af68a35d
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2004-07-14 08:12:36 +0000

    Fix the po template file and the script which created it.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@1375 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Languages/Make_Pot.bat
+++ src/Languages/Make_Pot.bat
@@ -2,9 +2,12 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 if "%TortoiseVars%"=="" call ..\..\TortoiseVars.bat
-..\..\bin\release\ResText extract TortoiseProcLang.dll TortoiseMergeLang.dll Tmp.pot
-msgremove Tmp.pot -i ignore.po -o Tortoise.pot
-del Tmp.pot
+..\..\bin\release\ResText extract TortoiseProcLang.dll TortoiseMergeLang.dll Tortoise.pot
+
+rem leave the next two lines commented. The msgremove tool changes the
+rem sequence "\r\n" to "r\n" - removing the backslash before the r!!!
+rem msgremove Tmp.pot -i ignore.po -o Tortoise.pot
+rem del Tmp.pot
 
 rem leave this line commented. I fear it destroys the asian .po files
 rem FOR %%i in (*.po) do msgmerge --no-wrap -s %%i Tortoise.pot -o %%i
