012225f956146d1add14148c9be6782061bc65e8
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2004-07-20 21:18:26 +0000

    Don't use the language param in ResText - this doesn't seem to work correctly.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@1418 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Languages/Make_Installer.bat
+++ src/Languages/Make_Installer.bat
@@ -16,8 +16,8 @@ goto :eof
 :doit
 echo.
 echo Building %5 dlls and installer
-..\..\bin\release\restext apply TortoiseProcLang.dll ..\..\bin\TortoiseProc%2.dll Tortoise_%1.po %2
-..\..\bin\release\restext apply TortoiseMergeLang.dll ..\..\bin\TortoiseMerge%2.dll Tortoise_%1.po %2
+..\..\bin\release\restext apply TortoiseProcLang.dll ..\..\bin\TortoiseProc%2.dll Tortoise_%1.po 
+..\..\bin\release\restext apply TortoiseMergeLang.dll ..\..\bin\TortoiseMerge%2.dll Tortoise_%1.po 
 
 echo !define PRODUCT_NAME "TortoiseSVN %~6" > %OFile%
 echo !define CountryCode "%1" >> %OFile%
