12762f02e70dc3e5ab58a2673fe7ca06cfcaa8e7
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2004-07-20 09:30:43 +0000

    make the build script configurable.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@1409 154be670-7969-47af-9ab0-c6fb378d2295

--- build.bat
+++ build.bat
@@ -6,6 +6,29 @@ rem build script for TortoiseSVN
 rem
 @if "%VSINSTALLDIR%"=="" call "%VS71COMNTOOLS%\vsvars32.bat"
 
+if "%1"=="" (
+  SET _RELEASE=ON
+  SET _DEBUG=ON
+  SET _RELEASE_MBCS=ON
+  SET _DOCS=ON
+  SET _SETUP=ON
+)
+
+:getparam
+if "%1"=="release" SET _RELEASE=ON
+if "%1"=="RELEASE" SET _RELEASE=ON
+if "%1"=="debug" SET _DEBUG=ON
+if "%1"=="DEBUG" SET _DEBUG=ON
+if "%1"=="release_mbcs" SET _RELEASE_MBCS=ON
+if "%1"=="RELEASE_MBCS" SET _RELEASE_MBCS=ON
+if "%1"=="doc" SET _DOCS=ON
+if "%1"=="DOC" SET _DOCS=ON
+if "%1"=="setup" SET _SETUP=ON
+if "%1"=="SETUP" SET _SETUP=ON
+shift
+if NOT "%1"=="" goto :getparam
+
+
 rem OpenSSL
 echo ================================================================================
 echo building OpenSSL
@@ -33,8 +56,13 @@ copy ..\TortoiseSVN\vcnet_sln.ezt build\generator\vcnet_sln.ezt
 call python gen-make.py -t vcproj --with-openssl=..\Common\openssl --with-zlib=..\Common\zlib
 copy build\generator\vcnet_sln7.ezt build\generator\vcnet_sln.ezt /Y
 del build\generator\vcnet_sln7.ezt
-devenv subversion_vcnet.sln /rebuild debug /project "__ALL__"
-devenv subversion_vcnet.sln /rebuild release /project "__ALL__"
+if %_DEBUG%==ON (
+  devenv subversion_vcnet.sln /rebuild debug /project "__ALL__")
+if %_RELEASE%==ON (
+  devenv subversion_vcnet.sln /rebuild release /project "__ALL__")
+else if %_RELEASE_MBCS%==ON (
+  devenv subversion_vcnet.sln /rebuild release /project "__ALL__")
+
 @echo off
 
 rem TortoiseSVN
@@ -43,41 +71,56 @@ echo building TortoiseSVN
 cd ..\TortoiseSVN\src
 devenv TortoiseSVN.sln /rebuild release /project SubWCRev
 ..\bin\release\SubWCRev.exe .. version.in version.h
-devenv TortoiseSVN.sln /rebuild release
-devenv TortoiseSVN.sln /rebuild release_mbcs
-devenv TortoiseSVN.sln /rebuild debug
+if %_RELEASE%==ON (
+  devenv TortoiseSVN.sln /rebuild release )
+if %_RELEASE_MBCS%==ON (
+  devenv TortoiseSVN.sln /rebuild release_mbcs)
+if %_DEBUG%==ON (
+  devenv TortoiseSVN.sln /rebuild debug)
 cd Languages
 call Make_Pot.bat
 cd ..\..
-if EXIST bin\debug\iconv rmdir /S /Q bin\debug\iconv > NUL
-if EXIST bin\release\iconv rmdir /S /Q bin\release\iconv > NUL
-if EXIST bin\release_mbcs\iconv rmdir /S /Q bin\release_mbcs\iconv > NUL
-mkdir bin\debug\iconv > NUL
-mkdir bin\release\iconv > NUL
-mkdir bin\release_mbcs\iconv > NUL
-copy ..\Subversion\apr-iconv\LibD\iconv\*.so bin\debug\iconv > NUL
-copy ..\Subversion\apr-iconv\LibR\iconv\*.so bin\release\iconv > NUL
-copy ..\Subversion\apr-iconv\LibR\iconv\*.so bin\release_mbcs\iconv > NUL
-copy ..\Common\openssl\out32dll\*.dll bin\debug /Y > NUL
-copy ..\Common\openssl\out32dll\*.dll bin\release /Y > NUL
-copy ..\Common\openssl\out32dll\*.dll bin\release_mbcs /Y > NUL
-copy ..\Subversion\db4-win32\bin\libdb42d.dll bin\debug /Y > NUL
-copy ..\Subversion\db4-win32\bin\libdb42.dll bin\release /Y > NUL
-copy ..\Subversion\db4-win32\bin\libdb42.dll bin\release_mbcs /Y > NUL
+if %_DEBUG%==ON (
+  if EXIST bin\debug\iconv rmdir /S /Q bin\debug\iconv > NUL
+  mkdir bin\debug\iconv > NUL
+  copy ..\Subversion\apr-iconv\LibD\iconv\*.so bin\debug\iconv > NUL
+  copy ..\Common\openssl\out32dll\*.dll bin\debug /Y > NUL
+  copy ..\Subversion\db4-win32\bin\libdb42d.dll bin\debug /Y > NUL
+)
+if %_RELEASE%==ON (
+  if EXIST bin\release\iconv rmdir /S /Q bin\release\iconv > NUL
+  mkdir bin\release\iconv > NUL
+  copy ..\Subversion\apr-iconv\LibR\iconv\*.so bin\release\iconv > NUL
+  copy ..\Common\openssl\out32dll\*.dll bin\release /Y > NUL
+  copy ..\Subversion\db4-win32\bin\libdb42.dll bin\release /Y > NUL
+)
+if %_RELEASE_MBCS%==ON (
+  if EXIST bin\release_mbcs\iconv rmdir /S /Q bin\release_mbcs\iconv > NUL
+  mkdir bin\release_mbcs\iconv > NUL
+  copy ..\Subversion\apr-iconv\LibR\iconv\*.so bin\release_mbcs\iconv > NUL
+  copy ..\Common\openssl\out32dll\*.dll bin\release_mbcs /Y > NUL
+  copy ..\Subversion\db4-win32\bin\libdb42.dll bin\release_mbcs /Y > NUL
+)
 @echo off
 
 echo ================================================================================
-echo generating docs
 cd doc
-call GenDoc.bat > NUL
+if %_DOCS%==ON (
+  echo generating docs
+  call GenDoc.bat > NUL
+)
 @echo off
 
 echo ================================================================================
-echo building installers
 cd ..\src\TortoiseSVNSetup
 @echo off
+if %_SETUP%==ON (
+  echo building installers
 
-call MakeMsi.bat
+  call MakeMsi.bat
+  cd ..\Languages
+  call Make_Installer.bat
+)
 
 cd ..\..
 echo started at: %starttime%
