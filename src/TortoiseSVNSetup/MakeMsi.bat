21bc42e43a4fcf5a58430ec14b9580fb660727d6
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2004-07-23 11:04:59 +0000

    Adjust the installer script to the new folder layout and the fact that apr is now linked dynamically instead of statically.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@1447 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseSVNSetup/MakeMsi.bat
+++ src/TortoiseSVNSetup/MakeMsi.bat
@@ -1,7 +1,7 @@
 set TSVNBUILD=release
 candle -out Setup.wixobj Setup.wxs
-light -out ..\..\bin\TortoiseSVN-1.0.x-UNICODE_svn-1.x.x.msi Setup.wixobj
+light -out ..\..\bin\TortoiseSVN-1.1.x-UNICODE_svn-1.1.x.msi Setup.wixobj
 set TSVNBUILD=release_mbcs
 candle -out Setup.wixobj Setup.wxs
-light -out ..\..\bin\TortoiseSVN-1.0.x-MBCS_svn-1.x.x.msi Setup.wixobj
+light -out ..\..\bin\TortoiseSVN-1.1.x-MBCS_svn-1.1.x.msi Setup.wixobj
 del Setup.wixobj
