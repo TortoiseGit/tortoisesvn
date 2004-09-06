625d9204e13f5556efd93ae29df7b403c9b8786f
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2004-09-06 15:25:31 +0000

    Fix the empty "show log -> show unified diff".
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@1651 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/LogDlg.cpp
+++ src/TortoiseProc/LogDlg.cpp
@@ -635,7 +635,7 @@ void CLogDlg::OnContextMenu(CWnd* pWnd, CPoint point)
 						CString tempfile = CUtils::GetTempFile();
 						tempfile += _T(".diff");
 						m_templist.Add(tempfile);
-						if (!PegDiff(m_path, (m_hasWC ? SVNRev::REV_WC : SVNRev::REV_HEAD), rev, rev, TRUE, FALSE, TRUE, _T(""), tempfile))
+						if (!PegDiff(m_path, (m_hasWC ? SVNRev::REV_WC : SVNRev::REV_HEAD), rev-1, rev, TRUE, FALSE, TRUE, _T(""), tempfile))
 						{
 							CMessageBox::Show(this->m_hWnd, GetLastErrorMessage(), _T("TortoiseSVN"), MB_ICONERROR);
 							break;		//exit
