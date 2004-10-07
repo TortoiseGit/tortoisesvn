035fb762979fadd14e878e25fb88026b832870d4
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2004-10-07 16:57:59 +0000

    Update the wix script to WiX version 2.0.2110.0. Some deprecated elements are still used though...
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@1788 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseSVNSetup/MakeMsi.bat
+++ src/TortoiseSVNSetup/MakeMsi.bat
@@ -1,7 +1,7 @@
 set TSVNBUILD=release
-candle -out Setup.wixobj Setup.wxs
-light -out ..\..\bin\TortoiseSVN-1.1.x-UNICODE_svn-1.1.x.msi Setup.wixobj
+candle -nologo -out Setup.wixobj Setup.wxs
+light -nologo -out ..\..\bin\TortoiseSVN-1.1.x-UNICODE_svn-1.1.x.msi Setup.wixobj
 set TSVNBUILD=release_mbcs
-candle -out Setup.wixobj Setup.wxs
-light -out ..\..\bin\TortoiseSVN-1.1.x-MBCS_svn-1.1.x.msi Setup.wixobj
+candle -nologo -out Setup.wixobj Setup.wxs
+light -nologo -out ..\..\bin\TortoiseSVN-1.1.x-MBCS_svn-1.1.x.msi Setup.wixobj
 del Setup.wixobj
