91a004abd172a29d0dbf321978b24ac56d796d4c
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2004-10-21 16:38:21 +0000

    Auto-generate the GUID's for the product and package id and generate the last version part with SubWCRev. Thanks to Erv Walter for giving the hints.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@1842 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseSVNSetup/MakeMsi.bat
+++ src/TortoiseSVNSetup/MakeMsi.bat
@@ -1,7 +1,12 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+..\..\bin\release\bin\SubWCRev.exe ..\.. Setup.wxs Setup_good.wxs
+if NOT EXIST Setup_good.wxs (copy Setup.wxs Setup_good.wxs)
 set TSVNBUILD=release
-candle -nologo -out Setup.wixobj Setup.wxs
+candle -nologo -out Setup.wixobj Setup_good.wxs
 light -nologo -out ..\..\bin\TortoiseSVN-1.1.x-UNICODE_svn-1.1.x.msi Setup.wixobj
 set TSVNBUILD=release_mbcs
-candle -nologo -out Setup.wixobj Setup.wxs
+candle -nologo -out Setup.wixobj Setup_good.wxs
 light -nologo -out ..\..\bin\TortoiseSVN-1.1.x-MBCS_svn-1.1.x.msi Setup.wixobj
 del Setup.wixobj
+del Setup_good.wxs
