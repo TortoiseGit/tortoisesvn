8c3ed41af0ce01aebafc5f6db56100e7632eed7e
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2004-12-10 20:03:17 +0000

    Fix broken "resolve on save" in TortoiseMerge.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@2095 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseMerge/MainFrm.cpp
+++ src/TortoiseMerge/MainFrm.cpp
@@ -746,7 +746,7 @@ void CMainFrame::OnFileSave()
 		MoveFile(m_Data.m_sMergedFile, m_Data.m_sMergedFile + _T(".bak"));
 	}
 	SaveFile(this->m_Data.m_sMergedFile);
-	if ((nConflictLine>=0)&&(((DWORD)CRegDWORD(_T("Software\\TortoiseMerge\\Resolve"))) != 0))
+	if ((m_pwndBottomView)&&(m_pwndBottomView->IsWindowVisible())&&(((DWORD)CRegDWORD(_T("Software\\TortoiseMerge\\Resolve"))) != 0))
 	{
 		TCHAR buf[MAX_PATH*3];
 		GetModuleFileName(NULL, buf, MAX_PATH);
