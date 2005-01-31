09a17ffc0dff7129681b48eb6135ecc54ec13098
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-01-31 15:55:30 +0000

    When showing the explorer context menu, check the status again if the cache returns 'unversioned' for a folder. Reason: in a nested layout, folders which don't belong to the original working copy have a status of unversioned but are still versioned.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@2592 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseShell/ContextMenu.cpp
+++ src/TortoiseShell/ContextMenu.cpp
@@ -88,12 +88,18 @@ STDMETHODIMP CShellExt::Initialize(LPCITEMIDLIST pIDFolder,
 							svn_wc_status_kind status = svn_wc_status_unversioned;
 							try
 							{
-								AutoLocker lock(g_csCacheGuard);
-								const FileStatusCacheEntry * s = g_CachedStatus.GetFullStatus(str.c_str(), PathIsDirectory(str.c_str()));
+								bool bIsDirectory = !!PathIsDirectory(str.c_str());
+								const FileStatusCacheEntry * s = g_CachedStatus.GetFullStatus(str.c_str(), bIsDirectory);
 								if (s)
 								{
 									status = s->status;
 								}
+								// if the status is unversioned and the item is a folder,
+								// it still could be versioned! That's because svn_client_status()
+								// returns unversioned for nested layouts if the folder
+								// is from a different repository/wc!
+								if ((status == svn_wc_status_unversioned)&&(bIsDirectory))
+									status = SVNStatus::GetAllStatus(str.c_str());
 							}
 							catch ( ... )
 							{
@@ -147,11 +153,18 @@ STDMETHODIMP CShellExt::Initialize(LPCITEMIDLIST pIDFolder,
 							try
 							{
 								AutoLocker lock(g_csCacheGuard);
-								const FileStatusCacheEntry * s = g_CachedStatus.GetFullStatus(str.c_str(), PathIsDirectory(str.c_str()));
+								bool bIsDirectory = !!PathIsDirectory(str.c_str());
+								const FileStatusCacheEntry * s = g_CachedStatus.GetFullStatus(str.c_str(), bIsDirectory);
 								if (s)
 								{
 									status = s->status;
 								}
+								// if the status is unversioned and the item is a folder,
+								// it still could be versioned! That's because svn_client_status()
+								// returns unversioned for nested layouts if the folder
+								// is from a different repository/wc!
+								if ((status == svn_wc_status_unversioned)&&(bIsDirectory))
+									status = SVNStatus::GetAllStatus(str.c_str());
 								statfetched = TRUE;
 							}
 							catch ( ... )
