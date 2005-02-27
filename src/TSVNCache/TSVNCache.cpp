97118a73fa27490c0886b9737f8cdeca816577a7
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-02-27 10:46:39 +0000

    Make the external cache a real windows application: - implement a window message handler - react to closing messages like WM_ENDSESSION, WM_QUIT, WM_CLOSE, WM_DESTROY and close the cache properly. - create a new thread which handles incoming pipe requests. The main thread is now used to handle the window messages.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@2743 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TSVNCache/TSVNCache.cpp
+++ src/TSVNCache/TSVNCache.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// External Cache Copyright (C) 2005 - Will Dean
+// External Cache Copyright (C) 2005 - Will Dean, Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -29,9 +29,11 @@
 CCrashReport crasher("crashreports@tortoisesvn.tigris.org", "Crash Report for TSVNCache : " STRPRODUCTVER);// crash
 
 
-VOID InstanceThread(LPVOID); 
-
-int __stdcall WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int /*cmdShow*/)
+VOID				InstanceThread(LPVOID); 
+VOID				PipeThread(LPVOID);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+bool bRun = true;
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int /*cmdShow*/)
 {
 	HANDLE hReloadProtection = ::CreateSemaphore(NULL, 0, 1, _T("TSVNCacheReloadProtection"));
 	if(hReloadProtection != INVALID_HANDLE_VALUE && GetLastError() == ERROR_ALREADY_EXISTS)
@@ -44,16 +46,121 @@ int __stdcall WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPST
 	apr_initialize();
 	CSVNStatusCache::Create();
 
-	BOOL fConnected; 
 	DWORD dwThreadId; 
-	HANDLE hPipe, hThread; 
+	HANDLE hPipeThread; 
+	MSG msg;
+	TCHAR szWindowClass[] = {_T("TSVNCacheWindow")};
 
+	// create a hidden window to receive window messages.
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX); 
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= (WNDPROC)WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= 0;
+	wcex.hCursor		= 0;
+	wcex.hbrBackground	= 0;
+	wcex.lpszMenuName	= NULL;
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= 0;
+	RegisterClassEx(&wcex);
+	HWND hWnd = CreateWindow(_T("TSVNCacheWindow"), _T("TSVNCacheWindow"), WS_CAPTION, 0, 0, 0, 0, NULL, 0, hInstance, 0);
+	msg.hwnd = hWnd;	
+	
+	// Create a thread which waits for incoming pipe connections 
+	hPipeThread = CreateThread( 
+		NULL,              // no security attribute 
+		0,                 // default stack size 
+		(LPTHREAD_START_ROUTINE) PipeThread, 
+		(LPVOID) &bRun,    // thread parameter 
+		0,                 // not suspended 
+		&dwThreadId);      // returns thread ID 
+
+	if (hPipeThread == NULL) 
+	{
+		printf("CreateThread failed"); 
+		return 0;
+	}
+	else CloseHandle(hPipeThread); 
+
+	// loop to handle window messages.
+	while (bRun)
+	{
+		GetMessage(&msg, NULL, 0, 0);
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	CSVNStatusCache::Destroy();
+	apr_terminate();
+
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message) 
+	{
+	case WM_ENDSESSION:
+	case WM_DESTROY:
+	case WM_QUIT:
+	case WM_CLOSE:
+		{
+			bRun = false;
+			// now connect to the pipe so that the running thread can exit
+			HANDLE hPipe = CreateFile( 
+				TSVN_CACHE_PIPE_NAME,   // pipe name 
+				GENERIC_READ |  // read and write access 
+				GENERIC_WRITE, 
+				0,              // no sharing 
+				NULL,           // default security attributes
+				OPEN_EXISTING,  // opens existing pipe 
+				0,              // default attributes 
+				NULL);          // no template file 
+
+			CloseHandle(hPipe);
+			Sleep(100);
+			PostQuitMessage(0);
+			ATLTRACE("WM_CLOSE/QUIT/DESTROY/ENDSESSION\n");
+			return 0;
+		}
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+VOID GetAnswerToRequest(const TSVNCacheRequest* pRequest, TSVNCacheResponse* pReply, DWORD* pResponseLength)
+{
+	CTSVNPath path;
+	if(pRequest->flags & TSVNCACHE_FLAGS_FOLDERISKNOWN)
+	{
+		path.SetFromWin(pRequest->path, !!(pRequest->flags & TSVNCACHE_FLAGS_ISFOLDER));
+	}
+	else
+	{
+		path.SetFromWin(pRequest->path);
+	}
+
+	CSVNStatusCache::Instance().GetStatusForPath(path, pRequest->flags).BuildCacheResponse(*pReply, *pResponseLength);
+}
+
+VOID PipeThread(LPVOID lpvParam)
+{
+	bool * bRun = (bool *)lpvParam;
 	// The main loop creates an instance of the named pipe and 
 	// then waits for a client to connect to it. When the client 
 	// connects, a thread is created to handle communications 
 	// with that client, and the loop is repeated. 
+	DWORD dwThreadId; 
+	BOOL fConnected;
+	HANDLE hPipe, hInstanceThread;
 
-	for (;;) 
+	while (*bRun) 
 	{ 
 		hPipe = CreateNamedPipe( 
 			TSVN_CACHE_PIPE_NAME,
@@ -73,58 +180,37 @@ int __stdcall WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPST
 			break;
 		}
 
-		// Wait for the client to connect; if it succeeds, 
-		// the function returns a nonzero value. If the function returns 
-		// zero, GetLastError returns ERROR_PIPE_CONNECTED. 
-		fConnected = ConnectNamedPipe(hPipe, NULL) ? TRUE : (GetLastError() == ERROR_PIPE_CONNECTED); 
-		if (fConnected) 
-		{ 
-			// Create a thread for this client. 
-			hThread = CreateThread( 
-				NULL,              // no security attribute 
-				0,                 // default stack size 
-				(LPTHREAD_START_ROUTINE) InstanceThread, 
-				(LPVOID) hPipe,    // thread parameter 
-				0,                 // not suspended 
-				&dwThreadId);      // returns thread ID 
+		if (WaitNamedPipe(TSVN_CACHE_PIPE_NAME, 100))
+		{
+			// Wait for the client to connect; if it succeeds, 
+			// the function returns a nonzero value. If the function returns 
+			// zero, GetLastError returns ERROR_PIPE_CONNECTED. 
+			fConnected = ConnectNamedPipe(hPipe, NULL) ? TRUE : (GetLastError() == ERROR_PIPE_CONNECTED); 
+			if (fConnected) 
+			{ 
+				// Create a thread for this client. 
+				hInstanceThread = CreateThread( 
+					NULL,              // no security attribute 
+					0,                 // default stack size 
+					(LPTHREAD_START_ROUTINE) InstanceThread, 
+					(LPVOID) hPipe,    // thread parameter 
+					0,                 // not suspended 
+					&dwThreadId);      // returns thread ID 
 
-			if (hThread == NULL) 
-			{
-				printf("CreateThread failed"); 
-				return 0;
-			}
-			else CloseHandle(hThread); 
-		} 
-		else 
-			// The client could not connect, so close the pipe. 
-			CloseHandle(hPipe); 
-	} 
-
-
-	CSVNStatusCache::Destroy();
-	apr_terminate();
-
-	return 0;
-}
-
-
-//////////////////////////////////////////////////////////////////////////
-
-VOID GetAnswerToRequest(const TSVNCacheRequest* pRequest, TSVNCacheResponse* pReply, DWORD* pResponseLength)
-{
-	CTSVNPath path;
-	if(pRequest->flags & TSVNCACHE_FLAGS_FOLDERISKNOWN)
-	{
-		path.SetFromWin(pRequest->path, !!(pRequest->flags & TSVNCACHE_FLAGS_ISFOLDER));
+				if (hInstanceThread == NULL) 
+				{
+					printf("CreateThread failed"); 
+					return;
+				}
+				else CloseHandle(hInstanceThread); 
+			} 
+			else 
+				// The client could not connect, so close the pipe. 
+				CloseHandle(hPipe); 
+		}
 	}
-	else
-	{
-		path.SetFromWin(pRequest->path);
-	}
-
-	CSVNStatusCache::Instance().GetStatusForPath(path, pRequest->flags).BuildCacheResponse(*pReply, *pResponseLength);
+	ATLTRACE("Pipe thread exited\n");
 }
-
 
 VOID InstanceThread(LPVOID lpvParam) 
 { 
@@ -137,7 +223,7 @@ VOID InstanceThread(LPVOID lpvParam)
 
 	hPipe = (HANDLE) lpvParam; 
 
-	while (1) 
+	while (bRun) 
 	{ 
 		// Read client requests from the pipe. 
 		TSVNCacheRequest request;
@@ -172,5 +258,6 @@ VOID InstanceThread(LPVOID lpvParam)
 	FlushFileBuffers(hPipe); 
 	DisconnectNamedPipe(hPipe); 
 	CloseHandle(hPipe); 
+	ATLTRACE("Instance thread exited\n");
 }
 
