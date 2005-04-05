d808fda04ab439ca696803207f0cdffe2998ed02
luebbe.tortoisesvn <luebbe.tortoisesvn@154be670-7969-47af-9ab0-c6fb378d2295>
2005-04-05 09:20:11 +0000

    Finally found a way to check the po files and build the installer without having to use quotes around the strings in Languages.txt. Now using tabs as separators.
    reduced the statusreport to a compact output format
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@2967 154be670-7969-47af-9ab0-c6fb378d2295

--- Languages/Make_Check.bat
+++ Languages/Make_Check.bat
@@ -4,66 +4,77 @@ rem Script to build the language dlls
 SETLOCAL ENABLEDELAYEDEXPANSION
 if "%TortoiseVars%"=="" call ..\TortoiseVars.bat
 set LogFile=statusreport.txt
-
+set LotsOfBlanks="                              "
 
 rem Count all messages in PO Template file
 FOR /F "usebackq" %%p IN (`Check_Attrib.bat Tortoise.pot`) DO SET total=%%p
 FOR /F "usebackq" %%p IN (`svnversion .`) DO SET version=%%p
 
-copy Tortoise_*.po _Tortois_*.po /Y
-FOR %%i in (_Tortois_*.po) do msgmerge --no-wrap --quiet --no-fuzzy-matching -s %%i Tortoise.pot -o %%i
+copy Tortoise_*.po _Tortois_*.po /Y 2>NUL
+echo.
+echo Merging translations with latest .pot file
+echo ------------------------------------------
+FOR %%i in (_Tortois_*.po) DO (
+  echo %%i
+  msgmerge --no-wrap --quiet --no-fuzzy-matching -s %%i Tortoise.pot -o %%i 2> NUL
+)
 
 echo. > %LogFile%
 echo Translation Status report for r!version:~0,4! >> %LogFile%
 echo ----------------------------------- >> %LogFile%
 echo Total=!total! >> %LogFile%
 echo. >> %LogFile%
+echo Language : translated/fuzzy/untranslated/missing accelerator keys >> %LogFile%
+echo. >> %LogFile%
 
-rem fourth parameter is needed with quotes!
-FOR /F "eol=# delims=; tokens=1,2,3,4,5" %%i in (Languages.txt) do call :doit %%i %%j %%m %%m
+echo.
+echo Translation Status report for r!version:~0,4!
+echo ------------------------------------------
+echo Total=!total!
+echo.
+echo Language : translated/fuzzy/untranslated/missing accelerator keys
+echo.
+
+rem !!! ATTENTION 
+rem !!! There is a real TAB key inside "delims=	;"
+rem !!! Please leave it there
+
+FOR /F "eol=# delims=	; tokens=1,5" %%i IN (Languages.txt) DO (
+
+  SET POFILE=_Tortois_%%i.po
+  SET LANGNAME=%%j ^(%%i^)%LotsOfBlanks:~1,30%
+  SET LANGNAME=!LANGNAME:~0,30! :
+
+  if exist !POFILE! (
+    set errors=0
+    set tra=0
+    set unt=0
+    set fuz=0
+    set obs=0
+
+    FOR /F "usebackq skip=1 " %%p IN (`Check_Accel.bat !POFILE!`) DO SET errors=%%p
+    FOR /F "usebackq" %%p IN (`Check_Attrib.bat --translated --no-obsolete !POFILE!`) DO SET tra=%%p
+    FOR /F "usebackq" %%p IN (`Check_Attrib.bat --only-fuzzy --no-obsolete !POFILE!`) DO SET fuz=%%p
+    FOR /F "usebackq" %%p IN (`Check_Attrib.bat --untranslated --no-obsolete !POFILE!`) DO SET unt=%%p
+
+    SET /A errsum=!fuz!+!unt!+!errors!
+
+    if !errsum! EQU 0 (
+      echo !LANGNAME! !total! - OK
+      echo !LANGNAME! !total! - OK >> %LogFile% 
+    ) else (
+      echo !LANGNAME! !tra!/!fuz!/!unt!/!errors!
+      echo !LANGNAME! !tra!/!fuz!/!unt!/!errors! >> %LogFile% 
+    )
+  ) ELSE (
+    echo !LANGNAME! !POFILE! not found
+  )
+
+)
 
 del _Tortois_*.po /Q
 del *.mo
 
 :end
 ENDLOCAL
-goto :eof
-
-:doit
-echo %4 (%1)
-echo %4 (%1) >> %LogFile%
-
-if exist _Tortois_%1%.po (
-  set errors=0
-  set tra=0
-  set unt=0
-  set fuz=0
-  set obs=0
-
-  FOR /F "usebackq skip=1 " %%p IN (`Check_Accel.bat _Tortois_%1.po`) DO SET errors=%%p
-rem   FOR /F "usebackq" %%p IN (`Check_Attrib.bat _Tortois_%1.po --only-obsolete`) DO SET obs=%%p
-  FOR /F "usebackq" %%p IN (`Check_Attrib.bat --translated --no-obsolete _Tortois_%1.po`) DO SET tra=%%p
-  FOR /F "usebackq" %%p IN (`Check_Attrib.bat --only-fuzzy --no-obsolete _Tortois_%1.po`) DO SET fuz=%%p
-  FOR /F "usebackq" %%p IN (`Check_Attrib.bat --untranslated --no-obsolete _Tortois_%1.po`) DO SET unt=%%p
-
-  echo translated  :    !tra! >> %LogFile%
-  echo fuzzy       :    !fuz! >> %LogFile%
-  echo untranslated:    !unt! >> %LogFile%
-  echo missing hotkeys: !errors! >> %LogFile%
-  echo. >> %LogFile%
-
-  if !tra! EQU !total! (
-    SET unt=0
-  )
-
-  SET /A tra=!tra!-!fuz!
-
-  SET /A wt=200*!tra!/!total!
-  SET /A wf=200*!fuz!/!total!
-  SET /A wu=200*!unt!/!total!
-
-  SET /A tra=!wt!/2
-
-)
-
 goto :eof
--- Languages/Make_Installer.bat
+++ Languages/Make_Installer.bat
@@ -16,35 +16,40 @@ set OFile=product.nsh
 del ..\bin\Tortoise*.dll
 del ..\bin\LanguagePack*.exe
 
-FOR /F "eol=# tokens=1,2,3,4,5,6 delims=;" %%i in (Languages.txt) do call :doit %%i %%j %%k %%l %%m %%n %1
+rem !!! ATTENTION 
+rem !!! There is a real TAB key inside "delims=	;"
+rem !!! Please leave it there
+
+FOR /F "eol=# tokens=1,2,3,4,5,6 delims=	;" %%i IN (Languages.txt) DO (
+
+  if NOT "%1"=="" (
+    if NOT "%1"=="%%i" goto :eof
+  )
+
+  echo.
+  echo Building %%m dlls and installer
+  ..\bin\release\bin\restext apply TortoiseProcLang.dll ..\bin\TortoiseProc%%j.dll Tortoise_%%i.po 
+  ..\bin\release\bin\restext apply TortoiseMergeLang.dll ..\bin\TortoiseMerge%%j.dll Tortoise_%%i.po 
+
+  echo ^^!define PRODUCT_NAME "TortoiseSVN %%n" > %OFile%
+  echo ^^!define CountryCode "%%i" >> %OFile%
+  echo ^^!define CountryID "%%j" >> %OFile%
+  echo ^^!define InstLang "%%l" >> %OFile%
+  if %%k EQU 1 echo ^^!define LangHelp >> %OFile%
+
+  if EXIST ..\..\Subversion\Subversion\po\%%i.po (
+    msgfmt ..\..\Subversion\Subversion\po\%%i.po -o subversion.mo -f
+    if EXIST subversion.mo echo ^^!define MoFile "subversion.mo" >> %OFile%
+  )
+
+  MakeNSIS /V1 LanguagePack.nsi
+  del ..\bin\TortoiseProc%%j.dll
+  del ..\bin\TortoiseMerge%%j.dll
+  if EXIST subversion.mo del subversion.mo
+)
+
+rem call :doit %%i %%j %%k %%l %%m %%n %1
 
 :end
 endlocal
-goto :eof
-
-:doit
-if NOT "%7"=="" (
-  if NOT "%7"=="%1" goto :eof
-)
-
-echo.
-echo Building %5 dlls and installer
-..\bin\release\bin\restext apply TortoiseProcLang.dll ..\bin\TortoiseProc%2.dll Tortoise_%1.po 
-..\bin\release\bin\restext apply TortoiseMergeLang.dll ..\bin\TortoiseMerge%2.dll Tortoise_%1.po 
-
-echo ^^!define PRODUCT_NAME "TortoiseSVN %~6" > %OFile%
-echo ^^!define CountryCode "%1" >> %OFile%
-echo ^^!define CountryID "%2" >> %OFile%
-echo ^^!define InstLang "%4" >> %OFile%
-if %3 EQU 1 echo ^^!define LangHelp >> %OFile%
-
-if EXIST ..\..\Subversion\Subversion\po\%1.po (
-msgfmt ..\..\Subversion\Subversion\po\%1.po -o subversion.mo -f
-if EXIST subversion.mo echo ^^!define MoFile "subversion.mo" >> %OFile%
-)
-
-MakeNSIS /V1 LanguagePack.nsi
-del ..\bin\TortoiseProc%2.dll
-del ..\bin\TortoiseMerge%2.dll
-if EXIST subversion.mo del subversion.mo
 goto :eof
