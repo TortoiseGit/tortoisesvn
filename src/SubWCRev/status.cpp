d3385279b1f06e2ddb82c03bc983da75c1ead9e0
simonlarge <simonlarge@154be670-7969-47af-9ab0-c6fb378d2295>
2005-04-12 21:09:22 +0000

    Indentation cleanup.
    Make the source file use tabs instead of spaces, like the rest of the project source.
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@3023 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SubWCRev/status.cpp
+++ src/SubWCRev/status.cpp
@@ -74,61 +74,63 @@ void getallstatus(void * baton, const char * /*path*/, svn_wc_status_t * status)
 }
 
 svn_error_t *
-svn_status (       const char *path,
-                   void *status_baton,
-                   svn_boolean_t no_ignore,
-                   svn_client_ctx_t *ctx,
-                   apr_pool_t *pool)
+svn_status (	const char *path,
+				void *status_baton,
+				svn_boolean_t no_ignore,
+				svn_client_ctx_t *ctx,
+				apr_pool_t *pool)
 {
-  svn_wc_adm_access_t *adm_access;
-  svn_wc_traversal_info_t *traversal_info = svn_wc_init_traversal_info (pool);
-  const char *anchor, *target;
-  const svn_delta_editor_t *editor;
-  void *edit_baton;
-  const svn_wc_entry_t *entry;
-  svn_revnum_t edit_revision = SVN_INVALID_REVNUM;
+	svn_wc_adm_access_t *adm_access;
+	svn_wc_traversal_info_t *traversal_info = svn_wc_init_traversal_info (pool);
+	const char *anchor, *target;
+	const svn_delta_editor_t *editor;
+	void *edit_baton;
+	const svn_wc_entry_t *entry;
+	svn_revnum_t edit_revision = SVN_INVALID_REVNUM;
 
-  svn_utf_initialize(pool);
+	svn_utf_initialize(pool);
 
-  /* Need to lock the tree as even a non-recursive status requires the
-     immediate directories to be locked. */
-  SVN_ERR (svn_wc_adm_probe_open2 (&adm_access, NULL, path, 
-                                  FALSE, 0, pool));
+  	// Need to lock the tree as even a non-recursive status requires the
+	// immediate directories to be locked.
+	SVN_ERR (svn_wc_adm_probe_open2 (&adm_access, NULL, path, FALSE, 0, pool));
 
-  /* Get the entry for this path so we can determine our anchor and
-     target.  If the path is unversioned, and the caller requested
-     that we contact the repository, we error. */
-  SVN_ERR (svn_wc_entry (&entry, path, adm_access, FALSE, pool));
-  if (entry)
-    SVN_ERR (svn_wc_get_actual_target (path, &anchor, &target, pool));
-  else 
-    svn_path_split (path, &anchor, &target, pool);
-  
-  /* Close up our ADM area.  We'll be re-opening soon. */
-  SVN_ERR (svn_wc_adm_close (adm_access));
+	// Get the entry for this path so we can determine our anchor and
+	// target.  If the path is unversioned, and the caller requested
+	// that we contact the repository, we error.
+	SVN_ERR (svn_wc_entry (&entry, path, adm_access, FALSE, pool));
+	if (entry)
+	{
+		SVN_ERR (svn_wc_get_actual_target (path, &anchor, &target, pool));
+	}
+	else
+	{
+		svn_path_split (path, &anchor, &target, pool);
+	}
 
-  /* Need to lock the tree as even a non-recursive status requires the
-     immediate directories to be locked. */
-  SVN_ERR (svn_wc_adm_probe_open2 (&adm_access, NULL, anchor, 
-                                  FALSE, -1, pool));
+	// Close up our ADM area.  We'll be re-opening soon.
+	SVN_ERR (svn_wc_adm_close (adm_access));
 
-  /* Get the status edit, and use our wrapping status function/baton
-     as the callback pair. */
-  SVN_ERR (svn_wc_get_status_editor (&editor, &edit_baton, &edit_revision,
-                                     adm_access, target, ctx->config, TRUE,
-                                     TRUE, no_ignore, getallstatus, status_baton,
-                                     ctx->cancel_func, ctx->cancel_baton,
-                                     traversal_info, pool));
+	// Need to lock the tree as even a non-recursive status requires the
+	// immediate directories to be locked.
+	SVN_ERR (svn_wc_adm_probe_open2 (&adm_access, NULL, anchor, FALSE, -1, pool));
 
-      SVN_ERR (editor->close_edit (edit_baton, pool));
+	// Get the status edit, and use our wrapping status function/baton
+	// as the callback pair.
+	SVN_ERR (svn_wc_get_status_editor (&editor, &edit_baton, &edit_revision,
+									   adm_access, target, ctx->config, TRUE,
+									   TRUE, no_ignore, getallstatus, status_baton,
+									   ctx->cancel_func, ctx->cancel_baton,
+									   traversal_info, pool));
 
-
-  /* Close the access baton here, as svn_client__do_external_status()
-     calls back into this function and thus will be re-opening the
-     working copy. */
-  SVN_ERR (svn_wc_adm_close (adm_access));
+	SVN_ERR (editor->close_edit (edit_baton, pool));
 
 
-  return SVN_NO_ERROR;
+	// Close the access baton here, as svn_client__do_external_status()
+	// calls back into this function and thus will be re-opening the
+	// working copy.
+	SVN_ERR (svn_wc_adm_close (adm_access));
+
+
+	return SVN_NO_ERROR;
 }
 #pragma warning(pop)
