fe1e600d6e1385305a4146cd24c45c6c0f161148
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-04-15 18:07:44 +0000

    Fix showing the wrong Subversion version number in the about dialog. Patch from Mark Phippard.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@3038 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/AboutDlg.cpp
+++ src/TortoiseProc/AboutDlg.cpp
@@ -66,7 +66,7 @@ BOOL CAboutDlg::OnInitDialog()
 	const svn_version_t * svnver = svn_client_version();
 
 	temp.Format(IDS_ABOUTVERSION, TSVN_VERMAJOR, TSVN_VERMINOR, TSVN_VERMICRO, TSVN_VERBUILD, _T(STRINGWIDTH),
-		svnver->major, svnver->major, svnver->patch, CString(svnver->tag), 
+		svnver->major, svnver->minor, svnver->patch, CString(svnver->tag), 
 		APR_MAJOR_VERSION, APR_MINOR_VERSION, APR_PATCH_VERSION,
 		API_MAJOR_VERSION, API_MINOR_VERSION, API_PATCH_VERSION,
 		APU_MAJOR_VERSION, APU_MINOR_VERSION, APU_PATCH_VERSION,
