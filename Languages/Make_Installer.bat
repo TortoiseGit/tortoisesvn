71f544ca6fe348c4bf2e75bc1471fbfb58b06f5b
luebbe.tortoisesvn <luebbe.tortoisesvn@154be670-7969-47af-9ab0-c6fb378d2295>
2005-05-20 14:09:23 +0000

    GUI Translation update: * Tortoise_de.po: Lübbe Onken * Make_Installer.bat: fixed error when only a single language was given on the commandline
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@3422 154be670-7969-47af-9ab0-c6fb378d2295

--- Languages/Make_Installer.bat
+++ Languages/Make_Installer.bat
@@ -22,34 +22,34 @@ rem !!! Please leave it there
 
 FOR /F "eol=# tokens=1,2,3,4,5,6 delims=	;" %%i IN (Languages.txt) DO (
 
+  set BUILD=1
   if NOT "%1"=="" (
-    if NOT "%1"=="%%i" goto :eof
+    if NOT "%1"=="%%i" set BUILD=0
   )
 
-  echo.
-  echo Building %%m dlls and installer
-  ..\bin\release\bin\restext apply TortoiseProcLang.dll ..\bin\TortoiseProc%%j.dll Tortoise_%%i.po 
-  ..\bin\release\bin\restext apply TortoiseMergeLang.dll ..\bin\TortoiseMerge%%j.dll Tortoise_%%i.po 
+  if !BUILD! EQU 1 (
+    echo.
+    echo Building %%m dlls and installer
+    ..\bin\release\bin\restext apply TortoiseProcLang.dll ..\bin\TortoiseProc%%j.dll Tortoise_%%i.po 
+    ..\bin\release\bin\restext apply TortoiseMergeLang.dll ..\bin\TortoiseMerge%%j.dll Tortoise_%%i.po 
 
-  echo ^^!define PRODUCT_NAME "TortoiseSVN %%n" > %OFile%
-  echo ^^!define CountryCode "%%i" >> %OFile%
-  echo ^^!define CountryID "%%j" >> %OFile%
-  echo ^^!define InstLang "%%l" >> %OFile%
-  if %%k EQU 1 echo ^^!define LangHelp >> %OFile%
+    echo ^^!define PRODUCT_NAME "TortoiseSVN %%n" > %OFile%
+    echo ^^!define CountryCode "%%i" >> %OFile%
+    echo ^^!define CountryID "%%j" >> %OFile%
+    echo ^^!define InstLang "%%l" >> %OFile%
+    if %%k EQU 1 echo ^^!define LangHelp >> %OFile%
 
-  if EXIST ..\..\Subversion\Subversion\po\%%i.po (
-    msgfmt ..\..\Subversion\Subversion\po\%%i.po -o subversion.mo -f
-    if EXIST subversion.mo echo ^^!define MoFile "subversion.mo" >> %OFile%
+    if EXIST ..\..\Subversion\Subversion\po\%%i.po (
+      msgfmt ..\..\Subversion\Subversion\po\%%i.po -o subversion.mo -f
+      if EXIST subversion.mo echo ^^!define MoFile "subversion.mo" >> %OFile%
+    )
+
+    MakeNSIS /V1 LanguagePack.nsi
+    del ..\bin\TortoiseProc%%j.dll
+    del ..\bin\TortoiseMerge%%j.dll
+    if EXIST subversion.mo del subversion.mo
   )
 
-  MakeNSIS /V1 LanguagePack.nsi
-  del ..\bin\TortoiseProc%%j.dll
-  del ..\bin\TortoiseMerge%%j.dll
-  if EXIST subversion.mo del subversion.mo
 )
 
-rem call :doit %%i %%j %%k %%l %%m %%n %1
-
-:end
-endlocal
-goto :eof
+ENDLOCAL
