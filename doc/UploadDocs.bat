af9afbbb1b282c3b53fd60686c0c2300820b4632
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-06-24 16:11:00 +0000

    Add script to upload the docs to sourceforge.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@3777 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ doc/UploadDocs.bat
@@ -0,0 +1,34 @@
+@echo off
+rem run this batch file to upload the docs to our sourceforge server
+rem to make this work, you have to create a file "docserverlogin.bat"
+rem which sets the variables USERNAME and PASSWORD, and also set the PSCP
+rem variable to point to your scp program the PLINK variable to the plink
+rem program and the ZIP variable to your zip program
+
+rem example docserverlogin.bat file
+rem 
+rem @echo off
+rem set USERNAME=myname
+rem set PASSWORD=mypassword
+rem set PsCP="C:\Programme\PuttY\pscp.exe"
+rem set PLINK="C:\Programme\Putty\plink.exe"
+rem set ZIP="C:\Programme\7-zip\7z.exe"
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+call docserverlogin.bat
+
+cd output
+del docs.zip
+%ZIP% a -r -tzip docs.zip *
+
+%PSCP% -r -l %USERNAME% -pw %PASSWORD% docs.zip shell.sourceforge.net:/home/groups/t/to/tortoisesvn/htdocs/docs
+
+if "%1"=="" (
+%PLINK% shell.sourceforge.net -l %USERNAME% -pw %PASSWORD% unzip -o /home/groups/t/to/tortoisesvn/htdocs/docs/docs.zip -d /home/groups/t/to/tortoisesvn/htdocs/docs/nightly;chmod -R g+rw /home/groups/t/to/tortoisesvn/htdocs/docs/nightly;rm -f docs.zip
+) else (
+%PLINK% shell.sourceforge.net -l %USERNAME% -pw %PASSWORD% unzip -o /home/groups/t/to/tortoisesvn/htdocs/docs/docs.zip -d /home/groups/t/to/tortoisesvn/htdocs/docs/release;chmod -R g+rw /home/groups/t/to/tortoisesvn/htdocs/docs/release;rm -f docs.zip
+)
+del docs.zip
+
+cd ..
