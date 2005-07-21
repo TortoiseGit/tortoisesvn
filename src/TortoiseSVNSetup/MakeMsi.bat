db6b0e220df4ff6e69cd72a15cd15a5ef34601ef
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-07-21 15:36:48 +0000

    Remove the english thesaurus from the installer. Instead, include the British spell checker (if available).
    This saves about 2.5MB in the installer package.
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@3953 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseSVNSetup/MakeMsi.bat
+++ src/TortoiseSVNSetup/MakeMsi.bat
@@ -8,9 +8,9 @@ if NOT EXIST Setup_good.wxs (copy Setup.wxs Setup_good.wxs)
 if NOT EXIST makemsisub.bat (copy makemsisub.in makemsisub.bat)
 rem include the spell checker and thesaurus if they're available
 set ENSPELL=0
-set ENTHES=0
-if EXIST ..\..\..\Common\Spell_Thes\en_US.aff (set ENSPELL=1)
-if EXIST ..\..\..\Common\Spell_Thes\th_en_US_v2.idx (set ENTHES=1)
+set ENGBSPELL=0
+if EXIST ..\..\..\Common\Spell\en_US.aff (set ENSPELL=1)
+if EXIST ..\..\..\Common\Spell\en_GB.aff (set ENGBSPELL=1)
 rem don't include the crashreport dll if the build is not done by myself.
 rem Since I don't have the debug symbols of those builds, crashreports sent
 rem to me are unusable.
