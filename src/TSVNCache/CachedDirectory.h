894deb3fd6e395580ce05eafed3cd7dfd1a472be
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-08-04 16:51:42 +0000

    Revert revision 4061 because it didn't work with externals.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@4064 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TSVNCache/CachedDirectory.h
+++ src/TSVNCache/CachedDirectory.h
@@ -51,44 +51,42 @@ private:
 	CStatusCacheEntry LookForItemInCache(const CTSVNPath& path, bool &bFound);
 	void UpdateChildDirectoryStatus(const CTSVNPath& childDir, svn_wc_status_kind childStatus);
 
-	/// Calculate the complete, composite status from ourselves, our files, and our decendants
+	// Calculate the complete, composite status from ourselves, our files, and our decendants
 	svn_wc_status_kind CalculateRecursiveStatus() const;
 
-	/// Update our composite status and deal with things if it's changed
+	// Update our composite status and deal with things if it's changed
 	void UpdateCurrentStatus();
 
-	/// Get the current full status of this folder, recalculating if necessary
+	// Get the current full status of this folder, recalculating if necessary
 	svn_wc_status_kind GetCurrentFullStatus();
 
 private:
 	CComAutoCriticalSection m_critSec;
 
-	/// The cache of files and directories within this directory
+	// The cache of files and directories within this directory
 	typedef std::map<CString, CStatusCacheEntry> CacheEntryMap; 
 	CacheEntryMap m_entryCache; 
 
-	/// A vector if iterators to child directorys - used to put-together recursive status
+	/// A vector if iterators to child directories - used to put-together recursive status
 	typedef std::map<CTSVNPath, svn_wc_status_kind>  ChildDirStatus;
 	ChildDirStatus m_childDirectories;
 
-	/// The timestamp of the .SVN\entries file.  For an unversioned directory, this will be zero
+	// The timestamp of the .SVN\entries file.  For an unversioned directory, this will be zero
 	__int64 m_entriesFileTime;
-	/// The timestamp of the .SVN\props dir.  For an unversioned directory, this will be zero
+	// The timestamp of the .SVN\props dir.  For an unversioned directory, this will be zero
 	__int64 m_propsDirTime;
-	/// the timestamp of the folder itself.
-	__int64 m_FileTime;
 	
-	/// The path of the directory with this object looks after
+	// The path of the directory with this object looks after
 	CTSVNPath	m_directoryPath;
 
-	/// The status of THIS directory (not a composite of children or members)
+	// The status of THIS directory (not a composite of children or members)
 	CStatusCacheEntry m_ownStatus;
 
-	/// Our current fully recursive status
+	// Our current fully recursive status
 	svn_wc_status_kind  m_currentFullStatus;
 	bool m_bCurrentFullStatusValid;
 
-	/// The most important status from all our file entries
+	// The most important status from all our file entries
 	svn_wc_status_kind m_mostImportantFileStatus;
 
 	bool m_bRecursive;		// used in the status callback
