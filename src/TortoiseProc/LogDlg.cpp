b9b8d3c23b8250d41c0fa7ea7d0d1c70c67e8e06
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-08-05 14:18:29 +0000

    Pass a peg revision to the diff function.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@4082 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/LogDlg.cpp
+++ src/TortoiseProc/LogDlg.cpp
@@ -1683,7 +1683,7 @@ void CLogDlg::DoDiffFromLog(int selIndex, long rev)
 	secondfile = filepath + secondfile.Trim();
 
 	SVNDiff diff(this, this->m_hWnd, true);
-	diff.ShowCompare(CTSVNPath(secondfile), fromrev, CTSVNPath(firstfile), rev);
+	diff.ShowCompare(CTSVNPath(secondfile), fromrev, CTSVNPath(firstfile), rev, rev);
 
 	theApp.DoWaitCursor(-1);
 	GetDlgItem(IDOK)->EnableWindow(TRUE);
