6d2e7c701c8c5cf52b5beb4eaef24e259204bb46
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-08-13 09:38:39 +0000

    Use the end revision as the peg revision when merging. Otherwise merging from tag/branch creations doesn't work.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@4141 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/SVNProgressDlg.cpp
+++ src/TortoiseProc/SVNProgressDlg.cpp
@@ -861,7 +861,7 @@ UINT CSVNProgressDlg::ProgressThread()
 				if (m_url.IsEquivalentTo(urlTo))
 				{
 					if (!m_pSvn->PegMerge(m_url, m_Revision, m_RevisionEnd, 
-						m_pegRev.IsValid() ? m_pegRev : (m_url.IsUrl() ? m_Revision : SVNRev(SVNRev::REV_WC)),
+						m_pegRev.IsValid() ? m_pegRev : (m_url.IsUrl() ? m_RevisionEnd : SVNRev(SVNRev::REV_WC)),
 						m_targetPathList[0], true, true, false, !!(m_options & ProgOptDryRun)))
 					{
 						ReportSVNError();
