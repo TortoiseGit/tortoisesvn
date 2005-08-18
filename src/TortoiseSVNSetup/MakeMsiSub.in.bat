a91ae9f259846552d02fd2c0e52a09291f7beea1
petemounce <petemounce@154be670-7969-47af-9ab0-c6fb378d2295>
2005-08-18 20:38:40 +0000

    Inserted the version number tokens so that version.bat can substitute them during builds.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@4197 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseSVNSetup/MakeMsiSub.in.bat
+++ src/TortoiseSVNSetup/MakeMsiSub.in.bat
@@ -2,14 +2,14 @@ echo MakeMsiSub.bat: candle (WiX compilation)
 candle -nologo -out ..\..\bin\ TortoiseSVN.wxs FeaturesFragment.wxs StructureFragment.wxs UIFragment.wxs
 
 echo MakeMsiSub.bat: light (WiX linking)
-light -out ..\..\bin\TortoiseSVN-1.2.1.$WCREV$-dev-svn-1.2.1.msi ..\..\bin\TortoiseSVN.wixobj ..\..\bin\FeaturesFragment.wixobj ..\..\bin\StructureFragment.wixobj ..\..\bin\UIFragment.wixobj
+light -out ..\..\bin\TortoiseSVN-$MajorVersion$.$MinorVersion$.$MicroVersion$.$WCREV$-dev-svn-1.2.1.msi ..\..\bin\TortoiseSVN.wixobj ..\..\bin\FeaturesFragment.wixobj ..\..\bin\StructureFragment.wixobj ..\..\bin\UIFragment.wixobj
 
 echo MakeMsiSub.bat: Cleaning up
 cd ..\..\bin
 del *.wixobj
 
 echo MakeMsiSub.bat: md5
-md5 -omd5.txt TortoiseSVN-1.2.1.$WCREV$-dev-svn-1.2.1.msi
+md5 -omd5.txt TortoiseSVN-$MajorVersion$.$MinorVersion$.$MicroVersion$.$WCREV$-dev-svn-1.2.1.msi
 cd ..\src\TortoiseSVNSetup
 
 echo MakeMsiSub.bat: Done
