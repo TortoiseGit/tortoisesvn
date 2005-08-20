c9a80c5c66ddb248033faeb9771138f594d4aecc
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-08-20 08:17:41 +0000

    Patch from Stefan Fuhrmann: We cannot test for C/C++ names to be defined while still in the preprocessor. We were just happy that identical redeclaration is allowed.
    Replace with unconditional typedefs (that happen to be redeclarations in VC7.1 and newer) and support Win64, too.
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@4219 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/NewMenu/NewMenu.h
+++ src/Utils/NewMenu/NewMenu.h
@@ -51,16 +51,24 @@
 #define MFT_CENTER      0x0040
 
 // Typedefinition for compatibility to MFC 7.0
-#ifndef DWORD_PTR
-typedef DWORD DWORD_PTR, *PDWORD_PTR;
-#endif
 
-#ifndef ULONG_PTR
-typedef unsigned long ULONG_PTR, *PULONG_PTR;
-#endif
+#if defined(_WIN64)
+    typedef __int64 INT_PTR, *PINT_PTR;
+    typedef unsigned __int64 UINT_PTR, *PUINT_PTR;
 
-#ifndef LONG_PTR
-typedef long LONG_PTR, *PLONG_PTR;
+    typedef __int64 LONG_PTR, *PLONG_PTR;
+    typedef unsigned __int64 ULONG_PTR, *PULONG_PTR;
+
+    #define __int3264   __int64
+
+#else
+    typedef _W64 int INT_PTR, *PINT_PTR;
+    typedef _W64 unsigned int UINT_PTR, *PUINT_PTR;
+
+    typedef _W64 long LONG_PTR, *PLONG_PTR;
+    typedef _W64 unsigned long ULONG_PTR, *PULONG_PTR;
+
+    #define __int3264   __int32
 #endif
 
 // Additional flagdefinition for highlighting
