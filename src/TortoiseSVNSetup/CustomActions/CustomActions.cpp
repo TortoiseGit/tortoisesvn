b82add64839077633e003497052d44d9db9f1f99
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-09-10 15:23:36 +0000

    Add a custom action for the installer. * The custom action dll for now only has one single exported function (TerminateCache), which sends a WM_CLOSE message to the maybe running cache process. * The TerminateCache function is called right after the uninstalling of TortoiseSVN. This way a later install can replace the TSVNCache.exe without problems, and also the new exe is used right away after installing, without the need to kill that process first manually.
    This implements issue #130.
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@4373 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ src/TortoiseSVNSetup/CustomActions/CustomActions.cpp
@@ -0,0 +1,40 @@
+// CustomActions.cpp : Defines the entry point for the DLL application.
+//
+
+#include "stdafx.h"
+
+BOOL APIENTRY DllMain( HANDLE hModule, 
+                       DWORD  ul_reason_for_call, 
+                       LPVOID lpReserved
+					 )
+{
+    return TRUE;
+}
+
+UINT __stdcall TerminateCache(MSIHANDLE hModule)
+{
+	HWND hWnd = FindWindow(_T("TSVNCacheWindow"), _T("TSVNCacheWindow"));
+	if (hWnd)
+	{
+		// First, delete the registry key telling the shell where to find
+		// the cache. This is to make sure that the cache won't be started
+		// again right after we close it.
+		HKEY hKey = 0;
+		if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, _T("Software\\TortoiseSVN"), 0, KEY_WRITE, &hKey)==ERROR_SUCCESS)
+		{
+			RegDeleteValue(hKey, _T("CachePath"));
+			RegCloseKey(hKey);
+		}
+		SendMessage(hWnd, WM_CLOSE, NULL, NULL);
+		Sleep(1500);
+		if (!IsWindow(hWnd))
+		{
+			// Cache is gone!
+			return ERROR_SUCCESS;
+		}
+		return ERROR_FUNCTION_FAILED;
+	}
+	// cache wasn't even running
+	return ERROR_SUCCESS;
+}
+
