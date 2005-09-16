1d2120778d30c9390ec4bbb10b3468b20cf6abac
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-09-16 17:15:12 +0000

    Add new class implementing a personal dictionary. The personal dictionary is implemented as a set of simple words. The personal dict is to be used in conjunction with the spellchecker: Every word in this personal dictionary should simply not be marked as misspelled even if the spellchecker thinks it is.
    This is the first step for issue #168.
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@4401 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ src/Utils/PersonalDictionary.cpp
@@ -0,0 +1,124 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2005 - Tim Kemp and Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include "StdAfx.h"
+#include <fstream>
+#include "PersonalDictionary.h"
+
+CPersonalDictionary::CPersonalDictionary(LONG lLanguage /* = 0*/) :
+	m_bLoaded(false)
+{
+	m_lLanguage = lLanguage;
+}
+
+CPersonalDictionary::~CPersonalDictionary()
+{
+}
+
+bool CPersonalDictionary::Load()
+{
+	CString sWord;
+	TCHAR line[100*1024];
+
+	if (m_bLoaded)
+		return true;
+	TCHAR path[MAX_PATH];		//MAX_PATH ok here.
+	TCHAR sLang[10];
+	if (SHGetFolderPath(NULL, CSIDL_APPDATA, NULL, SHGFP_TYPE_CURRENT, path)!=S_OK)
+		return false;
+	_tcscat(path, _T("\\TortoiseSVN"));
+
+	if (!PathIsDirectory(path))
+		CreateDirectory(path, NULL);
+
+	if (m_lLanguage==0)
+		m_lLanguage = GetUserDefaultLCID();
+
+	_stprintf(sLang, _T("\\%ld"), m_lLanguage);
+	_tcscat(path, sLang);
+	_tcscat(path, _T(".dic"));
+
+	std::wifstream File;
+	char filepath[MAX_PATH+1];
+	ZeroMemory(filepath, sizeof(filepath));
+	WideCharToMultiByte(CP_ACP, NULL, path, -1, filepath, MAX_PATH, NULL, NULL);
+	File.open(filepath);
+	if (!File.good())
+	{
+		return false;
+	}
+	std::vector<std::wstring> entry;
+	do
+	{
+		File.getline(line, sizeof(line)/sizeof(TCHAR));
+		sWord = line;
+		dict.insert(sWord);
+	} while (File.gcount() > 0);
+	File.close();
+	m_bLoaded = true;
+	return true;
+}
+
+bool CPersonalDictionary::AddWord(const CString& sWord)
+{
+	dict.insert(sWord);
+	return true;
+}
+
+bool CPersonalDictionary::FindWord(const CString& sWord)
+{
+	if (!m_bLoaded)
+		Load();
+	// even if the load failed for some reason, we mark it as loaded
+	// and just assume an empty personal dictionary
+	m_bLoaded = true;
+	std::set<CString>::iterator it;
+	it = dict.find(sWord);
+	return (it != dict.end());
+}
+
+bool CPersonalDictionary::Save()
+{
+	TCHAR path[MAX_PATH];		//MAX_PATH ok here.
+	TCHAR sLang[10];
+	if (SHGetFolderPath(NULL, CSIDL_APPDATA, NULL, SHGFP_TYPE_CURRENT, path)!=S_OK)
+		return false;
+	_tcscat(path, _T("\\TortoiseSVN"));
+
+	if (!PathIsDirectory(path))
+		CreateDirectory(path, NULL);
+
+	if (m_lLanguage==0)
+		m_lLanguage = GetUserDefaultLCID();
+
+	_stprintf(sLang, _T("\\%ld"), m_lLanguage);
+	_tcscat(path, sLang);
+	_tcscat(path, _T(".dic"));
+
+	std::wofstream File;
+	char filepath[MAX_PATH+1];
+	ZeroMemory(filepath, sizeof(filepath));
+	WideCharToMultiByte(CP_ACP, NULL, path, -1, filepath, MAX_PATH, NULL, NULL);
+	File.open(filepath, std::ios_base::binary);
+	for (std::set<CString>::iterator it = dict.begin(); it != dict.end(); ++it)
+	{
+		File << (LPCTSTR)*it << _T("\n");
+	}
+	File.close();
+	return true;
+}
