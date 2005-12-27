3c1324d296a3665cdcde8c2e07fa499c68e62f82
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2005-12-27 17:51:33 +0000

    * Insert empty line numbers in the left and right view when resolving with "theirs before mine" or "mine before theirs" * recalculate the line numbers after that operation.
    This solves issue #197.
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@5267 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseMerge/BottomView.cpp
+++ src/TortoiseMerge/BottomView.cpp
@@ -109,22 +109,32 @@ void CBottomView::OnContextMenu(CPoint point, int /*nLine*/)
 					m_arDiffLines->SetAt(i, m_pwndRight->m_arDiffLines->GetAt(i));
 					m_arLineStates->SetAt(i, m_pwndRight->m_arLineStates->GetAt(i));
 					m_pwndRight->m_arLineStates->SetAt(i, CDiffData::DIFFSTATE_YOURSADDED);
-				} // for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++) 
+				}
 				
 				// your block is done, now insert their block
 				int index = m_nSelBlockEnd+1;
 				for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++)
 				{
 					m_arDiffLines->InsertAt(index, m_pwndLeft->m_arDiffLines->GetAt(i));
+					m_arLineLines->InsertAt(index, m_pwndLeft->m_arLineLines->GetAt(i));
 					m_arLineStates->InsertAt(index++, m_pwndLeft->m_arLineStates->GetAt(i));
 					m_pwndLeft->m_arLineStates->SetAt(i, CDiffData::DIFFSTATE_THEIRSADDED);
-				} // for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++) 
+				}
+				// adjust line numbers
+				for (int i=m_nSelBlockEnd+1; i<GetLineCount(); ++i)
+				{
+					long oldline = (long)m_arLineLines->GetAt(i);
+					if (oldline >= 0)
+						m_arLineLines->SetAt(i, oldline+(index-m_nSelBlockEnd));
+				}
 
 				// now insert an empty block in both yours and theirs
 				m_pwndLeft->m_arDiffLines->InsertAt(m_nSelBlockStart, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndLeft->m_arLineStates->InsertAt(m_nSelBlockStart, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arLineLines->InsertAt(m_nSelBlockStart, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndRight->m_arDiffLines->InsertAt(m_nSelBlockEnd+1, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndRight->m_arLineStates->InsertAt(m_nSelBlockEnd+1, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndRight->m_arLineLines->InsertAt(m_nSelBlockEnd+1, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
 				SetModified();
 				m_pwndLeft->SetModified();
 				m_pwndRight->SetModified();
@@ -136,23 +146,31 @@ void CBottomView::OnContextMenu(CPoint point, int /*nLine*/)
 				{
 					m_arDiffLines->SetAt(i, m_pwndLeft->m_arDiffLines->GetAt(i));
 					m_arLineStates->SetAt(i, m_pwndLeft->m_arLineStates->GetAt(i));
-					//m_pwndLeft->m_arLineStates->SetAt(i, CDiffData::DIFFSTATE_THEIRSADDED);
-				} // for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++) 
+				}
 				
 				// your block is done, now insert their block
 				int index = m_nSelBlockEnd+1;
 				for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++)
 				{
 					m_arDiffLines->InsertAt(index, m_pwndRight->m_arDiffLines->GetAt(i));
+					m_arLineLines->InsertAt(index, m_pwndLeft->m_arLineLines->GetAt(i));
 					m_arLineStates->InsertAt(index++, m_pwndRight->m_arLineStates->GetAt(i));
-					//m_pwndRight->m_arLineStates->SetAt(i, CDiffData::DIFFSTATE_YOURSADDED);
-				} // for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++) 
+				}
+				// adjust line numbers
+				for (int i=m_nSelBlockEnd+1; i<GetLineCount(); ++i)
+				{
+					long oldline = (long)m_arLineLines->GetAt(i);
+					if (oldline >= 0)
+						m_arLineLines->SetAt(i, oldline+(index-m_nSelBlockEnd));
+				}
 
 				// now insert an empty block in both yours and theirs
-				m_pwndRight->m_arDiffLines->InsertAt(m_nSelBlockStart, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
-				m_pwndRight->m_arLineStates->InsertAt(m_nSelBlockStart, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
-				m_pwndLeft->m_arDiffLines->InsertAt(m_nSelBlockEnd+1, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
-				m_pwndLeft->m_arLineStates->InsertAt(m_nSelBlockEnd+1, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arDiffLines->InsertAt(m_nSelBlockStart, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arLineStates->InsertAt(m_nSelBlockStart, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arLineLines->InsertAt(m_nSelBlockStart, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndRight->m_arDiffLines->InsertAt(m_nSelBlockEnd+1, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndRight->m_arLineStates->InsertAt(m_nSelBlockEnd+1, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndRight->m_arLineLines->InsertAt(m_nSelBlockEnd+1, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
 				SetModified();
 				m_pwndLeft->SetModified();
 				m_pwndRight->SetModified();
--- src/TortoiseMerge/LeftView.cpp
+++ src/TortoiseMerge/LeftView.cpp
@@ -184,22 +184,32 @@ void CLeftView::OnContextMenu(CPoint point, int /*nLine*/)
 					m_pwndBottom->m_arDiffLines->SetAt(i, m_pwndRight->m_arDiffLines->GetAt(i));
 					m_pwndBottom->m_arLineStates->SetAt(i, m_pwndRight->m_arLineStates->GetAt(i));
 					m_pwndRight->m_arLineStates->SetAt(i, CDiffData::DIFFSTATE_YOURSADDED);
-				} // for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++) 
+				}
 
 				// your block is done, now insert their block
 				int index = m_nSelBlockEnd+1;
 				for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++)
 				{
 					m_pwndBottom->m_arDiffLines->InsertAt(index, m_pwndLeft->m_arDiffLines->GetAt(i));
+					m_pwndBottom->m_arLineLines->InsertAt(index, m_pwndLeft->m_arLineLines->GetAt(i));
 					m_pwndBottom->m_arLineStates->InsertAt(index++, m_pwndLeft->m_arLineStates->GetAt(i));
 					m_pwndLeft->m_arLineStates->SetAt(i, CDiffData::DIFFSTATE_THEIRSADDED);
-				} // for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++) 
+				}
+				// adjust line numbers
+				for (int i=m_nSelBlockEnd+1; i<GetLineCount(); ++i)
+				{
+					long oldline = (long)m_pwndBottom->m_arLineLines->GetAt(i);
+					if (oldline >= 0)
+						m_pwndBottom->m_arLineLines->SetAt(i, oldline+(index-m_nSelBlockEnd));
+				}
 
 				// now insert an empty block in both yours and theirs
 				m_pwndLeft->m_arDiffLines->InsertAt(m_nSelBlockStart, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndLeft->m_arLineStates->InsertAt(m_nSelBlockStart, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arLineLines->InsertAt(m_nSelBlockStart, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndRight->m_arDiffLines->InsertAt(m_nSelBlockEnd+1, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndRight->m_arLineStates->InsertAt(m_nSelBlockEnd+1, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndRight->m_arLineLines->InsertAt(m_nSelBlockEnd+1, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndBottom->SetModified();
 				m_pwndLeft->SetModified();
 				m_pwndRight->SetModified();
@@ -211,23 +221,31 @@ void CLeftView::OnContextMenu(CPoint point, int /*nLine*/)
 				{
 					m_pwndBottom->m_arDiffLines->SetAt(i, m_pwndLeft->m_arDiffLines->GetAt(i));
 					m_pwndBottom->m_arLineStates->SetAt(i, m_pwndLeft->m_arLineStates->GetAt(i));
-					//m_pwndLeft->m_arLineStates->SetAt(i, CDiffData::DIFFSTATE_THEIRSADDED);
-				} // for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++) 
+				}
 
 				// your block is done, now insert their block
 				int index = m_nSelBlockEnd+1;
 				for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++)
 				{
 					m_pwndBottom->m_arDiffLines->InsertAt(index, m_pwndRight->m_arDiffLines->GetAt(i));
+					m_pwndBottom->m_arLineLines->InsertAt(index, m_pwndLeft->m_arLineLines->GetAt(i));
 					m_pwndBottom->m_arLineStates->InsertAt(index++, m_pwndRight->m_arLineStates->GetAt(i));
-					//m_pwndRight->m_arLineStates->SetAt(i, CDiffData::DIFFSTATE_YOURSADDED);
-				} // for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++) 
+				}
+				// adjust line numbers
+				for (int i=m_nSelBlockEnd+1; i<GetLineCount(); ++i)
+				{
+					long oldline = (long)m_pwndBottom->m_arLineLines->GetAt(i);
+					if (oldline >= 0)
+						m_pwndBottom->m_arLineLines->SetAt(i, oldline+(index-m_nSelBlockEnd));
+				}
 
 				// now insert an empty block in both yours and theirs
-				m_pwndRight->m_arDiffLines->InsertAt(m_nSelBlockStart, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
-				m_pwndRight->m_arLineStates->InsertAt(m_nSelBlockStart, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
-				m_pwndLeft->m_arDiffLines->InsertAt(m_nSelBlockEnd+1, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
-				m_pwndLeft->m_arLineStates->InsertAt(m_nSelBlockEnd+1, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arDiffLines->InsertAt(m_nSelBlockStart, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arLineStates->InsertAt(m_nSelBlockStart, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arLineLines->InsertAt(m_nSelBlockStart, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndRight->m_arDiffLines->InsertAt(m_nSelBlockEnd+1, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndRight->m_arLineStates->InsertAt(m_nSelBlockEnd+1, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndRight->m_arLineLines->InsertAt(m_nSelBlockEnd+1, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndBottom->SetModified();
 				m_pwndLeft->SetModified();
 				m_pwndRight->SetModified();
