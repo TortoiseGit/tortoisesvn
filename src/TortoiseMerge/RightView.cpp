60df16730b772afb86c80d65cce9849194309458
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2006-03-22 11:12:56 +0000

    Add two new context menu entries to use both (left and right) blocks in two way  diff. This implements issue #235.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@6087 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseMerge/RightView.cpp
+++ src/TortoiseMerge/RightView.cpp
@@ -66,6 +66,9 @@ void CRightView::OnContextMenu(CPoint point, int /*nLine*/)
 #define ID_USEFILE 2
 #define ID_USETHEIRANDYOURBLOCK 3
 #define ID_USEYOURANDTHEIRBLOCK 4
+#define ID_USEBOTHTHISFIRST 5
+#define ID_USEBOTHTHISLAST 6
+
 		UINT uEnabled = MF_ENABLED;
 		if ((m_nSelBlockStart == -1)||(m_nSelBlockEnd == -1))
 			uEnabled |= MF_DISABLED | MF_GRAYED;
@@ -92,6 +95,13 @@ void CRightView::OnContextMenu(CPoint point, int /*nLine*/)
 			popup.AppendMenu(MF_STRING | uEnabled, ID_USEYOURANDTHEIRBLOCK, temp);
 			temp.LoadString(IDS_VIEWCONTEXTMENU_USETHEIRANDYOURBLOCK);
 			popup.AppendMenu(MF_STRING | uEnabled, ID_USETHEIRANDYOURBLOCK, temp);
+		}
+		else
+		{
+			temp.LoadString(IDS_VIEWCONTEXTMENU_USEBOTHTHISFIRST);
+			popup.AppendMenu(MF_STRING | uEnabled, ID_USEBOTHTHISFIRST, temp);
+			temp.LoadString(IDS_VIEWCONTEXTMENU_USEBOTHTHISLAST);
+			popup.AppendMenu(MF_STRING | uEnabled, ID_USEBOTHTHISLAST, temp);
 		}
 
 		int cmd = popup.TrackPopupMenu(TPM_RETURNCMD | TPM_LEFTALIGN | TPM_NONOTIFY, point.x, point.y, this, 0);
@@ -256,6 +266,67 @@ void CRightView::OnContextMenu(CPoint point, int /*nLine*/)
 				m_pwndRight->m_arLineStates->InsertAt(m_nSelBlockEnd+1, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndRight->m_arLineLines->InsertAt(m_nSelBlockEnd+1, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndBottom->SetModified();
+				m_pwndLeft->SetModified();
+				m_pwndRight->SetModified();
+			}
+			break;
+		case ID_USEBOTHTHISFIRST:
+			{
+				for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++)
+				{
+					m_pwndRight->m_arLineStates->SetAt(i, CDiffData::DIFFSTATE_YOURSADDED);
+				}
+
+				// your block is done, now insert their block
+				int index = m_nSelBlockEnd+1;
+				for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++)
+				{
+					m_pwndRight->m_arDiffLines->InsertAt(index, m_pwndLeft->m_arDiffLines->GetAt(i));
+					m_pwndRight->m_arLineLines->InsertAt(index, m_pwndRight->m_arLineLines->GetAt(i));
+					m_pwndRight->m_arLineStates->InsertAt(index++, CDiffData::DIFFSTATE_THEIRSADDED);
+				}
+				// adjust line numbers
+				index--;
+				for (int i=m_nSelBlockEnd+1; i<GetLineCount(); ++i)
+				{
+					long oldline = (long)m_pwndRight->m_arLineLines->GetAt(i);
+					if (oldline >= 0)
+						m_pwndRight->m_arLineLines->SetAt(i, oldline+(index-m_nSelBlockEnd));
+				}
+
+				// now insert an empty block in the left view
+				m_pwndLeft->m_arDiffLines->InsertAt(m_nSelBlockEnd+1, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arLineStates->InsertAt(m_nSelBlockEnd+1, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arLineLines->InsertAt(m_nSelBlockEnd+1, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->SetModified();
+				m_pwndRight->SetModified();
+			}
+			break;
+		case ID_USEBOTHTHISLAST:
+			{
+				// get line number from just before the block
+				long linenumber = 0;
+				if (m_nSelBlockStart > 0)
+					linenumber = m_pwndRight->m_arLineLines->GetAt(m_nSelBlockStart-1);
+				linenumber++;
+				for (int i=m_nSelBlockStart; i<=m_nSelBlockEnd; i++)
+				{
+					m_pwndRight->m_arDiffLines->InsertAt(i, m_pwndLeft->m_arDiffLines->GetAt(i));
+					m_pwndRight->m_arLineStates->InsertAt(i, CDiffData::DIFFSTATE_THEIRSADDED);
+					m_pwndRight->m_arLineLines->InsertAt(i, linenumber++);
+				}
+				// adjust line numbers
+				for (int i=m_nSelBlockEnd+1; i<GetLineCount(); ++i)
+				{
+					long oldline = (long)m_pwndRight->m_arLineLines->GetAt(i);
+					if (oldline >= 0)
+						m_pwndRight->m_arLineLines->SetAt(i, oldline+(m_nSelBlockEnd-m_nSelBlockStart)+1);
+				}
+
+				// now insert an empty block in both yours and theirs
+				m_pwndLeft->m_arDiffLines->InsertAt(m_nSelBlockStart, _T(""), m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arLineStates->InsertAt(m_nSelBlockStart, CDiffData::DIFFSTATE_EMPTY, m_nSelBlockEnd-m_nSelBlockStart+1);
+				m_pwndLeft->m_arLineLines->InsertAt(m_nSelBlockStart, (DWORD)-1, m_nSelBlockEnd-m_nSelBlockStart+1);
 				m_pwndLeft->SetModified();
 				m_pwndRight->SetModified();
 			}
