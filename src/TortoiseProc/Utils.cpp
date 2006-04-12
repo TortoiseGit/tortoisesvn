952dd19cd09cf47fb0a5f0a5eebf135301609343
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2006-04-12 20:04:07 +0000

    Patch from Ivan Gerasimov (Ivan dot Gerasimov at transas dot com): Better sorting-by-numbers function for the repobrowser.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@6266 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/Utils.cpp
+++ src/TortoiseProc/Utils.cpp
@@ -909,42 +909,87 @@ CString CUtils::ParsePathInString(const CString& Str)
 	return sToken;
 }
 
-int CUtils::CompareNumerical(LPCTSTR str1, LPCTSTR str2)
+#define IsCharNumeric(C) (!IsCharAlpha(C) && IsCharAlphaNumeric(C))
+
+int CUtils::CompareNumerical(LPCTSTR x_str, LPCTSTR y_str)
 {
-	// returns true, if x < y, false otherwise.
-	// skip same characters
-	while ((*str1 || *str2) && *str1 == *str2)
-	{
-		++str1;
-		++str2;
-	}
+  LPCTSTR num_x_begin = x_str, num_y_begin = y_str;
+  DWORD num_x_cnt = 0, num_y_cnt = 0;
 
-	// parse numeric part of the first arg
-	if (IsCharAlphaNumeric(*str1) && !IsCharAlpha(*str1))
-	{
-		UINT x_num = *str1 - '0';
-		LPCTSTR str1_tmp = str1 + 1;
-		while (IsCharAlphaNumeric(*str1_tmp) && !IsCharAlpha(*str1_tmp))
-		{
-			x_num = 10 * x_num + *str1_tmp - '0';
-			++str1_tmp;
-		}
+  // skip same chars and remember last numeric part of strings
+  while ((*x_str || *y_str) && CompareString(LOCALE_USER_DEFAULT, NORM_IGNOREWIDTH, x_str, 1, y_str, 1) == 2 /* equal */ )
+  {
+    if (IsCharNumeric(*x_str))
+    {
+      ++num_x_cnt;
+      ++num_y_cnt;
+    }
+    else
+    {
+      num_x_begin = CharNext(x_str);
+      num_y_begin = CharNext(y_str);
+      num_x_cnt = 0;
+      num_y_cnt = 0;
+    }
+    x_str = CharNext(x_str);
+    y_str = CharNext(y_str);
+  }
 
-		// parse numeric part of the second arg
-		if (IsCharAlphaNumeric(*str2) && !IsCharAlpha(*str2))
-		{
-			UINT y_num = *str2 - '0';
-			LPCTSTR  str2_tmp = str2 + 1;
-			while (IsCharAlphaNumeric(*str2_tmp) && !IsCharAlpha(*str2_tmp))
-			{
-				y_num = 10 * y_num + *str2_tmp - '0';
-				++str2_tmp;
-			}
+  // parse numeric part of first arg
+  if (num_x_cnt || IsCharNumeric(*x_str))
+  {
+    LPCTSTR x_str_tmp = x_str;
+    while (IsCharNumeric(*x_str_tmp))
+    {
+      ++num_x_cnt;
+      x_str_tmp = CharNext(x_str_tmp);
+    }
 
-			// compare numeric parts of the arguments
-			return x_num - y_num;
-		}
-	}
-	// otherwise, compare literally
-	return _tcsicmp(str1, str2);
+    // parse numeric part of second arg
+    if (num_y_cnt || IsCharNumeric(*y_str))
+    {
+      LPCTSTR y_str_tmp = y_str;
+      while (IsCharNumeric(*y_str_tmp))
+      {
+        ++num_y_cnt;
+        y_str_tmp = CharNext(y_str_tmp);
+      }
+
+      DWORD num_x_cnt_with_zeros = num_x_cnt, num_y_cnt_with_zeros = num_y_cnt;
+
+      while (num_x_cnt < num_y_cnt)
+      {
+        if (CompareString(LOCALE_USER_DEFAULT, NORM_IGNOREWIDTH, num_y_begin, 1, TEXT("0"), 1) != 2 /* not equal to '0' */ )
+          return -1;
+        num_y_begin = CharNext(num_y_begin);
+        --num_y_cnt;
+      }
+
+      while (num_x_cnt > num_y_cnt)
+      {
+        if (CompareString(LOCALE_USER_DEFAULT, NORM_IGNOREWIDTH, num_x_begin, 1, TEXT("0"), 1) != 2 /* not equal to '0' */ )
+          return 1;
+        num_x_begin = CharNext(num_x_begin);
+        --num_x_cnt;
+      }
+
+      // here num_x_cnt == num_y_cnt
+      int cmp_result = CompareString(LOCALE_USER_DEFAULT, NORM_IGNOREWIDTH, num_x_begin, num_x_cnt, num_y_begin, num_y_cnt);
+
+      if (cmp_result == 1 /* less */)
+        return -1;
+      if (cmp_result == 3 /* greater */)
+        return 1;
+      if (num_x_cnt_with_zeros != num_y_cnt_with_zeros)
+        return num_x_cnt_with_zeros < num_y_cnt_with_zeros ? -1 : 1;
+    }
+  }
+
+  // otherwise, compare literally
+  int cmp_result = CompareString(LOCALE_USER_DEFAULT, NORM_IGNOREWIDTH, x_str, -1, y_str, -1);
+  if (cmp_result == 1 /* less */)
+	  return -1;
+  if (cmp_result == 3 /* greater */)
+	  return 1;
+  return 0; /* equal */
 }
