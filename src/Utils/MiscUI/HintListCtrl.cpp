762fafc949ea9cfb1aa52256b5908a811749b2e3
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2006-04-14 14:13:23 +0000

    Add new class CHintListCtrl which provides a CListCtrl with the ability to show a text e.g. while a thread is running.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@6280 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ src/Utils/MiscUI/HintListCtrl.cpp
@@ -0,0 +1,85 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2006 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+
+#include "stdafx.h"
+#include "MemDC.h"
+#include "HintListCtrl.h"
+
+CHintListCtrl::CHintListCtrl() : CListCtrl()
+{
+}
+
+CHintListCtrl::~CHintListCtrl()
+{
+}
+
+void CHintListCtrl::ShowText(const CString& sText)
+{
+	m_sText = sText;
+	Invalidate();
+}
+
+void CHintListCtrl::ClearText()
+{
+	m_sText.Empty();
+	Invalidate();
+}
+
+BEGIN_MESSAGE_MAP(CHintListCtrl, CListCtrl)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+void CHintListCtrl::OnPaint()
+{
+	Default();
+	if (!m_sText.IsEmpty())
+	{
+		COLORREF clrText = ::GetSysColor(COLOR_WINDOWTEXT);
+		COLORREF clrTextBk;
+		if (IsWindowEnabled())
+			clrTextBk = ::GetSysColor(COLOR_WINDOW);
+		else
+			clrTextBk = ::GetSysColor(COLOR_3DFACE);
+
+		CRect rc;
+		GetClientRect(&rc);
+		CHeaderCtrl* pHC;
+		pHC = GetHeaderCtrl();
+		if (pHC != NULL)
+		{
+			CRect rcH;
+			pHC->GetItemRect(0, &rcH);
+			rc.top += rcH.bottom;
+		}
+		CDC* pDC = GetDC();
+		{
+			CMemDC memDC(pDC, &rc);
+
+			memDC.SetTextColor(clrText);
+			memDC.SetBkColor(clrTextBk);
+			memDC.FillSolidRect(rc, clrTextBk);
+			rc.top += 10;
+			CGdiObject * oldfont = memDC.SelectStockObject(ANSI_VAR_FONT);
+			memDC.DrawText(m_sText, rc, DT_CENTER | DT_VCENTER |
+				DT_WORDBREAK | DT_NOPREFIX | DT_NOCLIP);
+			memDC.SelectObject(oldfont);
+		}
+		ReleaseDC(pDC);
+	}
+}
