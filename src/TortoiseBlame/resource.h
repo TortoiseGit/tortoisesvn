8bd004bb07f3dbfdc166f1e43ce34bb8a6df3e4e
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2006-05-13 12:14:30 +0000

    Correct the version and copyright information in our resources.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@6485 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseBlame/resource.h
+++ src/TortoiseBlame/resource.h
@@ -24,7 +24,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_COMMAND_VALUE         32779
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
