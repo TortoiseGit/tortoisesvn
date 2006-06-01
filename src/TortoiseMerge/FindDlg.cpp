cbd5d388af7757cb63241bcb4954e202c8b6250c
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2006-06-01 21:02:42 +0000

    Autsch: add missing files (forgot them when implementing issue #248.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@6681 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ src/TortoiseMerge/FindDlg.cpp
@@ -0,0 +1,80 @@
+// FindDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "TortoiseMerge.h"
+#include "FindDlg.h"
+
+
+// CFindDlg dialog
+
+IMPLEMENT_DYNAMIC(CFindDlg, CDialog)
+
+CFindDlg::CFindDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CFindDlg::IDD, pParent)
+	, m_bTerminating(false)
+	, m_bFindNext(false)
+	, m_bMatchCase(FALSE)
+	, m_bLimitToDiffs(FALSE)
+	, m_sFindText(_T(""))
+{
+}
+
+CFindDlg::~CFindDlg()
+{
+}
+
+void CFindDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Check(pDX, IDC_MATCHCASE, m_bMatchCase);
+	DDX_Check(pDX, IDC_LIMITTODIFFS, m_bLimitToDiffs);
+	DDX_Text(pDX, IDC_SEARCHTEXT, m_sFindText);
+}
+
+
+BEGIN_MESSAGE_MAP(CFindDlg, CDialog)
+	ON_EN_CHANGE(IDC_SEARCHTEXT, &CFindDlg::OnEnChangeSearchtext)
+END_MESSAGE_MAP()
+
+
+// CFindDlg message handlers
+
+void CFindDlg::OnCancel()
+{
+	m_bTerminating = true;
+	if (GetParent())
+		GetParent()->SendMessage(m_FindMsg);
+	DestroyWindow();
+}
+
+void CFindDlg::PostNcDestroy()
+{
+	delete this;
+}
+
+void CFindDlg::OnOK()
+{
+	UpdateData();
+	if (m_sFindText.IsEmpty())
+		return;
+	m_bFindNext = true;
+	if (GetParent())
+		GetParent()->SendMessage(m_FindMsg);
+	m_bFindNext = false;
+}
+
+BOOL CFindDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+	m_FindMsg = RegisterWindowMessage(FINDMSGSTRING);
+
+	GetDlgItem(IDC_SEARCHTEXT)->SetFocus();
+	return FALSE;
+}
+
+void CFindDlg::OnEnChangeSearchtext()
+{
+	UpdateData();
+	GetDlgItem(IDOK)->EnableWindow(!m_sFindText.IsEmpty());
+}
---
+++ src/TortoiseMerge/FindDlg.h
@@ -0,0 +1,38 @@
+#pragma once
+
+
+// CFindDlg dialog
+
+class CFindDlg : public CDialog
+{
+	DECLARE_DYNAMIC(CFindDlg)
+
+public:
+	CFindDlg(CWnd* pParent = NULL);   // standard constructor
+	virtual ~CFindDlg();
+	void Create(CWnd * pParent = NULL) {CDialog::Create(IDD, pParent);ShowWindow(SW_SHOW);UpdateWindow();}
+	bool IsTerminating() {return m_bTerminating;}
+	bool FindNext() {return m_bFindNext;}
+	bool MatchCase() {return !!m_bMatchCase;}
+	bool LimitToDiffs() {return !!m_bLimitToDiffs;}
+	CString GetFindString() {return m_sFindText;}
+// Dialog Data
+	enum { IDD = IDD_FIND };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+	virtual void OnCancel();
+	virtual void PostNcDestroy();
+	virtual void OnOK();
+	virtual BOOL OnInitDialog();
+	afx_msg void OnEnChangeSearchtext();
+private:
+	UINT		m_FindMsg;
+	bool		m_bTerminating;
+	bool		m_bFindNext;
+	BOOL		m_bMatchCase;
+	BOOL		m_bLimitToDiffs;
+	CString		m_sFindText;
+};
