bc3b3da16b24b3226025e65c2163b42059f582eb
luebbe.tortoisesvn <luebbe.tortoisesvn@154be670-7969-47af-9ab0-c6fb378d2295>
2006-09-27 07:55:28 +0000

    Translation status restructuring: - Move php scripts into drupal folder - Fix copy&paste bug in trans_countries.inc
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@7634 154be670-7969-47af-9ab0-c6fb378d2295

--- contrib/drupal-modules/translation-status/genstats.sh
+++ contrib/drupal-modules/translation-status/genstats.sh
@@ -5,7 +5,7 @@
 
 rm trans_data.inc
 
-# Create doc statistics
+# Create doc statistics. Run cleanup on the external first to be safe
 (cd work/doc; svn cleanup > /dev/null 2>&1; svn up > /dev/null 2>&1)
 ./get-doc-stats.sh >>trans_data.inc
 
