662e929c3aec6389e52ea740cf0d9db203a2fef0
luebbe.tortoisesvn <luebbe.tortoisesvn@154be670-7969-47af-9ab0-c6fb378d2295>
2006-12-14 15:32:33 +0000

    Fixed missing flag image path
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@8326 154be670-7969-47af-9ab0-c6fb378d2295

--- contrib/drupal-modules/module/download.php
+++ contrib/drupal-modules/module/download.php
@@ -46,25 +46,26 @@ function print_langpack($i, $postat, $v, $w)
   $infobits=$postat[1];
   $lang_cc=$postat[2];
   $lang_name=$postat[3];
+  $flagimg=$v['flagpath'].$lang_cc.".png";
   $flagtag="<img src=\"$flagimg\" height=\"12\" width=\"18\" alt=\"$lang_name flag\"/>";
-
+  
   $dlfile32=get_langpack($lang_cc, 'Setup', $v, $w['w32']);
   $dlfile64=get_langpack($lang_cc, 'Setup', $v, $w['x64']);
   if ( ($infobits & "01") <> "0")   {
-    $t_ln="SpellChecker_".$lang_cc.".exe";
-    $dlfilechecker="<a href=\"".$v['url1'].$t_ln.$v['url2']."\">Spellchecker</a>";
-    } else {
-    $dlfilechecker="";
+   $t_ln="SpellChecker_".$lang_cc.".exe";
+   $dlfilechecker="<a href=\"".$v['url1'].$t_ln.$v['url2']."\">Spellchecker</a>";
+	} else {
+   $dlfilechecker="";
   }
   
   if ( ($infobits & "10") <> "0")   {
-    $t_ts="TortoiseSVN-".$v['release'].'-'.$lang_cc.".pdf";
-    $t_tm="TortoiseMerge-".$v['release'].'-'.$lang_cc.".pdf";
-    $dlmanTSVN="<a href=\"".$v['url1'].$t_ts.$v['url2']."\">TSVN</a>";
-    $dlmanTMerge="<a href=\"".$v['url1'].$t_tm.$v['url2']."\">TMerge</a>";
+   $t_ts="TortoiseSVN-".$v['release'].'-'.$lang_cc.".pdf";
+   $t_tm="TortoiseMerge-".$v['release'].'-'.$lang_cc.".pdf";
+   $dlmanTSVN="<a href=\"".$v['url1'].$t_ts.$v['url2']."\">TSVN</a>";
+   $dlmanTMerge="<a href=\"".$v['url1'].$t_tm.$v['url2']."\">TMerge</a>";
   } else {
-    $dlmanTSVN="";
-    $dlmanTMerge="";
+   $dlmanTSVN="";
+   $dlmanTMerge="";
   }
 
   echo "<tr class=\"ok\">";
