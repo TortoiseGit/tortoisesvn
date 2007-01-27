94a994cb72b11d9964dc5b69f9eb17bc781a87d5
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2007-01-27 08:17:01 +0000

    Sort the entries in the log dialog by revision *descending* before showing the statistics dialog.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@8577 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/LogDlg.cpp
+++ src/TortoiseProc/LogDlg.cpp
@@ -1641,7 +1641,7 @@ void CLogDlg::OnBnClickedStatbutton()
 	if (m_arShownList.IsEmpty())
 		return;		// nothing is shown, so no statistics.
 	// the statistics dialog expects the log entries to be sorted by revision
-	SortByColumn(0, true);
+	SortByColumn(0, false);
 	CPtrArray shownlist;
 	RecalculateShownList(&shownlist);
 	// create arrays which are aware of the current filter
