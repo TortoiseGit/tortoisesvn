e6c590b10c28c2734076317b15f97f23eba72348
schaefer <schaefer@154be670-7969-47af-9ab0-c6fb378d2295>
2007-02-18 19:10:31 +0000

    Revert "d:\Development\SVN\issue_334" back to "d:\Development\SVN\TortoiseSVN" which revision 8723 had changed when merging the branch back to trunk.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@8731 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/resource.h
+++ src/TortoiseProc/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by d:\Development\SVN\issue_334\src\Resources\TortoiseProcENG.rc
+// Used by d:\Development\SVN\TortoiseSVN\src\Resources\TortoiseProcENG.rc
 //
 #define IDR_MAINFRAME                   1
 #define IDD_SETTINGSPROXY               102
