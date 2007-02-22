16f5a3eccefd8dc57b5f5c03263569a53ce8b14a
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2007-02-22 20:14:10 +0000

    Add file to version control which was forgotten in revision 8759.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@8775 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ src/Utils/MiscUI/StandAloneDlg.cpp
@@ -0,0 +1,100 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2007 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+#include "stdafx.h"
+#include "Resource.h"
+#include "StandAloneDlg.h"
+
+IMPLEMENT_DYNAMIC(CResizableStandAloneDialog, CStandAloneDialogTmpl<CResizableDialog>)
+CResizableStandAloneDialog::CResizableStandAloneDialog(UINT nIDTemplate, CWnd* pParentWnd /*= NULL*/)
+	: CStandAloneDialogTmpl<CResizableDialog>(nIDTemplate, pParentWnd)
+	, m_bVertical(false)
+	, m_bHorizontal(false)
+{
+}
+
+BEGIN_MESSAGE_MAP(CResizableStandAloneDialog, CStandAloneDialogTmpl<CResizableDialog>)
+	ON_WM_SIZE()
+	ON_WM_NCMBUTTONUP()
+	ON_WM_NCRBUTTONUP()
+END_MESSAGE_MAP()
+
+void CResizableStandAloneDialog::OnSize(UINT nType, int cx, int cy)
+{
+	CStandAloneDialogTmpl<CResizableDialog>::OnSize(nType, cx, cy);
+	if (nType == SIZE_RESTORED)
+	{
+		m_bVertical = m_bHorizontal = false;
+	}
+}
+
+void CResizableStandAloneDialog::OnNcMButtonUp(UINT nHitTest, CPoint point) 
+{
+	WINDOWPLACEMENT windowPlacement;
+	if ((nHitTest == HTMAXBUTTON) && GetWindowPlacement(&windowPlacement) && windowPlacement.showCmd == SW_SHOWNORMAL)
+	{
+		CRect rcWorkArea, rcWindowRect;
+		GetWindowRect(&rcWindowRect);
+		if (m_bVertical)
+		{
+			rcWindowRect.top = m_rcOrgWindowRect.top;
+			rcWindowRect.bottom = m_rcOrgWindowRect.bottom;
+		}
+		else if (SystemParametersInfo(SPI_GETWORKAREA, 0U, &rcWorkArea, 0U))
+		{
+			m_rcOrgWindowRect.top = rcWindowRect.top;
+			m_rcOrgWindowRect.bottom = rcWindowRect.bottom;
+			rcWindowRect.top = rcWorkArea.top;
+			rcWindowRect.bottom = rcWorkArea.bottom;
+		}
+		bool bVertical = !m_bVertical;
+		bool bHorizontal = m_bHorizontal;
+		MoveWindow(&rcWindowRect);
+		m_bVertical = bVertical;
+		m_bHorizontal = bHorizontal;
+	}
+	CStandAloneDialogTmpl<CResizableDialog>::OnNcMButtonUp(nHitTest, point);
+}
+
+void CResizableStandAloneDialog::OnNcRButtonUp(UINT nHitTest, CPoint point) 
+{
+	WINDOWPLACEMENT windowPlacement;
+	if ((nHitTest == HTMAXBUTTON) && GetWindowPlacement(&windowPlacement) && windowPlacement.showCmd == SW_SHOWNORMAL)
+	{
+		CRect rcWorkArea, rcWindowRect;
+		GetWindowRect(&rcWindowRect);
+		if (m_bHorizontal)
+		{
+			rcWindowRect.left = m_rcOrgWindowRect.left;
+			rcWindowRect.right = m_rcOrgWindowRect.right;
+		}
+		else if (SystemParametersInfo(SPI_GETWORKAREA, 0U, &rcWorkArea, 0U))
+		{
+			m_rcOrgWindowRect.left = rcWindowRect.left;
+			m_rcOrgWindowRect.right = rcWindowRect.right;
+			rcWindowRect.left = rcWorkArea.left;
+			rcWindowRect.right = rcWorkArea.right;
+		}
+		bool bVertical = m_bVertical;
+		bool bHorizontal = !m_bHorizontal;
+		MoveWindow(&rcWindowRect);
+		m_bVertical = bVertical;
+		m_bHorizontal = bHorizontal;
+	}
+	CStandAloneDialogTmpl<CResizableDialog>::OnNcRButtonUp(nHitTest, point);
+}
