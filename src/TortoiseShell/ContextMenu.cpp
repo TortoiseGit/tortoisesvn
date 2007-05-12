3f1cb33b3b5d9bad11a44cc1c75a43400eff2499
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2007-05-12 09:37:27 +0000

    bail out if the command verb isn't found.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@9377 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseShell/ContextMenu.cpp
+++ src/TortoiseShell/ContextMenu.cpp
@@ -1058,6 +1058,8 @@ STDMETHODIMP CShellExt::InvokeCommand(LPCMINVOKECOMMANDINFO lpcmi)
 			std::map<stdstring, UINT_PTR>::const_iterator verb_it = myVerbsMap.lower_bound(verb);
 			if (verb_it != myVerbsMap.end() && verb_it->first == verb)
 				idCmd = verb_it->second;
+			else
+				return hr;
 		}
 
 		// See if we have a handler interface for this id
