080336121c2642416500a796b5c1f1075830acec
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2007-05-27 08:21:37 +0000

    Only initialize the SASL plugins path if the class is compiled with MFC.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@9570 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNStatus.cpp
+++ src/SVN/SVNStatus.cpp
@@ -71,16 +71,16 @@ SVNStatus::SVNStatus(bool * pbCanceled)
 			APR_HASH_KEY_STRING);
 		svn_config_set(cfg, SVN_CONFIG_SECTION_TUNNELS, "ssh", CUnicodeUtils::GetUTF8(tsvn_ssh));
 	}
+	// to avoid that SASL will look for and load its plugin dlls all around the
+	// system, we set the path here.
+	// Note that SASL doesn't have to be initialized yet for this to work
+	sasl_set_path(SASL_PATH_TYPE_PLUGIN, (LPSTR)(LPCSTR)CUnicodeUtils::GetUTF8(CPathUtils::GetAppDirectory()));
 #else
 	svn_config_ensure(NULL, m_pool);
 
 	// set up the configuration
 	m_err = svn_config_get_config (&(ctx->config), g_pConfigDir, m_pool);
 
-	// to avoid that SASL will look for and load its plugin dlls all around the
-	// system, we set the path here.
-	// Note that SASL doesn't have to be initialized yet for this to work
-	sasl_set_path(SASL_PATH_TYPE_PLUGIN, (LPSTR)(LPCSTR)CUnicodeUtils::GetUTF8(CPathUtils::GetAppDirectory()));
 #endif
 }
 
