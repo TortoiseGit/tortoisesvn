86b1bfc2c5cf30b6b6f60c212f188236f625ba86
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2007-06-21 20:12:47 +0000

    Update test script
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@9846 154be670-7969-47af-9ab0-c6fb378d2295

--- test/BuildRevisionGraphTestRepo.bat
+++ test/BuildRevisionGraphTestRepo.bat
@@ -86,68 +86,54 @@ echo testline9 > file1
 svn ci -m ""
 :: revision 16
 svn cp %REPOROOT%/trunk -r10 %REPOROOT%/tags/V2 -m "Version 2"
-
 :: revision 17
 
 svn cp %REPOROOT%/trunk -r9 %REPOROOT%/tags/V2 -m "Version 1.5"
-
 :: revision 18
 
 svn rm %REPOROOT%/tags/V2/trunk -m ""
-
 :: revision 19
 
 svn cp %REPOROOT%/trunk -r9 %REPOROOT%/tags/V1.5 -m "Version 1.5"
-
 :: revision 20
 
 svn cp %REPOROOT%/trunk %REPOROOT%/branches/newtest -m "new test"
-
 :: revision 21
 
 svn rm %REPOROOT%/branches/newtest -m "remove wrong branch"
-
 :: revision 22
 
 svn cp %REPOROOT%/trunk -r16 %REPOROOT%/branches/newtest -m "new test"
-
 :: revision 23
 
 svn mkdir Ordner2
 svn ci -m "new folder"
-
 :: revision 24
 
 svn rm Ordner
 svn ci -m "remove folder"
-
 :: revision 25
 
 svn mv Ordner2 Ordner
 svn ci -m "move folder"
-
 :: revision 26
 
 echo testline > Ordner\file1
 svn add Ordner\file1
 svn ci -m "add file"
-
 :: revision 27
 
 echo testline1 > Ordner\file1
 svn ci -m "mod"
-
 :: revision 28
 
 svn up
 svn rm Ordner
 svn ci -m ""
-
 :: revision 29
 
 svn cp %REPOROOT%/trunk/Ordner -r24 Ordner
 svn ci -m "restore original"
-
 :: revision 30
 
 svn cp Ordner Ordner2
@@ -156,7 +142,6 @@ svn cp Ordner Ordner4
 svn mv file1 Ordner3/mvfile
 svn up
 svn ci -m "mess up"
-
 :: revision 31
 
 svn cp Ordner2 Ordner5
@@ -168,83 +153,70 @@ svn cp Ordner2 Ordner10
 svn cp Ordner folder1
 svn cp Ordner folder2
 svn ci -m ""
-
 :: revision 32
 
 svn mv Ordner testOrdner
 svn mv Ordner2 Ordner22
 svn ci -m ""
-
 :: revision 33
 
 svn mv testOrdner Ordner
 svn ci -m ""
-
 :: revision 34
 
 svn rm Ordner\file1
 echo replacing > Ordner\file1
 svn add Ordner\file1
 svn ci -m "replacing file"
-
 :: revision 35
 
 echo modified > Ordner\file1
 svn ci -m ""
-
 :: revision 36
 
 svn cp %REPOROOT%/trunk %REPOROOT%/branches/5.2.x -m ""
 cd ..
 svn up
-
 :: revision 37
 
 echo ttt > branches\5.2.x\file2
 svn ci -m ""
-
 :: revision 38
 
 svn cp %REPOROOT%/branches/5.2.x %REPOROOT%/tags/5.2.2 -m ""
-
 :: revision 39
 
 echo ttt > tags\V1.5\file2
 svn ci -m ""
-
 :: revision 40
 
 echo tt > branches\5.2.x\file2
 svn ci -m ""
-
 :: revision 41
 
 svn cp %REPOROOT%/branches/5.2.x %REPOROOT%/tags/5.2.2 -r37 -m ""
-
 :: revision 42
 
 svn rm %REPOROOT%/tags/5.2.2 -m ""
-
 :: revision 43
 
 svn cp %REPOROOT%/branches/5.2.x %REPOROOT%/tags/5.2.2 -m ""
-
 :: revision 44
 
 svn rm %REPOROOT%/tags/5.2.2 -m ""
-
 :: revision 45
 
 svn cp %REPOROOT%/branches/5.2.x %REPOROOT%/tags/5.2.2 -m ""
-
 :: revision 46
 
 echo test > trunk\file2
 svn ci -m ""
 :: revision 47
+
 echo test2 > trunk\file2
 svn ci -m ""
 :: revision 48
+
 echo test3 > trunk\file2
 svn ci -m ""
 :: revision 49
@@ -295,5 +267,44 @@ svn rm trunk\file1_ab
 svn rm trunk\file1_cd
 svn ci -m ""
 :: revision 60
+
+svn cp trunk original
+svn ci -m ""
+:: revision 61
+
+svn mkdir original2
+svn ci -m ""
+:: revision 62
+
+svn mv original original2
+svn up
+svn ci -m ""
+:: revision 63
+
+svn mv original2\original\Ordner trunk\Ord
+svn up
+svn ci -m ""
+:: revision 64
+
+echo modified2 > trunk\Ord\file1
+svn ci -m ""
+:: revision 65
+
+svn mv %REPOROOT%/tags/5.2.2 %REPOROOT%/tags/05.2.2 -m ""
+:: revision 66
+
+svn cp %REPOROOT%/trunk %REPOROOT%/tags/5.2.3 -m ""
+:: revision 67
+
+svn rm %REPOROOT%/tags/5.2.3 -m ""
+:: revision 68
+
+svn up
+echo modified > trunk\Ord\file1
+svn ci -m ""
+:: revision 69
+
+svn cp %REPOROOT%/trunk %REPOROOT%/tags/5.2.3 -m ""
+:: revision 70
 
 cd ..
