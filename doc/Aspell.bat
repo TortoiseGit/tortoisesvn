4a9f4ccd59564fb207a8e92820b7b1cad0d756c7
luebbe.tortoisesvn <luebbe.tortoisesvn@154be670-7969-47af-9ab0-c6fb378d2295>
2007-07-09 09:47:53 +0000

    Fix Aspell log output so it goes into $app$language.log
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@10033 154be670-7969-47af-9ab0-c6fb378d2295

--- doc/Aspell.bat
+++ doc/Aspell.bat
@@ -1,3 +1,8 @@
 @echo off
-echo --- %3 >> ASpell\spellcheck_%2.log
-%1 --mode=sgml --encoding=utf-8 --add-extra-dicts=./ASpell/%2.pws --add-extra-dicts=./ASpell/Temp.pws --lang=%2 list check < %3 >> ASpell\spellcheck_%2.log
+rem parameters
+rem %1 = path to spellcheck executable
+rem %2 = language
+rem %3 = name of the file to check
+rem %4 = name of the logfile
+echo --- %3 >> %4
+%1 --mode=sgml --encoding=utf-8 --add-extra-dicts=./ASpell/%2.pws --add-extra-dicts=./ASpell/Temp.pws --lang=%2 list check < %3 >> %4
