85d7eee8fbd028b6add135743d832bb83bc721cc
simonlarge <simonlarge@154be670-7969-47af-9ab0-c6fb378d2295>
2007-07-20 23:41:01 +0000

    Update the test repo generator: * Speed up by combining commits * Add files and then update to prev so we see remote changes in CfM * Lock a file * Change a property * Disable auto-props in most cases
    Update screenshot instructions accordingly, and add a few new ones.
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@10189 154be670-7969-47af-9ab0-c6fb378d2295

--- doc/test/MakeTest.bat
+++ doc/test/MakeTest.bat
@@ -25,18 +25,34 @@ svn co -q file:///%docpath%/test/temp/repos2 ext
 cd ext
 :: Copy some files from the docs to create content
 copy ..\..\..\source\en\TortoiseSVN\tsvn_server\server*.xml > nul
-@svn add server*.xml
-@svn ci -q -m "Document the server" .
+svn add server*.xml
+svn ci -q -m "Document the server" .
 cd ..
 rd /s/q ext
 
 cd doc
 :: Copy some files from the docs to create content
-for %%f in (add blame checkout commit export ignore relocate revert showlog) do (
+type nul > ..\targets
+for %%f in (add blame checkout) do (
 	@copy ..\..\..\source\en\TortoiseSVN\tsvn_dug\dug_%%f.xml+..\..\footnote1.txt dug_%%f.xml> nul
-	@svn add dug_%%f.xml
-	@svn ci -q -m "Document the %%f command" .
+	@echo dug_%%f.xml >> ..\targets
 )
+svn add --no-auto-props --targets ..\targets
+svn ci -q -m "Document some commands" .
+type nul > ..\targets
+for %%f in (commit export ignore) do (
+	@copy ..\..\..\source\en\TortoiseSVN\tsvn_dug\dug_%%f.xml+..\..\footnote1.txt dug_%%f.xml> nul
+	@echo dug_%%f.xml >> ..\targets
+)
+svn add --no-auto-props --targets ..\targets
+svn ci -q -m "Document commands group 2" .
+type nul > ..\targets
+for %%f in (relocate revert showlog) do (
+	@copy ..\..\..\source\en\TortoiseSVN\tsvn_dug\dug_%%f.xml+..\..\footnote1.txt dug_%%f.xml> nul
+	@echo dug_%%f.xml >> ..\targets
+)
+svn add --targets ..\targets
+svn ci -q -m "Document some more commands" .
 
 :: Add a reference to the external repository
 svn ps -q svn:externals "ext file:///%docpath%/test/temp/repos2/" .
@@ -44,9 +60,10 @@ svn ci -q -m "create externals reference" .
 svn up -q
 
 copy ..\..\subwcrev1.txt subwcrev.txt > nul
-svn add subwcrev.txt
+svn add --no-auto-props subwcrev.txt
 svn ci -q -m "Document the SubWCRev program" .
 svn up -q ../docs
+
 :: Now make some changes that will cause conflicts later.
 :: Change detection is broken when timestamps are the same.
 :: Force a current timestamp by using type instead of copy.
@@ -55,19 +72,45 @@ for %%f in (ignore showlog) do (
 	@copy ..\..\..\source\en\TortoiseSVN\tsvn_dug\dug_%%f.xml+..\..\footnote2.txt dug_%%f.xml> nul
 )
 svn ci -q -m "Clarify the description of SubWCRev" .
+
+:: Add yet more files
+type nul > ..\targets
+for %%f in (branchtag conflicts general) do (
+	@copy ..\..\..\source\en\TortoiseSVN\tsvn_dug\dug_%%f.xml > nul
+	@echo dug_%%f.xml >> ..\targets
+)
+type ..\..\footnote1.txt >> dug_ignore.xml
+svn add --no-auto-props --targets ..\targets
+svn ci -q -m "Document final commands" .
+
+:: Update to just before the latest commit
+svn up -q -r6
+
+:: Modify some files
+for %%f in (add blame relocate) do (
+	@copy ..\..\..\source\en\TortoiseSVN\tsvn_dug\dug_%%f.xml+..\..\footnote2.txt dug_%%f.xml> nul
+)
 :: Add an unversioned file
 echo Read Me > readme.txt
+:: Add a nested WC
+md nested
+svn co -q file:///%docpath%/test/temp/repos2 nested
+:: Change a file property
+svn ps -q local:property "This is a local property" dug_revert.xml
+:: Lock a file
+svn lock -m "Major update" dug_add.xml > NUL
 
 cd ..\docs
 :: Make changes which conflict with what we did earlier.
 copy /y ..\..\subwcrev3.txt subwcrev.txt > nul
-for %%f in (blame commit export ignore showlog) do (
+for %%f in (checkout commit export ignore showlog) do (
 	@copy ..\..\..\source\en\TortoiseSVN\tsvn_dug\dug_%%f.xml+..\..\footnote3.txt dug_%%f.xml> nul
 )
 svn diff . > ..\docs.patch
 svn up -q
 
 cd ..
+del targets
 goto end
 :checkout_fail
 echo Perhaps you have not set the correct unix-style path in DocPath.txt
