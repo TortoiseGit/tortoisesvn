7b724c05525f9ef5811f4aa2166382a86698ac6c
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2007-07-25 17:00:51 +0000

    Only try the property diff on folders and then return. Don't try the content diff on folders.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@10232 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNDiff.cpp
+++ src/SVN/SVNDiff.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2006 - Stefan Kueng
+// Copyright (C) 2003-2007 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -73,6 +73,8 @@ bool SVNDiff::DiffWCFile(const CTSVNPath& filePath,
 	{
 		DiffProps(filePath, SVNRev::REV_HEAD, SVNRev::REV_WC);
 	}
+	if (filePath.IsDirectory())
+		return true;
 
 	if (text_status > svn_wc_status_normal)
 		basePath = SVN::GetPristinePath(filePath);
