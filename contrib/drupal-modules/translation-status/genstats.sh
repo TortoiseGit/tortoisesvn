1b60f21e0abbd86f26fee1245657911f956cc6c6
luebbe.tortoisesvn <luebbe.tortoisesvn@154be670-7969-47af-9ab0-c6fb378d2295>
2007-07-26 11:10:54 +0000

    Translation status needs an 'svn up' on the root
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@10242 154be670-7969-47af-9ab0-c6fb378d2295

--- contrib/drupal-modules/translation-status/genstats.sh
+++ contrib/drupal-modules/translation-status/genstats.sh
@@ -3,22 +3,24 @@
 # Create the doc and gui statistics
 #
 
-# Create statistics for trunk
-# Create doc statistics. Run cleanup on the external first to be safe
-(cd trunk/doc; svn cleanup > /dev/null 2>&1; svn up > /dev/null 2>&1)
+# First 'svn cleanup' the externals manually, because cleanup doesn't walk into externals
+# Afterwards 'svn up' everything
+(cd trunk/gui; svn cleanup > /dev/null 2>&1)
+(cd trunk/doc; svn cleanup > /dev/null 2>&1)
+(cd branch/doc; svn cleanup > /dev/null 2>&1)
+(cd branch/gui; svn cleanup > /dev/null 2>&1)
+(svn cleanup > /dev/null 2>&1; svn up > /dev/null 2>&1)
+
+# Create doc statistics for trunk
 ./get-doc-stats.sh trunk >trans_data_trunk.inc
 
 # Create gui statistics
-(cd trunk/gui; svn cleanup > /dev/null 2>&1; svn up > /dev/null 2>&1)
 ./get-gui-stats.sh trunk >>trans_data_trunk.inc
 
-# Create statistics for branch
-# Create doc statistics. Run cleanup on the external first to be safe
-(cd branch/doc; svn cleanup > /dev/null 2>&1; svn up > /dev/null 2>&1)
+# Create doc statistics for branch
 ./get-doc-stats.sh branch >trans_data_branch.inc
 
 # Create gui statistics
-(cd branch/gui; svn cleanup > /dev/null 2>&1; svn up > /dev/null 2>&1)
 ./get-gui-stats.sh branch >>trans_data_branch.inc
 
 # Copy results into web space
