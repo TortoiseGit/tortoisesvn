9c398cdfbe3266503c075e5c488798e5efe3822e
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2007-08-21 19:31:59 +0000

    New custom control: a filter edit control, with a custom info button on the left side indicating e.g. a search icon, and a cancel button on the right side to clear the filter text.
    This is the first step for implementing issue #379.
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@10389 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ src/Utils/MiscUI/FilterEdit.cpp
@@ -0,0 +1,279 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2007 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "stdafx.h"
+#include "FilterEdit.h"
+
+IMPLEMENT_DYNAMIC(CFilterEdit, CEdit)
+
+CFilterEdit::CFilterEdit() : m_hIconCancelNormal(NULL)
+	, m_hIconCancelPressed(NULL)
+	, m_hIconInfo(NULL)
+	, m_bPressed(FALSE)
+	, m_bShowCancelButtonAlways(FALSE)
+	, m_iButtonClickedMessageId(WM_FILTEREDIT_INFOCLICKED)
+	, m_iCancelClickedMessageId(WM_FILTEREDIT_CANCELCLICKED)
+{
+	m_rcEditArea.SetRect(0, 0, 0, 0);
+	m_rcButtonArea.SetRect(0, 0, 0, 0);
+	m_rcInfoArea.SetRect(0, 0, 0, 0);
+	m_sizeInfoIcon.SetSize(0, 0);
+	m_sizeCancelIcon.SetSize(0, 0);
+}
+
+CFilterEdit::~CFilterEdit()
+{
+	if (m_hIconCancelNormal)
+		DestroyIcon(m_hIconCancelNormal);
+	if (m_hIconCancelPressed)
+		DestroyIcon(m_hIconCancelPressed);
+	if (m_hIconInfo)
+		DestroyIcon(m_hIconInfo);
+}
+
+BEGIN_MESSAGE_MAP(CFilterEdit, CEdit)
+
+	ON_MESSAGE(WM_SETFONT, OnSetFont)
+	ON_WM_SIZE()
+	ON_WM_ERASEBKGND()
+	ON_WM_CHAR()
+	ON_WM_KEYDOWN()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_CREATE()
+	ON_WM_SETCURSOR()
+END_MESSAGE_MAP()
+
+
+
+// CFilterEdit message handlers
+
+void CFilterEdit::PreSubclassWindow( )
+{	
+	// We must have a multi line edit
+	// to be able to set the edit rect
+	ASSERT( GetStyle() & ES_MULTILINE );
+
+	ResizeWindow();
+}
+
+BOOL CFilterEdit::PreTranslateMessage( MSG* pMsg )
+{
+	return CEdit::PreTranslateMessage(pMsg);
+}
+
+BOOL CFilterEdit::SetCancelBitmaps(UINT uCancelNormal, UINT uCancelPressed, BOOL bShowAlways)
+{
+	m_bShowCancelButtonAlways = bShowAlways;
+
+	if (m_hIconCancelNormal)
+		DestroyIcon(m_hIconCancelNormal);
+	if (m_hIconCancelPressed)
+		DestroyIcon(m_hIconCancelPressed);
+
+	m_hIconCancelNormal = (HICON)LoadImage(AfxGetResourceHandle(), MAKEINTRESOURCE(uCancelNormal), IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
+	m_hIconCancelPressed = (HICON)LoadImage(AfxGetResourceHandle(), MAKEINTRESOURCE(uCancelPressed), IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
+
+	if ((m_hIconCancelNormal == 0) || (m_hIconCancelPressed == 0))
+		return FALSE;
+
+	m_sizeCancelIcon = GetIconSize(m_hIconCancelNormal);
+
+	ResizeWindow();
+	return TRUE;
+}
+
+BOOL CFilterEdit::SetInfoIcon(UINT uInfo)
+{
+	if (m_hIconInfo)
+		DestroyIcon(m_hIconInfo);
+
+	m_hIconInfo = (HICON)LoadImage(AfxGetResourceHandle(), MAKEINTRESOURCE(uInfo), IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
+
+	if (m_hIconInfo == 0)
+		return FALSE;
+
+	m_sizeInfoIcon = GetIconSize(m_hIconInfo);
+
+	ResizeWindow();
+	return TRUE;
+}
+
+void CFilterEdit::ResizeWindow()
+{
+	if (!::IsWindow(m_hWnd)) 
+		return;
+
+	RECT editrc, rc;
+	GetRect(&editrc);
+	GetClientRect(&rc);
+	editrc.left = rc.left + 4;
+	editrc.top = rc.top + 1;
+	editrc.right = rc.right - 4;
+	editrc.bottom = rc.bottom - 4;
+
+	m_rcEditArea.left = editrc.left + m_sizeInfoIcon.cx;
+	m_rcEditArea.right = editrc.right - m_sizeCancelIcon.cx - 5;
+	m_rcEditArea.top = editrc.top;
+	m_rcEditArea.bottom = editrc.bottom;
+
+	m_rcButtonArea.left = m_rcEditArea.right + 5;
+	m_rcButtonArea.right = rc.right;
+	m_rcButtonArea.top = (((rc.bottom)-m_sizeCancelIcon.cy)/2);
+	m_rcButtonArea.bottom = m_rcButtonArea.top + m_sizeCancelIcon.cy;
+
+	m_rcInfoArea.left = 0;
+	m_rcInfoArea.right = m_rcEditArea.left;
+	m_rcInfoArea.top = (((rc.bottom)-m_sizeInfoIcon.cy)/2);
+	m_rcInfoArea.bottom = m_rcInfoArea.top + m_sizeInfoIcon.cy;
+
+	SetRect(&m_rcEditArea);
+}
+
+void CFilterEdit::SetButtonClickedMessageId(UINT iButtonClickedMessageId, UINT iCancelClickedMessageId)
+{
+	m_iButtonClickedMessageId = iButtonClickedMessageId;
+	m_iCancelClickedMessageId = iCancelClickedMessageId;
+}
+
+CSize CFilterEdit::GetIconSize(HICON hIcon)
+{
+	CSize size(0, 0);
+	ICONINFO iconinfo;
+	if (GetIconInfo(hIcon, &iconinfo))
+	{
+		BITMAP bmp;
+		if (GetObject(iconinfo.hbmColor, sizeof(BITMAP), &bmp))
+		{
+			size.cx = bmp.bmWidth;
+			size.cy = bmp.bmHeight;
+		}
+	}
+	return size;
+}
+
+BOOL CFilterEdit::OnEraseBkgnd(CDC* pDC)
+{
+	RECT rc;
+	GetClientRect(&rc);
+	pDC->FillSolidRect(&rc, RGB(255,255,255));
+
+	if (GetWindowTextLength() || m_bShowCancelButtonAlways)
+	{
+		if (!m_bPressed)
+		{
+			DrawIconEx(pDC->GetSafeHdc(), m_rcButtonArea.left, m_rcButtonArea.top, m_hIconCancelNormal, 
+				m_sizeCancelIcon.cx, m_sizeCancelIcon.cy, 0, NULL, DI_NORMAL);
+		}
+		else
+		{
+			DrawIconEx(pDC->GetSafeHdc(), m_rcButtonArea.left, m_rcButtonArea.top, m_hIconCancelPressed, 
+				m_sizeCancelIcon.cx, m_sizeCancelIcon.cy, 0, NULL, DI_NORMAL);
+		}
+	}
+	if (m_hIconInfo)
+	{
+		DrawIconEx(pDC->GetSafeHdc(), m_rcInfoArea.left, m_rcInfoArea.top, m_hIconInfo, 
+			m_sizeInfoIcon.cx, m_sizeInfoIcon.cy, 0, NULL, DI_NORMAL);
+	}
+
+	return TRUE;
+}
+
+void CFilterEdit::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	// this will draw the background again
+	// so that the button will be drawn if the text exists
+	InvalidateRect(NULL);
+
+	CEdit::OnKeyDown(nChar, nRepCnt, nFlags);
+}
+
+void CFilterEdit::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	m_bPressed = FALSE;
+	InvalidateRect(NULL);
+	if (m_rcButtonArea.PtInRect(point))
+	{
+		SetWindowText(_T(""));
+		CWnd *pOwner = GetOwner();
+		if (pOwner)
+		{
+			pOwner->SendMessage(m_iCancelClickedMessageId, 0, 0);
+		}
+	}
+	if (m_rcInfoArea.PtInRect(point))
+	{
+		CWnd *pOwner = GetOwner();
+		if (pOwner)
+		{
+			RECT rc = m_rcInfoArea;
+			ClientToScreen(&rc);
+			pOwner->SendMessage(m_iButtonClickedMessageId, 0, (LPARAM)(LPRECT)&rc);
+		}
+	}
+
+	CEdit::OnLButtonUp(nFlags, point);
+}
+
+void CFilterEdit::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	m_bPressed = m_rcButtonArea.PtInRect(point);
+	InvalidateRect(NULL);
+	CEdit::OnLButtonDown(nFlags, point);
+}
+
+int CFilterEdit::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CEdit::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	ResizeWindow();
+
+	return 0;
+}
+
+LRESULT CFilterEdit::OnSetFont( WPARAM wParam, LPARAM lParam )
+{
+	DefWindowProc( WM_SETFONT, wParam, lParam );
+
+	ResizeWindow();
+
+	return 0;
+}
+
+void CFilterEdit::OnSize( UINT nType, int cx, int cy ) 
+{
+	CEdit::OnSize( nType, cx, cy );
+	ResizeWindow();
+}
+
+BOOL CFilterEdit::OnSetCursor(CWnd* pWnd, UINT nHitTest, UINT message)
+{
+	CPoint pntCursor;
+	GetCursorPos(&pntCursor);
+	ScreenToClient(&pntCursor);
+	// if the cursor is not in the edit area, show the normal arrow cursor
+	if (!m_rcEditArea.PtInRect(pntCursor))
+	{
+		SetCursor(AfxGetApp()->LoadStandardCursor(MAKEINTRESOURCE(IDC_ARROW)));
+		return TRUE;
+	}
+
+	return CEdit::OnSetCursor(pWnd, nHitTest, message);
+}
