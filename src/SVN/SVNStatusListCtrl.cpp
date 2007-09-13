4284a836551b3d36880653903bcf2f7e1cc17bf0
schaefer <schaefer@154be670-7969-47af-9ab0-c6fb378d2295>
2007-09-13 18:46:13 +0000

    Sorting in the commit dialog sometimes lost the check marks if the list had previously been refreshed.
    To reproduce:
    1. Call the commit dialog
    2. Deselect an item which is checked by default
    3. Refresh with F5
    4. Select the previously deselected item
    5. Sort using an arbitrary column
    The previously set check mark is now lost.
    
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@10608 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNStatusListCtrl.cpp
+++ src/SVN/SVNStatusListCtrl.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2007 - Stefan Kueng
+// Copyright (C) 2003-2007 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -1639,6 +1639,7 @@ void CSVNStatusListCtrl::OnHdnItemclick(NMHDR *pNMHDR, LRESULT *pResult)
 	else
 		m_bAscending = TRUE;
 	m_nSortedColumn = phdr->iItem;
+	m_mapFilenameToChecked.clear();
 	Sort();
 
 	CHeaderCtrl * pHeader = GetHeaderCtrl();
