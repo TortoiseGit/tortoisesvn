852b95e4f37778079152c3e163294d7f30c86b7c
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2007-11-07 18:03:25 +0000

    Add two more blame test files.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@11287 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ test/blame/chineseUTF8.blame.php
@@ -0,0 +1,5 @@
+  line   rev    date                           path                                                         author                         content 
+
+       0   6752 09.06.2006 17:07:20            /trunk/test/mergediff/chinese_utf8_right.php                 steveking                      <?PHP
+       1   6752 09.06.2006 17:07:20            /trunk/test/mergediff/chinese_utf8_right.php                 steveking                      	echo "测试_UTF-8";
+       2   6752 09.06.2006 17:07:20            /trunk/test/mergediff/chinese_utf8_right.php                 steveking                      ?>
