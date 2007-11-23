0eeee37d0ab71addf9fd0342b923dfcfbc139153
stefan.fuhrmann.1974 <stefan.fuhrmann.1974@154be670-7969-47af-9ab0-c6fb378d2295>
2007-11-23 20:17:51 +0000

    * use "file" member if message is empty   (seems to happen for certain types of errors)
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@11427 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNError.cpp
+++ src/SVN/SVNError.cpp
@@ -14,6 +14,6 @@ SVNError::SVNError (svn_errno_t code, const CStringA& message)
 
 SVNError::SVNError (svn_error_t* error)
 	: code (static_cast<svn_errno_t>(error->apr_err))
-	, message (error->message)
+    , message (error->message != NULL ? error->message : error->file)
 {
 }
