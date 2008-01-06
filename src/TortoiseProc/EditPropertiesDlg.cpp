b30a64835c3a893d5feea4e19ac87e991e6455fd
schaefer <schaefer@154be670-7969-47af-9ab0-c6fb378d2295>
2008-01-06 15:44:27 +0000

    Double clicking on the empty list of the EditPropertiesDlg does not open the add dialog anymore.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@11821 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/EditPropertiesDlg.cpp
+++ src/TortoiseProc/EditPropertiesDlg.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2007 - TortoiseSVN
+// Copyright (C) 2003-2008 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -342,7 +342,9 @@ void CEditPropertiesDlg::OnBnClickedRemoveProps()
 
 void CEditPropertiesDlg::OnNMDblclkEditproplist(NMHDR * /*pNMHDR*/, LRESULT *pResult)
 {
-	EditProps();
+	if (m_propList.GetSelectedCount() == 1)
+		EditProps();
+
 	*pResult = 0;
 }
 
