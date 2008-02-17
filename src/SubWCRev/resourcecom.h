04f30f33031dfccc9706b033435cdf46c416820b
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2008-02-17 17:23:31 +0000

    Add application icons to SubWCRev, SubWCRevCOM and the cache.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@12157 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SubWCRev/resourcecom.h
+++ src/SubWCRev/resourcecom.h
@@ -2,13 +2,15 @@
 // Microsoft Visual C++ generated include file.
 // Used by SubWCRevCOM.rc
 //
+#define IDI_ICON1                       102
+#define IDI_SUBWCREV                    102
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
