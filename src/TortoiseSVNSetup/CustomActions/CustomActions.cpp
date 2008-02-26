7e5c1d21cc64077b7639b22ef49c6148b777c3c3
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2008-02-26 16:49:56 +0000

    Remove the custom action "CleanupRegistry" from the installer: we don't need this anymore.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@12196 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseSVNSetup/CustomActions/CustomActions.cpp
+++ src/TortoiseSVNSetup/CustomActions/CustomActions.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2007 - TortoiseSVN
+// Copyright (C) 2003-2008 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -74,69 +74,6 @@ UINT __stdcall TerminateCache(MSIHANDLE hModule)
 	return ERROR_SUCCESS;
 }
 
-UINT __stdcall ClearRegistry(MSIHANDLE hModule)
-{
-	TCHAR entries[36][255] = {	_T("Software\\Classes\\Drive\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
-		_T("Software\\Classes\\Directory\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
-		_T("Software\\Classes\\Directory\\Background\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
-		_T("Software\\Classes\\Folder\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
-		_T("Software\\Classes\\lnkfile\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
-		_T("Software\\Classes\\InternetShortcut\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
-		_T("Software\\Classes\\*\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
-
-		_T("Software\\Classes\\Drive\\shellex\\PropertySheetHandlers\\TortoiseSVN"),
-		_T("Software\\Classes\\Directory\\shellex\\PropertySheetHandlers\\TortoiseSVN"),
-		_T("Software\\Classes\\*\\shellex\\PropertySheetHandlers\\TortoiseSVN"),
-
-		_T("Software\\Classes\\Directory\\shellex\\DragDropHandlers\\TortoiseSVN"),
-		_T("Software\\Classes\\Folder\\shellex\\DragDropHandlers\\TortoiseSVN"),
-
-		_T("Software\\Classes\\Folder\\shellex\\ColumnHandlers\\{30351349-7B7D-4FCC-81B4-1E394CA267EB}"),
-
-		_T("Software\\Classes\\CLSID\\{30351349-7B7D-4FCC-81B4-1E394CA267EB}"),
-		_T("Software\\Classes\\CLSID\\{3035134A-7B7D-4FCC-81B4-1E394CA267EB}"),
-		_T("Software\\Classes\\CLSID\\{3035134E-7B7D-4FCC-81B4-1E394CA267EB}"),
-		_T("Software\\Classes\\CLSID\\{3035134D-7B7D-4FCC-81B4-1E394CA267EB}"),
-		_T("Software\\Classes\\CLSID\\{3035134C-7B7D-4FCC-81B4-1E394CA267EB}"),
-		_T("Software\\Classes\\CLSID\\{30351346-7B7D-4FCC-81B4-1E394CA267EB}"),
-		_T("Software\\Classes\\CLSID\\{3035134B-7B7D-4FCC-81B4-1E394CA267EB}"),
-		_T("Software\\Classes\\CLSID\\{30351347-7B7D-4FCC-81B4-1E394CA267EB}"),
-		_T("Software\\Classes\\CLSID\\{30351348-7B7D-4FCC-81B4-1E394CA267EB}"),
-
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\1TortoiseSVN"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\2TortoiseSVN"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\3TortoiseSVN"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\4TortoiseSVN"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\5TortoiseSVN"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\6TortoiseSVN"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\7TortoiseSVN"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN1"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN2"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN3"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN4"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN5"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN6"),
-		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN7")
-	};
-
-	for (int i=0; i<36; ++i)
-	{
-		HKEY hKey;
-		if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, entries[i], 0, KEY_WRITE, &hKey)==ERROR_SUCCESS)
-		{
-			SHDeleteKey(HKEY_LOCAL_MACHINE, (LPCTSTR)entries[i]);
-		}
-	}
-	for (int i=0; i<36; ++i)
-	{
-		HKEY hKey;
-		if (RegOpenKeyEx(HKEY_CURRENT_USER, entries[i], 0, KEY_WRITE, &hKey)==ERROR_SUCCESS)
-		{
-			SHDeleteKey(HKEY_CURRENT_USER, (LPCTSTR)entries[i]);
-		}
-	}
-	return ERROR_SUCCESS;
-}
 
 UINT __stdcall MsgBox(MSIHANDLE hModule)
 {
