43cdc661bde6eb742c01426cb119c76a78fde63e
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2008-02-28 16:33:18 +0000

    Don't segfault if and SVNError object is created with a NULL error.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@12206 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNError.cpp
+++ src/SVN/SVNError.cpp
@@ -13,7 +13,7 @@ SVNError::SVNError (svn_errno_t code, const CStringA& message)
 }
 
 SVNError::SVNError (svn_error_t* error)
-	: code (static_cast<svn_errno_t>(error->apr_err))
-    , message (error->message != NULL ? error->message : error->file)
+    : code (error ? static_cast<svn_errno_t>(error->apr_err) : (svn_errno_t)0)
+    , message (error ? (error->message != NULL ? error->message : error->file) : 0)
 {
 }
