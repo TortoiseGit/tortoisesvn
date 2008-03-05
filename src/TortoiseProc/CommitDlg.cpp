4b8365d52c037f11d09b21176a788d452193d38f
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2008-03-05 18:07:31 +0000

    Check the slider position is valid before adjusting the controls in the commit dialog.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@12277 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/CommitDlg.cpp
+++ src/TortoiseProc/CommitDlg.cpp
@@ -198,14 +198,23 @@ BOOL CCommitDlg::OnInitDialog()
 		CenterWindow(CWnd::FromHandle(hWndExplorer));
 	EnableSaveRestore(_T("CommitDlg"), TRUE);
 	DWORD yPos = CRegDWORD(_T("Software\\TortoiseSVN\\TortoiseProc\\ResizableState\\CommitDlgSizer"));
+	RECT rcDlg, rcLogMsg, rcFileList;
+	GetClientRect(&rcDlg);
+	m_cLogMessage.GetWindowRect(&rcLogMsg);
+	ScreenToClient(&rcLogMsg);
+	m_ListCtrl.GetWindowRect(&rcFileList);
+	ScreenToClient(&rcFileList);
 	if (yPos)
 	{
 		RECT rectSplitter;
 		m_wndSplitter.GetWindowRect(&rectSplitter);
 		ScreenToClient(&rectSplitter);
 		int delta = yPos - rectSplitter.top;
-		m_wndSplitter.SetWindowPos(NULL, 0, yPos, 0, 0, SWP_NOSIZE);
-		DoSize(delta);
+		if ((rcLogMsg.bottom + delta > rcLogMsg.top)&&(rcLogMsg.bottom + delta < rcFileList.bottom - 30))
+		{
+			m_wndSplitter.SetWindowPos(NULL, 0, yPos, 0, 0, SWP_NOSIZE);
+			DoSize(delta);
+		}
 	}
 
 	// add all directories to the watcher
