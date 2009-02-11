ea958a3318c08e453b15f90f41bbc954a87f98fc
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2009-02-11 18:47:28 +0000

    Issue #431: * Add test script for local edit, incoming delete upon update * Add test script for local delete, incoming delete upon update
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@15335 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ test/treeconflicts/tc-localEditIncomingDeleteUponUpdate.bat
@@ -0,0 +1,56 @@
+::@echo off
+set ROOT=D:\Development\SVN\SVNTests
+set REPOROOT=D:/Development/SVN/SVNTests
+set REPONAME=tcLocalDeleteIncomingEditRepo
+set WCNAME=tcLocalDeleteIncomingEditWC
+set REPO=svn://localhost:40000/
+set WC=%ROOT%\%WCNAME%
+
+set SVNCLI=D:\Development\SVN\TortoiseSVN\bin\debug\bin\svn.exe
+set SVNADM=D:\Development\SVN\TortoiseSVN\bin\debug\bin\svnadmin.exe
+::set SVNCLI=svn.exe
+::set SVNADM=svnadmin.exe
+
+cd %ROOT%
+if exist %REPOROOT%/%REPONAME% rd /s /q %REPOROOT%/%REPONAME%
+if exist %WCNAME%1 rd /s /q %WCNAME%1
+if exist %WCNAME%2 rd /s /q %WCNAME%2
+
+mkdir %REPOROOT%\%REPONAME%
+svnadmin create %REPOROOT%\%REPONAME%
+
+echo [general]> %REPOROOT%\%REPONAME%\conf\svnserve.conf
+echo anon-access = write>> %REPOROOT%\%REPONAME%\conf\svnserve.conf
+rem Launch svnserve for current directory at port 40000
+start svnserve.exe --daemon --foreground --root %REPOROOT%\%REPONAME% --listen-port 40000 --listen-host localhost
+
+
+%SVNCLI% co %REPO% %WC%1
+
+mkdir %WC%1\foofolder
+%SVNCLI% add %WC%1\foofolder
+echo testline1 > %WC%1\foo.c
+echo testline1 > %WC%1\foo2.c
+%SVNCLI% add %WC%1\foo.c
+%SVNCLI% add %WC%1\foo2.c
+%SVNCLI% ci %WC%1 -m ""
+
+
+%SVNCLI% co %REPO% %WC%2
+
+:: user 1 edits file
+echo testlinemodified > %WC%1\foo.c
+echo testlinemodified > %WC%1\foo2.c
+:: user 1 edits folder
+%SVNCLI% propset testprop testvalue %WC%1\foofolder
+
+:: user 2 moves the same file
+%SVNCLI% mv %WC%2\foo.c %WC%2\bar.c
+%SVNCLI% rm %WC%2\foo2.c
+%SVNCLI% mv %WC%2\foofolder %WC%2\barfolder
+%SVNCLI% ci %WC%2 -m ""
+
+:: tree conflict on user 1 update
+%SVNCLI% up %WC%1
+%SVNCLI% st %WC%1
+
