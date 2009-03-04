fb54815d42fb9c17864263693667be5727ff0ee9
simonlarge <simonlarge@154be670-7969-47af-9ab0-c6fb378d2295>
2009-03-04 21:38:40 +0000

    Update the doc test generator following doc structure changes.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@15562 154be670-7969-47af-9ab0-c6fb378d2295

--- doc/test/MakeTest.bat
+++ doc/test/MakeTest.bat
@@ -1,4 +1,5 @@
 @echo off
+echo Creating test repositories and working copies
 :: There is no easy way to avoid using an absolute path here.
 :: To avoid editing a versioned file you should create a text file
 :: called DocPath.txt which contains the path to the TSVN doc folder.
@@ -24,7 +25,7 @@ svn co -q file:///%docpath%/test/temp/repos2 ext
 :: This is used to add content to the 'external' repository.
 cd ext
 :: Copy some files from the docs to create content
-copy ..\..\..\source\en\TortoiseSVN\tsvn_server\server*.xml > nul
+copy ..\..\..\source\en\TortoiseSVN\tsvn_repository\server*.xml > nul
 for %%f in (server*.xml) do svn add %%f
 :: Commit external repos2 r1
 svn ci -q -m "Document the server" .
@@ -50,7 +51,7 @@ svn add --no-auto-props --targets ..\targets
 :: Commit repos r2
 svn ci -q -m "Document commands group 2" .
 type nul > ..\targets
-for %%f in (relocate revert showlog) do (
+for %%f in (relocate revert log) do (
 	@copy/b ..\..\..\source\en\TortoiseSVN\tsvn_dug\dug_%%f.xml+..\..\footnote1.txt dug_%%f.xml> nul
 	@echo dug_%%f.xml >> ..\targets
 )
@@ -75,7 +76,7 @@ svn up -q ../docs
 :: Change detection is broken when timestamps are the same.
 :: Force a current timestamp by using type instead of copy.
 type ..\..\subwcrev2.txt > subwcrev.txt
-for %%f in (ignore showlog) do (
+for %%f in (ignore log) do (
 	@copy/b ..\..\..\source\en\TortoiseSVN\tsvn_dug\dug_%%f.xml+..\..\footnote2.txt dug_%%f.xml> nul
 )
 :: Add text which will need merging on next update
@@ -116,7 +117,7 @@ svn lock -m "Major update" dug_add.xml > NUL
 cd ..\docs
 :: Make changes which conflict with what we did earlier.
 copy /y ..\..\subwcrev3.txt subwcrev.txt > nul
-for %%f in (checkout commit export ignore showlog) do (
+for %%f in (checkout commit export ignore log) do (
 	@copy/b ..\..\..\source\en\TortoiseSVN\tsvn_dug\dug_%%f.xml+..\..\footnote3.txt dug_%%f.xml> nul
 )
 svn diff . > ..\docs.patch
