d39a8f3e557d090cf5018d6aa83a776912f8c168
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2009-03-19 18:21:00 +0000

    Use svn_path_internal_style() instead of svn_path_canonicalize().
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@15796 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseMerge/AppUtils.cpp
+++ src/TortoiseMerge/AppUtils.cpp
@@ -1,6 +1,6 @@
 // TortoiseMerge - a Diff/Patch program
 
-// Copyright (C) 2006-2008 - TortoiseSVN
+// Copyright (C) 2006-2009 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -97,7 +97,7 @@ bool CAppUtils::CreateUnifiedDiff(const CString& orig, const CString& modified, 
 	apr_file_t * outfile = NULL;
 	apr_pool_t * pool = svn_pool_create(NULL);
 
-	svn_error_t * err = svn_io_file_open (&outfile, svn_path_canonicalize(CUnicodeUtils::GetUTF8(output), pool),
+	svn_error_t * err = svn_io_file_open (&outfile, svn_path_internal_style(CUnicodeUtils::GetUTF8(output), pool),
 		APR_WRITE | APR_CREATE | APR_BINARY | APR_TRUNCATE,
 		APR_OS_DEFAULT, pool);
 	if (err == NULL)
@@ -109,12 +109,12 @@ bool CAppUtils::CreateUnifiedDiff(const CString& orig, const CString& modified, 
 			svn_diff_file_options_t * opts = svn_diff_file_options_create(pool);
 			opts->ignore_eol_style = false;
 			opts->ignore_space = svn_diff_file_ignore_space_none;
-			err = svn_diff_file_diff_2(&diff, svn_path_canonicalize(CUnicodeUtils::GetUTF8(orig), pool), 
-				svn_path_canonicalize(CUnicodeUtils::GetUTF8(modified), pool), opts, pool);
+			err = svn_diff_file_diff_2(&diff, svn_path_internal_style(CUnicodeUtils::GetUTF8(orig), pool), 
+				svn_path_internal_style(CUnicodeUtils::GetUTF8(modified), pool), opts, pool);
 			if (err == NULL)
 			{
-				err = svn_diff_file_output_unified(stream, diff, svn_path_canonicalize(CUnicodeUtils::GetUTF8(orig), pool), 
-					svn_path_canonicalize(CUnicodeUtils::GetUTF8(modified), pool),
+				err = svn_diff_file_output_unified(stream, diff, svn_path_internal_style(CUnicodeUtils::GetUTF8(orig), pool), 
+					svn_path_internal_style(CUnicodeUtils::GetUTF8(modified), pool),
 					NULL, NULL, pool);
 				svn_stream_close(stream);
 			}
