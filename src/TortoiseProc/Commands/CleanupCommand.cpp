63bd687ee066404c5290567d0826b094a32ba9b5
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2009-03-23 16:59:47 +0000

    Fix crash in "cleanup".
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@15892 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/Commands/CleanupCommand.cpp
+++ src/TortoiseProc/Commands/CleanupCommand.cpp
@@ -69,7 +69,7 @@ bool CleanupCommand::Execute()
 			CShellUpdater::Instance().AddPathsForUpdate(updateList);
 			CShellUpdater::Instance().Flush();
 			updateList.SortByPathname(true);
-			for (INT_PTR j=0; i<updateList.GetCount(); ++j)
+			for (INT_PTR j=0; j<updateList.GetCount(); ++j)
 			{
 				SHChangeNotify(SHCNE_UPDATEITEM, SHCNF_PATH, updateList[j].GetWinPath(), NULL);
 				ATLTRACE(_T("notify change for path %s\n"), updateList[j].GetWinPath());
