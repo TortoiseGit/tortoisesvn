816ec83eb1a464f8b344e80b1ef8ff701ac8edc0
stefan.fuhrmann.1974 <stefan.fuhrmann.1974@154be670-7969-47af-9ab0-c6fb378d2295>
2009-03-28 19:10:41 +0000

    add sanity check before accessing data buffer
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@15976 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TSVNCache/CacheInterface.cpp
+++ src/TSVNCache/CacheInterface.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// External Cache Copyright (C) 2007 - TortoiseSVN
+// External Cache Copyright (C) 2007,2009 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -35,20 +35,23 @@ CString GetCacheMutexName()
 }
 CString GetCacheID()
 {
+    CString t;
 	HANDLE token;
 	DWORD len;
 	BOOL result = OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token);
 	if(result)
 	{
 		GetTokenInformation(token, TokenStatistics, NULL, 0, &len);
-		LPBYTE data = new BYTE[len];
-		GetTokenInformation(token, TokenStatistics, data, len, &len);
-		LUID uid = ((PTOKEN_STATISTICS)data)->AuthenticationId;
-		delete [ ] data;
-		CString t;
-		t.Format(_T("-%08x%08x"), uid.HighPart, uid.LowPart);
-		CloseHandle(token);
-		return t;
+        if (len >= sizeof (TOKEN_STATISTICS))
+        {
+		    LPBYTE data = new BYTE[len];
+		    GetTokenInformation(token, TokenStatistics, data, len, &len);
+		    LUID uid = ((PTOKEN_STATISTICS)data)->AuthenticationId;
+		    t.Format(_T("-%08x%08x"), uid.HighPart, uid.LowPart);
+		    delete [ ] data;
+        }
+
+        CloseHandle(token);
 	}
-	return _T("");
+    return t;
 }
