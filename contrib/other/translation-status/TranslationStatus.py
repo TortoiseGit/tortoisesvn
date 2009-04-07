628883c63fa58452756d0ebc9ed38d28b22d5cb5
luebbe.tortoisesvn <luebbe.tortoisesvn@154be670-7969-47af-9ab0-c6fb378d2295>
2009-04-07 15:12:42 +0000

    Improve quoting Improve regex to match the repository URL for older and newer clients Replace re.sub with self.match Set executable flag
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@16091 154be670-7969-47af-9ab0-c6fb378d2295

--- contrib/other/translation-status/TranslationStatus.py
+++ contrib/other/translation-status/TranslationStatus.py
@@ -34,13 +34,13 @@ import csv
 import time
 import subprocess
 
-urlTrunk = "trunk"
-urlBranch = "branches/1.6.x"
-urlGui   = "Languages"
-urlDoc   = "doc/po"
+urlTrunk = 'trunk'
+urlBranch = 'branches/1.6.x'
+urlGui   = 'Languages'
+urlDoc   = 'doc/po'
 
 langList = os.path.join('gui', 'trunk', 'Languages.txt')
-langFields = ["Tag","LangCC","LangID","FlagByte","LangName","Translators"]
+langFields = ['Tag','LangCC','LangID','FlagByte','LangName','Translators']
 
 def usage_and_exit(errmsg=None):
     """Print a usage message, plus an ERRMSG (if provided), then exit.
@@ -54,7 +54,7 @@ def usage_and_exit(errmsg=None):
         stream = sys.stderr
     print >> stream, __doc__
     if errmsg:
-        print >> stream, "\nError: %s" % (errmsg)
+        print >> stream, '\nError: %s' % (errmsg)
         sys.exit(2)
     sys.exit(0)
 
@@ -65,15 +65,15 @@ def makeTimeString(format, timestamp):
 
 class includeWriter:
     def __init__(self, outfile):
-        self.out = file(outfile, "w")
+        self.out = file(outfile, 'w')
 
     def addHeader(self):
         self.out.write('<?php\n')
 
     def addFooter(self, wcrev, update):
         self.out.write('$tsvn_var = array(\n')
-        self.out.write('\"wcrev\" => \"%s\"\n' %(wcrev))
-        self.out.write('\"update\" => \"%s\"\n' %(update))
+        self.out.write('"wcrev" => "%s"\n' %(wcrev))
+        self.out.write('"update" => "%s"\n' %(update))
         self.out.write(');\n')
         self.out.write('?>\n')
 
@@ -81,12 +81,12 @@ class includeWriter:
         self.out.write('$%s = array(\n' %(section))
 
     def sectWrite(self, langCC, isPoFile, total, trans, fuzzy, untrans, accel, file, date):
-        self.out.write('\"%s\" => array(%s, %s, %s, %s, %s, %s, \"%s\", \"%s\"),\n' \
+        self.out.write('"%s" => array(%s, %s, %s, %s, %s, %s, "%s", "%s"),\n' \
           % (langCC,isPoFile,total,trans,fuzzy,untrans,accel,file,date))
 
     def sectClose(self, total, file, date):
         # Write the end of the array without a comma
-        self.out.write('\"zzz\" => array(-1, %s, 0, 0, %s, 0, \"%s\", \"%s\")\n' \
+        self.out.write('"zzz" => array(-1, %s, 0, 0, %s, 0, "%s", "%s")\n' \
           %(total, total ,file+'.pot', date))
         self.out.write(');\n')
 
@@ -100,7 +100,7 @@ class transCountryWriter:
         self.out.write('?>\n')
 
     def writeList(self):
-        csvReader = csv.DictReader(open(langList), langFields, delimiter=';', quotechar='\"')
+        csvReader = csv.DictReader(open(langList), langFields, delimiter=';', quotechar='"')
         csvReader.skipinitialspace = True
 
         for row in csvReader:
@@ -110,18 +110,18 @@ class transCountryWriter:
               row['LangName'].strip(), row['Translators'].strip())
 
     def writeRow(self, LangCC, Tag, Flag, LangName, Translators):
-        self.out.write('\"%s\" => array(\"%s\", \"%s\", \"%s\", \"%s\", %s)' \
+        self.out.write('"%s" => array("%s", "%s", "%s", "%s", %s)' \
           % (LangCC,Tag,Flag,LangCC,LangName,Translators))
-        if LangCC != "zzz":
+        if LangCC != 'zzz':
           self.out.write(',')
         self.out.write('\n')
 
 class transReport:
-    def __init__(self, to_email_id="luebbe@tigris.org"):
+    def __init__(self, to_email_id='luebbe@tigris.org'):
         self.to_email_id = to_email_id
-        self.from_email_id = "<dev@tortoisesvn.tigris.org>"
+        self.from_email_id = '<dev@tortoisesvn.tigris.org>'
 
-    def safe_command(self, cmd_and_args, cmd_in=""):
+    def safe_command(self, cmd_and_args, cmd_in=''):
         [stdout, stderr] = subprocess.Popen(cmd_and_args, \
                            stdin=subprocess.PIPE, \
                            stdout=subprocess.PIPE, \
@@ -189,13 +189,13 @@ class transReport:
           cmd = ['msgmerge','--no-wrap','--quiet','--no-fuzzy-matching', srcFile, potFile, '-o', dstFile]
           self.safe_command(cmd)
           total = self.checkAttrib('--untranslated', potFile)
-          timestamp = makeTimeString("%Y-%m-%d", os.path.getmtime(potFile))
+          timestamp = makeTimeString('%Y-%m-%d', os.path.getmtime(potFile))
         return (total, timestamp)
 
     def getPoDate(self, poFile):
         """returns the PO-Revision-Date of the the .po file in a string"""
         grepout = self.safe_command(['grep', '-E', 'PO-Revision-Date:', poFile])[0]
-        reout = re.sub('.*?(\d{4}-\d{2}-\d{2}).*','\\1', grepout.strip())
+        reout = self.match('.*?(\d{4}-\d{2}-\d{2}).*', grepout.strip())
         return reout
 
     def checkError(self, attrib, file):
@@ -203,7 +203,7 @@ class transReport:
         msgout = self.safe_command(['msgfmt', attrib, file])[1]
         if msgout:
            grepout = self.safe_command(['grep', '-E', 'msgfmt: found'], msgout)[0]
-           reout = re.sub('.*?(\d+).*','\\1', grepout.strip())
+           reout = self.match('.*?(\d+).*', grepout.strip())
            return int(reout)
         else:
            return 0
@@ -223,14 +223,14 @@ class transReport:
         if not os.path.isfile(wrkFile):
           # No need to write status for non-existent .po files
           # Just print on standard output for the "paper" report
-          # out.sectWrite(langCC, 0, total, 0, 0, total, 0, fileName, "NONE")
-          return "NONE"
+          # out.sectWrite(langCC, 0, total, 0, 0, total, 0, fileName, 'NONE')
+          return 'NONE'
         else:
           poDate = self.getPoDate(wrkFile)
           error = self.checkError('--check', wrkFile)
           if error:
             out.sectWrite(langCC, error, total, 0, 0, total, 0, fileName, poDate)
-            return "BROKEN"
+            return 'BROKEN'
           else:
             trans = self.checkAttrib('--translated', wrkFile)
             untrans = self.checkAttrib('--untranslated', wrkFile)
@@ -244,7 +244,7 @@ class transReport:
             out.sectWrite(langCC, 0, total, trans, fuzzy, untrans, accel, fileName, poDate)
 
             if untrans+fuzzy+accel == 0:
-              return "OK"
+              return 'OK'
             else:
               if trans == total:
                 percent = 99
@@ -252,12 +252,12 @@ class transReport:
                 percent = 100*(trans)/total
 
               if checkAccel:
-                return "%2s%% (%s/%s/%s)" % (percent, untrans, fuzzy, accel)
+                return '%2s%% (%s/%s/%s)' % (percent, untrans, fuzzy, accel)
               else:
-                return "%2s%% (%s/%s)" % (percent, untrans, fuzzy)
+                return '%2s%% (%s/%s)' % (percent, untrans, fuzzy)
 
     def checkTranslation(self, srcDir, fileMask, checkAccel):
-        print "%s" %(srcDir)
+        print '%s' %(srcDir)
 
         [totTrunk, tsTrunk] = self.prepareTMP(srcDir, 'trunk', fileMask)
         [totBranch, tsBranch] = self.prepareTMP(srcDir, 'branch', fileMask)
@@ -265,7 +265,7 @@ class transReport:
         outTrunk.sectOpen(fileMask)
         outBranch.sectOpen(fileMask)
 
-        print "%s %s" % (totTrunk, totBranch)
+        print '%s %s' % (totTrunk, totBranch)
 
         csvReader = csv.DictReader(open(langList), langFields, delimiter=';', quotechar='"')
         csvReader.skipinitialspace = True
@@ -281,7 +281,7 @@ class transReport:
             if statusTrunk != 'NONE' or statusBranch != 'NONE':
                langName = row['LangName'].strip()
                langName = langName + ' ('+langCC+')'
-               print "%-32s: %-20s: %-20s" % (langName,statusTrunk,statusBranch)
+               print '%-32s: %-20s: %-20s' % (langName,statusTrunk,statusBranch)
 
         outTrunk.sectClose(totTrunk, fileMask, tsTrunk)
         outBranch.sectClose(totBranch, fileMask, tsBranch)
@@ -291,11 +291,16 @@ class transReport:
     def createReport(self):
         [info_out, info_err] = self.safe_command(['svn', 'info'])
         if info_err:
-            print >> sys.stderr, "\nError: %s" % (info_err)
+            print >> sys.stderr, '\nError: %s' % (info_err)
             sys.exit(0)
+            
+        # Try different matches for older and newer svn clients
         reposRoot = self.match('Repository Root: (\S+)',info_out)
-        reposTrunk = reposRoot + "/" + urlTrunk + "/"
-        reposBranch = reposRoot + "/" + urlBranch + "/"
+        if reposRoot is None:
+           reposRoot = self.match('Repository: (\S+)',info_out)
+
+        reposTrunk = reposRoot + '/' + urlTrunk + '/'
+        reposBranch = reposRoot + '/' + urlBranch + '/'
 
         # Prepare the working copies for doc|gui * trunk|branch
         self.prepareWC(os.path.join('gui', 'trunk'), reposTrunk + urlGui)
@@ -316,27 +321,25 @@ class transReport:
 
         return None
 
-outTrunk = includeWriter("trans_data_trunk.inc")
-outBranch = includeWriter("trans_data_branch.inc")
+outTrunk = includeWriter('trans_data_trunk.inc')
+outBranch = includeWriter('trans_data_branch.inc')
 
 def main():
     # Parse the command-line options and arguments.
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], "hm:",
-                                       ["help",
-                                        "to-email-id=",
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'hm:',
+                                       ['help',
+                                        'to-email-id=',
                                         ])
     except getopt.GetoptError, msg:
         usage_and_exit(msg)
 
     to_email_id = None
     for opt, arg in opts:
-        if opt in ("-h", "--help"):
+        if opt in ('-h', '--help'):
             usage_and_exit()
-        elif opt in ("-m", "--to-email-id"):
+        elif opt in ('-m', '--to-email-id'):
             to_email_id = arg
-
-    transCountryWriter("trans_countries.inc")
 
     outTrunk.addHeader()
     outBranch.addHeader()
@@ -346,15 +349,16 @@ def main():
 
     [info_out, info_err] = report.safe_command(['svnversion', '.'])
     if info_err:
-        print >> sys.stderr, "\nError: %s" % (info_err)
+        print >> sys.stderr, '\nError: %s' % (info_err)
         sys.exit(0)
 
     wcrev = re.sub('[MS]', '', info_out).strip()
-    timestamp = makeTimeString("%a, %d %b %Y %H:%M", time.time())
+    timestamp = makeTimeString('%a, %d %b %Y %H:%M', time.time())
 
     outTrunk.addFooter(wcrev, timestamp)
     outBranch.addFooter(wcrev, timestamp)
 
+    transCountryWriter('trans_countries.inc')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
