3bfa88af7208c6d6a8efb2c392a8a244dbc66342
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2009-04-09 14:57:22 +0000

    Pass the escaped url to the List() method.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@16122 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/RepositoryBrowser.cpp
+++ src/TortoiseProc/RepositoryBrowser.cpp
@@ -1070,7 +1070,7 @@ bool CRepositoryBrowser::RefreshNode(HTREEITEM hNode, bool force /* = false*/, b
 	pTreeItem->children.clear();
 	pTreeItem->has_child_folders = false;
 	m_bCancelled = false;
-	if (!List(CTSVNPath(pTreeItem->url), GetRevision(), GetRevision(), recursive ? svn_depth_infinity : svn_depth_immediates, true))
+	if (!List(CTSVNPath(EscapeUrl(CTSVNPath(pTreeItem->url))), GetRevision(), GetRevision(), recursive ? svn_depth_infinity : svn_depth_immediates, true))
 	{
 		// error during list()
 		m_RepoList.ShowText(GetLastErrorMessage());
