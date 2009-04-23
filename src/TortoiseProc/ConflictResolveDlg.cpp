e2ce1d4b50a91fa9e10f55cd80c6b4c9cccceb69
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2009-04-23 19:54:12 +0000

    Pass svn_wc_conflict_choose_xxxx_full for binary files.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@16248 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/ConflictResolveDlg.cpp
+++ src/TortoiseProc/ConflictResolveDlg.cpp
@@ -164,13 +164,13 @@ BOOL CConflictResolveDlg::OnInitDialog()
 
 void CConflictResolveDlg::OnBnClickedUselocal()
 {
-	m_choice = svn_wc_conflict_choose_mine_conflict;
+	m_choice = 	m_pConflictDescription->is_binary ? svn_wc_conflict_choose_mine_full : svn_wc_conflict_choose_mine_conflict;
 	EndDialog(IDOK);
 }
 
 void CConflictResolveDlg::OnBnClickedUserepo()
 {
-	m_choice = svn_wc_conflict_choose_theirs_conflict;
+	m_choice = 	m_pConflictDescription->is_binary ? svn_wc_conflict_choose_theirs_full : svn_wc_conflict_choose_theirs_conflict;
 	EndDialog(IDOK);
 }
 
