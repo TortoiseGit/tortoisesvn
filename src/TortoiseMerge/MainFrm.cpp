b9df92c8f720d1db261cac756ecf21a0db7aeea7
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2009-10-05 17:06:48 +0000

    Keep the cursor on the same line when collapsing.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@17362 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseMerge/MainFrm.cpp
+++ src/TortoiseMerge/MainFrm.cpp
@@ -876,17 +876,24 @@ void CMainFrame::OnViewCollapsed()
 	CRegDWORD regViewCollapsed = CRegDWORD(_T("Software\\TortoiseMerge\\Collapsed"), 0);
 	regViewCollapsed = !(DWORD)regViewCollapsed;
 	m_bCollapsed = !!(DWORD)regViewCollapsed;
+
 	if (m_pwndLeftView)
 	{
+		m_pwndLeftView->UpdateCaret();
 		m_pwndLeftView->Invalidate();
+		m_pwndLeftView->EnsureCaretVisible();
 	}
 	if (m_pwndRightView)
 	{
+		m_pwndRightView->UpdateCaret();
 		m_pwndRightView->Invalidate();
+		m_pwndRightView->EnsureCaretVisible();
 	}
 	if (m_pwndBottomView)
 	{
+		m_pwndBottomView->UpdateCaret();
 		m_pwndBottomView->Invalidate();
+		m_pwndBottomView->EnsureCaretVisible();
 	}
 	m_wndLocatorBar.Invalidate();
 }
