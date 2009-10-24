2adb9f7dda81a67510231993d74251ee488a78db
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2009-10-24 15:20:29 +0000

    Apply our patches.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@17560 154be670-7969-47af-9ab0-c6fb378d2295

--- ext/libintl/gettext-0.14.6/gettext-runtime/intl/localename.c
+++ ext/libintl/gettext-0.14.6/gettext-runtime/intl/localename.c
@@ -1357,7 +1357,7 @@ _nl_locale_name_default (void)
 	switch (sub)
 	  {
 	  case SUBLANG_PUNJABI_INDIA: return "pa_IN"; /* Gurmukhi script */
-	  case SUBLANG_PUNJABI_PAKISTAN: return "pa_PK"; /* Arabic script */
+	  /*case SUBLANG_PUNJABI_PAKISTAN: return "pa_PK"; /* Arabic script */
 	  }
 	return "pa";
       case LANG_RHAETO_ROMANCE: return "rm_CH";
@@ -1365,7 +1365,7 @@ _nl_locale_name_default (void)
 	switch (sub)
 	  {
 	  case SUBLANG_ROMANIAN_ROMANIA: return "ro_RO";
-	  case SUBLANG_ROMANIAN_MOLDOVA: return "ro_MD";
+	  /*case SUBLANG_ROMANIAN_MOLDOVA: return "ro_MD";*/
 	  }
 	return "ro";
       case LANG_RUSSIAN:
