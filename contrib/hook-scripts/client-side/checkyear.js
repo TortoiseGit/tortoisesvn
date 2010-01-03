04ad2e0b7675288d563839a9cb5c8c0fec80549e
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2010-01-03 19:41:18 +0000

    From now on, check for 2010.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@18171 154be670-7969-47af-9ab0-c6fb378d2295

--- contrib/hook-scripts/client-side/checkyear.js
+++ contrib/hook-scripts/client-side/checkyear.js
@@ -18,7 +18,7 @@ if (num != 4)
     WScript.Quit(1);
 }
 
-var re = /^\/\/ Copyright.+(2009)(.*)/;
+var re = /^\/\/ Copyright.+(2010)(.*)/;
 var basere = /^\/\/ Copyright(.*)/;
 var filere = /(\.cpp$)|(\.h$)/;
 var found = true;
