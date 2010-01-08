380689da21fc50622ca0516694a4949591da19e2
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2010-01-08 18:15:36 +0000

    Check for invalid pointer.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@18247 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseShell/IconOverlay.cpp
+++ src/TortoiseShell/IconOverlay.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2009 - TortoiseSVN
+// Copyright (C) 2003-2010 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -57,6 +57,9 @@ STDMETHODIMP CShellExt::GetOverlayInfo(LPWSTR /*pwszIconFile*/, int /*cchMax*/, 
 
 STDMETHODIMP CShellExt::GetPriority(int *pPriority)
 {
+	if (pPriority == 0)
+		return E_POINTER;
+
 	switch (m_State)
 	{
 		case FileStateConflict:
