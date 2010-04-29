bf18a9a6e336cc6ab087bf2e42577c8689a133af
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2010-04-29 19:12:37 +0000

    Show a history of previously used urls in the "edit externals" dialog.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@19389 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/Properties/EditPropExternalsValue.cpp
+++ src/TortoiseProc/Properties/EditPropExternalsValue.cpp
@@ -77,7 +77,9 @@ BOOL CEditPropExternalsValue::OnInitDialog()
     SVNRev pegRev = SVNRev(m_External.pegrevision);
     if (pegRev.IsValid() && !pegRev.IsHead())
         m_sPegRev = pegRev.ToString();
-
+    
+    m_URLCombo.LoadHistory(_T("Software\\TortoiseSVN\\History\\repoURLS"), _T("url"));
+    m_URLCombo.SetURLHistory(TRUE);
     m_URLCombo.SetWindowText(m_External.url);
 
     UpdateData(false);
