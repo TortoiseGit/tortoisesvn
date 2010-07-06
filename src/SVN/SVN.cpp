d6394fd9b32bcdef691004031200134ed5172b8e
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2010-07-06 19:35:36 +0000

    Try opening the repo using the rootPath, not the (maybe not existing) full path.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@19824 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVN.cpp
+++ src/SVN/SVN.cpp
@@ -2014,7 +2014,7 @@ bool SVN::IsRepository(const CTSVNPath& path)
     if (rootPath)
     {
         svn_repos_t* pRepos = NULL;
-        Err = svn_repos_open (&pRepos, path.GetSVNApiPath(subPool), subPool);
+        Err = svn_repos_open (&pRepos, rootPath, subPool);
         if ((Err)&&(Err->apr_err == SVN_ERR_FS_BERKELEY_DB))
             return true;
         if (Err == NULL)
