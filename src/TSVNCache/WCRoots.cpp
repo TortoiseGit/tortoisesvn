e73a492435bdf19fa5a434720e07a56841f7e6d4
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2010-08-15 12:32:51 +0000

    Add new class CWCRoots which handles the file times of the wc.db files of all WC roots. This should work fine for single-db once it's ready. For now, it simply treats every folder as a WC root.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@20017 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ src/TSVNCache/WCRoots.cpp
@@ -0,0 +1,141 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// External Cache Copyright (C) 2010 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "StdAfx.h"
+#include "WCRoots.h"
+#include "SVNAdminDir.h"
+
+
+#define DBTIMEOUT 10000
+
+CWCRoots::CWCRoots()
+{
+
+}
+
+CWCRoots::~CWCRoots()
+{
+
+}
+
+__int64 CWCRoots::GetDBFileTime( const CTSVNPath& path )
+{
+    AutoLocker lock(m_critSec);
+    std::map<CTSVNPath, WCRootsTimes>::iterator it = m_WCDBs.lower_bound(path);
+    if (it != m_WCDBs.end())
+    {
+        if (it->first.IsAncestorOf(path))
+        {
+            DWORD ticks = GetTickCount();
+            if (ticks - it->second.LastTicks > DBTIMEOUT)
+            {
+                // refresh the file time
+                CTSVNPath wcDbFile(it->first);
+                wcDbFile.AppendPathString(g_SVNAdminDir.GetAdminDirName() + _T("\\wc.db"));
+                if (wcDbFile.Exists())
+                {
+                    WCRootsTimes dbTimes;
+                    dbTimes.LastTicks = ticks;
+                    dbTimes.FileTime = wcDbFile.GetLastWriteTime();
+                    it->second = dbTimes;
+                }
+                else
+                {
+                    // remove the path from the map
+                    m_WCDBs.erase(it);
+                    return 0;
+                }
+            }
+            return it->second.FileTime;
+        }
+        else
+        {
+            it = AddPathInternal(path);
+            if (it != m_WCDBs.end())
+                return it->second.FileTime;
+        }
+    }
+    else
+    {
+        it = AddPathInternal(path);
+        if (it != m_WCDBs.end())
+            return it->second.FileTime;
+    }
+    return 0;
+}
+
+
+std::map<CTSVNPath, WCRootsTimes>::iterator CWCRoots::AddPathInternal( const CTSVNPath& path )
+{
+    AutoLocker lock(m_critSec);
+    CTSVNPath p(path);
+    do 
+    {
+        CTSVNPath dbPath(p);
+        dbPath.AppendPathString(g_SVNAdminDir.GetAdminDirName() + _T("\\wc.db"));
+        if (!dbPath.Exists())
+            p = p.GetContainingDirectory();
+        else
+        {
+            WCRootsTimes dbTimes;
+            dbTimes.LastTicks = GetTickCount();
+            dbTimes.FileTime = dbPath.GetLastWriteTime();
+            return m_WCDBs.insert(std::pair<CTSVNPath, WCRootsTimes>(p, dbTimes)).first;
+        }
+    } while (!p.IsEmpty());
+
+    return m_WCDBs.end();
+}
+
+bool CWCRoots::AddPath( const CTSVNPath& path )
+{
+    return AddPathInternal(path) != m_WCDBs.end();
+}
+
+void CWCRoots::NotifyChange( const CTSVNPath& path )
+{
+    AutoLocker lock(m_critSec);
+    CTSVNPath p(path);
+    while (p.IsAdminDir())
+    {
+        p = p.GetContainingDirectory();
+    }
+    std::map<CTSVNPath, WCRootsTimes>::iterator it = m_WCDBs.lower_bound(p);
+    if (it != m_WCDBs.end())
+    {
+        if (it->first.IsAncestorOf(p))
+        {
+            // refresh the file time
+            CTSVNPath wcDbFile(it->first);
+            wcDbFile.AppendPathString(g_SVNAdminDir.GetAdminDirName() + _T("\\wc.db"));
+            if (wcDbFile.Exists())
+            {
+                WCRootsTimes dbTimes;
+                dbTimes.LastTicks = GetTickCount();
+                dbTimes.FileTime = wcDbFile.GetLastWriteTime();
+                it->second = dbTimes;
+            }
+            else
+            {
+                // remove the path from the map
+                m_WCDBs.erase(it);
+            }
+        }
+    }
+}
---
+++ src/TSVNCache/WCRoots.h
@@ -0,0 +1,56 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// External Cache Copyright (C) 2010 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#pragma once
+
+#include "TSVNPath.h"
+#include <map>
+
+struct WCRootsTimes
+{
+    __int64 FileTime;
+    DWORD   LastTicks;
+};
+
+class CWCRoots
+{
+public:
+    CWCRoots();
+    ~CWCRoots();
+
+    /// Returns the last-write file time of the WC database file.
+    /// If the path doesn't have a database file and also no parent
+    /// path of it, the returned file time is 0.
+    __int64 GetDBFileTime(const CTSVNPath& path);
+
+    /// adds a possible wc path. If the path itself is not a WC root,
+    /// this method tries to find the wc root itself.
+    /// returns true if the path was added, i.e., a WC root was 
+    /// found for it. Returns false otherwise.
+    bool AddPath(const CTSVNPath& path);
+
+    /// Forces a re-read of the last-write-filetime of the wc.db
+    /// file for the specified \c path.
+    void NotifyChange(const CTSVNPath& path);
+
+private:
+    std::map<CTSVNPath, WCRootsTimes>::iterator AddPathInternal(const CTSVNPath& path);
+
+    CComAutoCriticalSection                 m_critSec;
+    std::map<CTSVNPath, WCRootsTimes>       m_WCDBs;
+};
