770047f37ce37d41c594bd971ce36eac365d9e7f
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2010-11-06 13:28:27 +0000

    Extend CSelectFileFilter to support the COMDLG_FILTERSPEC used in IFileDialog.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@20386 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/SelectFileFilter.h
+++ src/Utils/SelectFileFilter.h
@@ -11,9 +11,15 @@ public:
 
     operator TCHAR*() { return buffer; }
     void Load(UINT stringId);
+    UINT GetCount() { return (UINT)filternames.size(); }
+    operator COMDLG_FILTERSPEC*() { return filterspec; }
 
 private:
     auto_buffer<TCHAR> buffer;
+    UINT count;
+    auto_buffer<COMDLG_FILTERSPEC> filterspec;
+    std::vector<CString> filternames;
+    std::vector<CString> filtermasks;
 };
 
 inline CSelectFileFilter::CSelectFileFilter(UINT stringId)
@@ -30,4 +36,24 @@ inline void CSelectFileFilter::Load(UINT stringId)
     buffer.reset(bufferLength);
     _tcscpy_s (buffer, bufferLength, sFilter);
     CStringUtils::PipesToNulls(buffer);
+    //Certificates|*.p12;*.pkcs12;*.pfx|All|*.*||
+    int pos = 0;
+    CString temp;
+    for (;;)
+    {
+        temp = sFilter.Tokenize(_T("|"), pos);
+        if (temp.IsEmpty())
+        {
+            break;
+        }
+        filternames.push_back(temp);
+        temp = sFilter.Tokenize(_T("|"), pos);
+        filtermasks.push_back(temp);
+    }
+    filterspec.reset(filternames.size());
+    for (int i = 0; i < filternames.size(); ++i)
+    {
+        filterspec[i].pszName = filternames[i];
+        filterspec[i].pszSpec = filtermasks[i];
+    }
 }
