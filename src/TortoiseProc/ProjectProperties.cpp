444d2da245feb9dd6c7ea94fd9529ed518e5aa71
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-01-12 19:32:04 +0000

    In case the tsvn:logsummary regex didn't match anything, use the whole log message instead.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@20688 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/ProjectProperties.cpp
+++ src/TortoiseProc/ProjectProperties.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2010 - TortoiseSVN
+// Copyright (C) 2003-2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -804,6 +804,9 @@ CString ProjectProperties::GetLogSummary(const CString& sMessage)
         catch (exception) {}
     }
     sRet.Trim();
+
+    if (sRet.IsEmpty())
+        return sMessage;
 
     return sRet;
 }
