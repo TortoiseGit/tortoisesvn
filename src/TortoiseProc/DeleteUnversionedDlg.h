565a380e6037a847587b2066aa87f3050ff6afff
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-01-20 19:47:21 +0000

    Patch from Dmitry: remove unused method declarations.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@20742 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/DeleteUnversionedDlg.h
+++ src/TortoiseProc/DeleteUnversionedDlg.h
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2007, 2009-2010 - TortoiseSVN
+// Copyright (C) 2003-2007, 2009-2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -46,8 +46,6 @@ protected:
     afx_msg LRESULT OnSVNStatusListCtrlNeedsRefresh(WPARAM, LPARAM);
 
     DECLARE_MESSAGE_MAP()
-
-    void StartDiff(int fileindex);
 
 private:
     static UINT StatusThreadEntry(LPVOID pVoid);
--- src/TortoiseProc/RevertDlg.h
+++ src/TortoiseProc/RevertDlg.h
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2010 - TortoiseSVN
+// Copyright (C) 2003-2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -50,8 +50,6 @@ protected:
     afx_msg void OnBnClickedDelunversioned();
 
     DECLARE_MESSAGE_MAP()
-
-    void StartDiff(int fileindex);
 
 private:
     static UINT RevertThreadEntry(LPVOID pVoid);
