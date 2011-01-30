4ad153fb454ced4545e13323bb19e9980f0f7e92
stefan.fuhrmann.1974 <stefan.fuhrmann.1974@154be670-7969-47af-9ab0-c6fb378d2295>
2011-01-30 18:12:04 +0000

    add CStringBuffer utility class (somewhat similar to a string builder)
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@20799 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ src/Utils/StringBuffer.cpp
@@ -0,0 +1,72 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2011 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "stdafx.h"
+#include "StringBuffer.h"
+
+void CStringBuffer::Reserve (size_t newCapacity)
+{
+    assert (newCapacity >= capacity);
+
+    newCapacity += ALIGNMENT;
+    char *newMemory = new char[newCapacity +  ALIGNMENT];
+    char *newBuffer = (char*)((size_t)(newMemory + ALIGNMENT-1) & -ALIGNMENT);
+
+    if (buffer)
+        memcpy (newBuffer, buffer, APR_ALIGN(16, size + 1));
+
+    delete memory;
+    memory = newMemory;
+    buffer = newBuffer;
+
+    capacity = newCapacity + ALIGNMENT;
+}
+
+CStringBuffer::CStringBuffer (size_t initialCapacity)
+    : buffer (NULL)
+    , memory (NULL)
+    , capacity (0)
+    , size (0)
+{
+    Reserve (initialCapacity);
+    buffer[0] = 0;
+}
+
+CStringBuffer::~CStringBuffer()
+{
+    delete memory;
+}
+
+void CStringBuffer::Append (const std::string& s)
+{
+    if (size + s.size() >= capacity)
+        Reserve (max (s.size(), 2 * capacity));
+
+    memcpy (buffer + size, s.c_str(), s.size()+1);
+    size += s.size();
+}
+
+void CStringBuffer::Append (const char* s)
+{
+    size_t size = strlen (s);
+    if (this->size + size >= capacity)
+        Reserve (max (size, 2 * capacity));
+
+    memcpy (buffer + this->size, s, size+1);
+    this->size += size;
+}
---
+++ src/Utils/StringBuffer.h
@@ -0,0 +1,125 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2011 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#pragma once
+
+/**
+ * Re-usable char[] buffer. In contrast to std::string, the
+ * data buffer will not be initialized and we can legally and
+ * safely write directly into the buffer.
+ */
+class CStringBuffer
+{
+private:
+
+    /** Align our buffer to SSE data boundaries and
+     * over-allocate by the same amount to allow for
+     * fast (aligned) and sloppy (copying a bit too much)
+     * data access.
+     */
+    enum {ALIGNMENT = sizeof (__m128)};
+
+    /// the buffer that was allocated     
+    char* memory;
+
+    /// the aligned start of our data buffer (memory + 0..15 bytes)
+    char* buffer;
+
+    /// length of the string (excluding the terminating 0)
+    size_t size;
+
+    /// maximum data capacity in buffer
+    size_t capacity;
+
+    // (re-)size management
+
+    void Reserve (size_t newCapacity);
+
+    // copying is not supported
+
+    CStringBuffer (const CStringBuffer&);
+    CStringBuffer& operator=(const CStringBuffer&);
+
+public:
+
+    /// construction
+    CStringBuffer (size_t initialCapacity = 0);
+
+    /// destruction
+    ~CStringBuffer();
+
+    /// data access
+    operator char*();
+    size_t GetSize() const;
+
+    /// Get the first unused element in the buffer.
+    /// Guarantee at least minFree bytes of addressible memory.
+    char* GetBuffer (size_t minFree);
+
+    /// mark additional size bytes of the buffer as used.
+    /// (usually called after GetBuffer())
+    void AddSize (size_t size);
+
+    /// Set total size to 0. Keep internal buffer.
+    void Clear();
+
+    /// add data to buffer.
+    void Append (char c);
+    void Append (const std::string& s);
+    void Append (const char* s);
+};
+
+inline CStringBuffer::operator char*()
+{
+    return buffer;
+}
+
+inline size_t CStringBuffer::GetSize() const
+{
+    return size;
+}
+
+inline char* CStringBuffer::GetBuffer (size_t minFree)
+{
+    if (size + minFree >= capacity)
+        Reserve (max (minFree, 2 * capacity));
+
+    return buffer;
+}
+
+inline void CStringBuffer::AddSize (size_t size)
+{
+    assert (size + this->size < capacity);
+    this->size += size;
+    buffer[this->size] = 0;
+}
+
+inline void CStringBuffer::Clear()
+{
+    size = 0;
+    buffer[0] = 0;
+}
+
+inline void CStringBuffer::Append (char c)
+{
+    if (size + 1 >= capacity)
+        Reserve (max (16, 2 * capacity));
+
+    buffer[size] = c;
+    buffer[++size] = 0;
+}
