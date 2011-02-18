a4c27eb79903612bac85e358d7c68913caa520a0
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-02-18 08:18:02 +0000

    Patch from Dmitry: Use clear() instead of (the wrong!) empty() to empty the string.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@20894 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/Properties/EditPropExecutable.cpp
+++ src/TortoiseProc/Properties/EditPropExecutable.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2010 - TortoiseSVN
+// Copyright (C) 2010-2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -87,7 +87,7 @@ void CEditPropExecutable::OnOK()
     if (bSet)
         m_PropValue = "*";
     else
-        m_PropValue.empty();
+        m_PropValue.clear();
     m_bChanged = true;
 
     CStandAloneDialog::OnOK();
