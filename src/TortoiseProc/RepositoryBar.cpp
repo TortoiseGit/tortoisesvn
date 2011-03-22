550b3681afbac84042f3585e812b97d8634a610a
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-03-22 17:30:40 +0000

    Replace backslashes with forward slashes in the repository browser.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@21038 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/RepositoryBar.cpp
+++ src/TortoiseProc/RepositoryBar.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2010 - TortoiseSVN
+// Copyright (C) 2003-2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -257,6 +257,7 @@ CString CRepositoryBar::GetCurrentUrl() const
     {
         CString path;
         m_cbxUrl.GetWindowText(path);
+        path.Replace('\\', '/');
         return path;
     }
     else
