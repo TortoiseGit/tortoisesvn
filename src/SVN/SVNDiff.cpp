15761724a22bd928de04cc91111e00584acf1ef0
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-03-31 20:08:45 +0000

    Also check for an empty string, not just a NULL string.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@21092 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNDiff.cpp
+++ src/SVN/SVNDiff.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2010 - TortoiseSVN
+// Copyright (C) 2003-2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -187,7 +187,7 @@ bool SVNDiff::DiffFileAgainstBase(
 
             if (infodata)
             {
-                if (infodata->copyfromurl)
+                if (infodata->copyfromurl && infodata->copyfromurl[0])
                     baseRev = infodata->copyfromrev;
                 else
                     baseRev = infodata->lastchangedrev;
