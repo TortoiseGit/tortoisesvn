3becec5b7ebd16eeb84484b35f9635465992f714
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-04-16 14:19:40 +0000

    Remove unused include.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@21165 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/TSVNPath.cpp
+++ src/SVN/TSVNPath.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2010 - TortoiseSVN
+// Copyright (C) 2003-2011 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -28,7 +28,6 @@
 #include "auto_buffer.h"
 
 #if defined(_MFC_VER)
-#include "MessageBox.h"
 #include "AppUtils.h"
 #include "StringUtils.h"
 #endif
