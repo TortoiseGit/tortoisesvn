6ac684b497a257bf8a3b9dd940e8cc5120b81e7d
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-05-25 17:56:44 +0000

    Use the splitter center method instead of trying to do the centering manually.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@21465 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseMerge/MainFrm.cpp
+++ src/TortoiseMerge/MainFrm.cpp
@@ -837,22 +837,8 @@ void CMainFrame::UpdateLayout()
 {
     if (m_bInitSplitter)
     {
-        CRect cr, rclocbar;
-        GetClientRect(&cr);
-        int width = cr.Width();
-        if (::IsWindow(m_wndLocatorBar) && m_wndLocatorBar.IsWindowVisible())
-        {
-            m_wndLocatorBar.GetWindowRect(&rclocbar);
-            width -= rclocbar.Width();
-        }
-        m_wndSplitter.SetRowInfo(0, cr.Height()/2, 0);
-        m_wndSplitter.SetRowInfo(1, cr.Height()/2, 0);
-        m_wndSplitter.SetColumnInfo(0, width / 2, 50);
-        m_wndSplitter2.SetRowInfo(0, cr.Height()/2, 0);
-        m_wndSplitter2.SetColumnInfo(0, width / 2, 50);
-        m_wndSplitter2.SetColumnInfo(1, width / 2, 50);
-
-        m_wndSplitter.RecalcLayout();
+        m_wndSplitter.CenterSplitter();
+        m_wndSplitter2.CenterSplitter();
     }
 }
 
