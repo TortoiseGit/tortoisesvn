67d3ce412f540b0e41f40896f68010d65d8d43b8
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-08-20 08:35:41 +0000

    Properly cast the SVNRev variables in the printf statement.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@21872 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/TreeConflictEditorDlg.cpp
+++ src/TortoiseProc/TreeConflictEditorDlg.cpp
@@ -94,9 +94,9 @@ BOOL CTreeConflictEditorDlg::OnInitDialog()
     CAppUtils::SetWindowTitle(m_hWnd, m_path.GetUIPathString(), sWindowTitle);
 
     CString sTemp;
-    sTemp.Format(_T("%s/%s@%ld"), (LPCTSTR)src_left_version_url, src_left_version_path, src_left_version_rev);
+    sTemp.Format(_T("%s/%s@%ld"), (LPCTSTR)src_left_version_url, src_left_version_path, (svn_revnum_t)src_left_version_rev);
     SetDlgItemText(IDC_SOURCELEFTURL, sTemp);
-    sTemp.Format(_T("%s/%s@%ld"), (LPCTSTR)src_right_version_url, (LPCTSTR)src_right_version_path, src_right_version_rev);
+    sTemp.Format(_T("%s/%s@%ld"), (LPCTSTR)src_right_version_url, (LPCTSTR)src_right_version_path, (svn_revnum_t)src_right_version_rev);
     SetDlgItemText(IDC_SOURCERIGHTURL, sTemp);
 
     if (conflict_reason == svn_wc_conflict_reason_deleted)
