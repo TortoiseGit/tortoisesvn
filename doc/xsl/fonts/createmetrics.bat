ece9c5b11cd9b94a9b7a580e50b75bcab96c3351
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-09-22 08:40:42 +0000

    * add all class paths for FOP * extract from ttc files if there might not be a ttf file
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@22015 154be670-7969-47af-9ab0-c6fb378d2295

--- doc/xsl/fonts/createmetrics.bat
+++ doc/xsl/fonts/createmetrics.bat
@@ -1,9 +1,30 @@
-set LIB=D:\Development\SVN\TortoiseSVN\Tools\fop\lib
-set CMD="c:\Program Files (x86)\Java\jre6\bin\java" -cp "D:\Development\SVN\TortoiseSVN\Tools\fop\build\fop.jar;%LIB%\xmlgraphics-commons-1.4.jar;%LIB%\commons-io-1.3.1.jar;%LIB%\commons-logging-1.0.4.jar;%LIB%\avalon-framework-4.2.0.jar;%LIB%\xercesImpl-2.7.1.jar"
+@echo off
+set LIBDIR=D:\Development\SVN\TortoiseSVN\Tools\fop\lib\
+set LOCAL_FOP_HOME=D:\Development\SVN\TortoiseSVN\Tools\fop\
+
+set LOCALCLASSPATH=%LOCAL_FOP_HOME%build\fop.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LOCAL_FOP_HOME%build\fop-sandbox.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LOCAL_FOP_HOME%build\fop-hyph.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xml-apis-1.3.04.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xml-apis-ext-1.3.04.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xercesImpl-2.7.1.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xalan-2.7.0.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\serializer-2.7.0.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\batik-all-1.7.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xmlgraphics-commons-1.4.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\avalon-framework-4.2.0.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\commons-io-1.3.1.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\commons-logging-1.0.4.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\jai_imageio.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\fop-hyph.jar
+
+
+set CMD="c:\Program Files (x86)\Java\jre6\bin\java" 
+set CMD=%CMD% -cp %LOCALCLASSPATH%
 set CMD=%CMD% org.apache.fop.fonts.apps.TTFReader
 set FONT_DIR=C:\Windows\Fonts
 %CMD% %FONT_DIR%\arial.ttf arial.xml
-%CMD% %FONT_DIR%\arialdb.ttf arialdb.xml
+%CMD% %FONT_DIR%\arialbd.ttf arialbd.xml
 %CMD% %FONT_DIR%\arialbi.ttf arialbi.xml
 %CMD% %FONT_DIR%\ariali.ttf ariali.xml
 %CMD% %FONT_DIR%\cour.ttf cour.xml
@@ -21,3 +42,7 @@ set FONT_DIR=C:\Windows\Fonts
 %CMD% %FONT_DIR%\timesbd.ttf timesbd.xml
 %CMD% %FONT_DIR%\timesbi.ttf timesbi.xml
 %CMD% %FONT_DIR%\timesi.ttf timesi.xml
+
+:: in case the ttf isn't available, try the ttc
+%CMD% -ttcname "MS Gothic" %FONT_DIR%\msgothic.ttc msgothic.xml
+%CMD% -ttcname "SimSun" %FONT_DIR%\simsun.ttc simsun.xml
