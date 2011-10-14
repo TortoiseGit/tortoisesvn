74ccec27786a9bf87692ef7e0c07fc0d911a0429
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2011-10-14 05:42:24 +0000

    Add the manifest to avoid entries about SideBySide problems in the event log. Those entries in the event log can be ignored, but some users don't want any entries there if possible.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@22097 154be670-7969-47af-9ab0-c6fb378d2295

--- src/crashrpt/StdAfx.h
+++ src/crashrpt/StdAfx.h
@@ -23,3 +23,5 @@ extern const LPCTSTR addressFormat;
 extern const LPCTSTR offsetFormat;
 extern const LPCTSTR sizeFormat;
 
+#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
