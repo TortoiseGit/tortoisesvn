19f37ab5db0246c010e178c46d5c9a7bfbc06f23
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2012-01-31 18:51:54 +0000

    Correctly check for different repository roots. (Fixes issue #197) : "Update item to revision" missing in Repo-browser
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@22504 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/RepositoryBrowser.cpp
+++ src/TortoiseProc/RepositoryBrowser.cpp
@@ -1019,7 +1019,7 @@ bool CRepositoryBrowser::ChangeToUrl(CString& url, SVNRev& rev, bool bAlreadyChe
     bool urlHasDifferentRoot
         =    root.IsEmpty()
           || root.Compare (url.Left (root.GetLength()))
-          || (url.GetAt (root.GetLength()) != '/');
+          || ((url.GetAt (root.GetLength()) != '/') && (url.GetLength()!=root.GetLength()));
 
     CString partUrl = url;
     if ((LONG(rev) != LONG(m_repository.revision)) || urlHasDifferentRoot)
