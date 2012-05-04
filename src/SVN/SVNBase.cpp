acc32159b412d0e63f590e6412d9bc29d6a46e6f
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2012-05-04 20:10:10 +0000

    In the ShowErrorDialog() method, also show post-commit errors if there are any. Update issue #289
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@22868 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNBase.cpp
+++ src/SVN/SVNBase.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2010-2011 - TortoiseSVN
+// Copyright (C) 2010-2012 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -203,7 +203,7 @@ int SVNBase::ShowErrorDialog( HWND hParent, const CTSVNPath& wcPath)
 
     typedef HRESULT (WINAPI *TDLG)(const TASKDIALOGCONFIG *pTaskConfig, int *pnButton, int *pnRadioButton, BOOL *pfVerificationFlagChecked);
 
-    CString sError = GetErrorString(Err);
+    CString sError = Err ? GetErrorString(Err) : PostCommitErr;
 
     CAutoLibrary hLib = LoadLibrary(L"Comctl32.dll");
     if (hLib)
