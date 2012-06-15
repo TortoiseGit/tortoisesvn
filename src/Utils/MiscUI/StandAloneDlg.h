6eb1172de8c63f78ce8e203d48f4d4a8db2c67f3
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2012-06-15 18:13:39 +0000

    Calculate the checkbox size from the system info instead of using a fixed value. (Fixes issue #339) : too short checkbox and radio controls in high dpi mode
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23004 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/MiscUI/StandAloneDlg.h
+++ src/Utils/MiscUI/StandAloneDlg.h
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2011 - TortoiseSVN
+// Copyright (C) 2003-2012 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -289,7 +289,8 @@ protected:
                 {
                     // we're dealing with radio buttons and check boxes,
                     // which means we have to add a little space for the checkbox
-                    controlrectorig.right = controlrectorig.left + (controlrect.right - controlrect.left) + 20;
+                    const int checkWidth = GetSystemMetrics(SM_CXMENUCHECK) + 2*GetSystemMetrics(SM_CXEDGE);
+                    controlrectorig.right = controlrectorig.left + (controlrect.right - controlrect.left) + checkWidth;
                     pwndDlgItem->MoveWindow(&controlrectorig);
                 }
             }
