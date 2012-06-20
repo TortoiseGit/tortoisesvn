76598d315361b5494dcd4ec723016c184888398b
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2012-06-20 17:36:54 +0000

    make RestText work on WinXP again.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23015 154be670-7969-47af-9ab0-c6fb378d2295

--- src/ResText/ResModule.cpp
+++ src/ResText/ResModule.cpp
@@ -18,6 +18,7 @@
 #include "StdAfx.h"
 #include "Utils.h"
 #include ".\resmodule.h"
+#include "..\Utils\SysInfo.h"
 #include <regex>
 #include <memory>
 
@@ -169,7 +170,10 @@ BOOL CResModule::CreateTranslatedResources(LPCTSTR lpszSrcLangDllPath, LPCTSTR l
     int count = 0;
     do
     {
-        m_hResDll = LoadLibraryEx (lpszSrcLangDllPath, NULL, LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE|LOAD_LIBRARY_AS_IMAGE_RESOURCE|LOAD_IGNORE_CODE_AUTHZ_LEVEL);
+        if (SysInfo::Instance().IsVistaOrLater())
+            m_hResDll = LoadLibraryEx (lpszSrcLangDllPath, NULL, LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE|LOAD_LIBRARY_AS_IMAGE_RESOURCE|LOAD_IGNORE_CODE_AUTHZ_LEVEL);
+        else
+        m_hResDll = LoadLibraryEx (lpszSrcLangDllPath, NULL, LOAD_LIBRARY_AS_IMAGE_RESOURCE|LOAD_IGNORE_CODE_AUTHZ_LEVEL);
         if (m_hResDll == NULL)
             Sleep(100);
         count++;
