00574c0e46e390f20703420c5bd4f60788fac502
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2012-07-12 19:17:46 +0000

    Remove illegal chars first when using an url as a template for a temp path. (Fixes issue #367) : temp files created with wrong names
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23085 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/TempFile.cpp
+++ src/Utils/TempFile.cpp
@@ -57,7 +57,13 @@ CTSVNPath CTempFiles::ConstructTempPath(const CTSVNPath& path, const SVNRev& rev
         {
             // use the UI path, which does unescaping for urls
             CString filename = path.GetUIFileOrDirectoryName();
-
+            // remove illegal chars which could be present in urls
+            filename.Remove('?');
+            filename.Remove('*');
+            filename.Remove('<');
+            filename.Remove('>');
+            filename.Remove('|');
+            filename.Remove('"');
             // the inner loop assures that the resulting path is < MAX_PATH
             // if that's not possible without reducing the 'filename' to less than 5 chars, use a path
             // that's longer than MAX_PATH (in that case, we can't really do much to avoid longer paths)
