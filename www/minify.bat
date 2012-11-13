9ae9bf57e2bc856616c1012b4b8eb1332689c3e0
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2012-11-13 16:04:49 +0000

    add normalize.css
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23461 154be670-7969-47af-9ab0-c6fb378d2295

--- www/minify.bat
+++ www/minify.bat
@@ -5,5 +5,5 @@ rem npm install -g uglify-js
 
 pushd %~dp0
 
-type css\lightbox.css css\prettify.css css\style.css | cleancss --s0 -o css\pack.css
+type css\lightbox.css css\prettify.css css\normalize.css css\style.css | cleancss --s0 -o css\pack.css
 uglifyjs --no-copyright -o js/lightbox.min.js js/lightbox.js
