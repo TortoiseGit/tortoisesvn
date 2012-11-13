fcb900a29cc80b86d18f0071153b9f926326a36d
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2012-11-13 18:34:09 +0000

    Don't include prettify.css in pack.css since it's only used in one page.  Also use "cmd /c" in minify.bat to bypass a stupid issue with node.js exiting the batch file after each command.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23466 154be670-7969-47af-9ab0-c6fb378d2295

--- www/minify.bat
+++ www/minify.bat
@@ -5,5 +5,6 @@ rem npm install -g uglify-js
 
 pushd %~dp0
 
-type css\lightbox.css css\prettify.css css\normalize.css css\style.css | cleancss --s0 -o css\pack.css
-uglifyjs --no-copyright -o js/lightbox.min.js js/lightbox.js
+type css\lightbox.css css\normalize.css css\style.css | cleancss --s0 -o css\pack.css
+cmd /c cleancss --s0 -o css\prettify.min.css css\prettify.css
+cmd /c uglifyjs --no-copyright -o js\lightbox.min.js js\lightbox.js
