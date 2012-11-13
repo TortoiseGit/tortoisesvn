b6280817d7446ee2a04a2c83cb62239ff7c7685e
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2012-11-13 09:28:13 +0000

    move javascript files in /js folder
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23444 154be670-7969-47af-9ab0-c6fb378d2295

--- www/js/lightbox/lightbox.js
+++ www/js/lightbox/lightbox.js
@@ -46,8 +46,8 @@
 //  Configurationl
 //
 LightboxOptions = Object.extend({
-    fileLoadingImage:        'images/loading.gif',     
-    fileBottomNavCloseImage: 'images/closelabel.png',
+    fileLoadingImage:        '../images/loading.gif',     
+    fileBottomNavCloseImage: '../images/closelabel.png',
 
     overlayOpacity: 0.8,   // controls transparency of shadow overlay
 
@@ -106,7 +106,7 @@ Lightbox.prototype = {
         //              </div>
         //              <div id="loading">
         //                  <a href="#" id="loadingLink">
-        //                      <img src="images/loading.gif">
+        //                      <img src="../images/loading.gif">
         //                  </a>
         //              </div>
         //          </div>
@@ -119,7 +119,7 @@ Lightbox.prototype = {
         //              </div>
         //              <div id="bottomNav">
         //                  <a href="#" id="bottomNavClose">
-        //                      <img src="images/close.gif">
+        //                      <img src="../images/close.gif">
         //                  </a>
         //              </div>
         //          </div>
