051c0c7bab85d780ec4a8f729d3007d14ed3935a
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2012-11-13 13:26:13 +0000

    * update lightbox to v2.51 * minify and combine the css/js files * move js inclusion in footer-js.html * fix prettyprint sort of broken due to the use of tabs
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23447 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ www/minify.bat
@@ -0,0 +1,9 @@
+@echo off
+rem install node.js and then run:
+rem npm install -g clean-css
+rem npm install -g uglify-js
+
+pushd %~dp0
+
+type css\lightbox.css css\style.css | cleancss --s0 -o css\pack.css
+uglifyjs --no-copyright -o js/lightbox.min.js js/lightbox.js
