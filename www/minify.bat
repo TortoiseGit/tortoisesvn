e74cef3fedb2710b98d0f837e49f74b51bbd64ea
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2012-11-14 04:36:02 +0000

    update minify.bat
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23472 154be670-7969-47af-9ab0-c6fb378d2295

--- www/minify.bat
+++ www/minify.bat
@@ -8,3 +8,5 @@ pushd %~dp0
 type css\lightbox.css css\normalize.css css\style.css | cleancss --s0 -o css\pack.css
 cmd /c cleancss --s0 -o css\prettify.min.css css\prettify.css
 cmd /c uglifyjs --no-copyright -o js\lightbox.min.js js\lightbox.js
+
+popd
