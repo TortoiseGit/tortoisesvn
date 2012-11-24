c4a116349fac5fcf0b5e3d4c30688d9987aae045
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2012-11-24 07:47:30 +0000

    switch to uglify-js 2.x
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23500 154be670-7969-47af-9ab0-c6fb378d2295

--- www/minify.bat
+++ www/minify.bat
@@ -7,6 +7,6 @@ pushd %~dp0
 
 type css\lightbox.css css\normalize.css css\style.css | cleancss --s0 -o css\pack.css
 cmd /c cleancss --s0 -o css\prettify.min.css css\prettify.css
-cmd /c uglifyjs --no-copyright -o js\lightbox.min.js js\lightbox.js
+cmd /c uglifyjs js\lightbox.js --compress --mangle -o js\lightbox.min.js
 
 popd
