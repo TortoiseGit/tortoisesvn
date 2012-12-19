92cd533b7d713094688ded45b6fd99607a08d9ab
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2012-12-19 07:49:01 +0000

    * switch to fancybox for the screenshots * add image dimensions and alt text (currently the image filename is used) * style.css: remove useless properties from img * various minor improvements
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23646 154be670-7969-47af-9ab0-c6fb378d2295

--- www/minify.bat
+++ www/minify.bat
@@ -5,8 +5,8 @@ rem npm install -g uglify-js
 
 pushd %~dp0
 
-type css\lightbox.css css\normalize.css css\style.css | cleancss --s0 -o css\pack.css
+type css\jquery.fancybox.css css\normalize.css css\style.css | cleancss --s0 -o css\pack.css
 cmd /c cleancss --s0 -o css\prettify.min.css css\prettify.css
-cmd /c uglifyjs js\lightbox.js --compress --mangle -o js\lightbox.min.js
+cmd /c uglifyjs js\jquery.fancybox.js js\jquery.mousewheel.js --compress --mangle -o js\pack.js
 
 popd
