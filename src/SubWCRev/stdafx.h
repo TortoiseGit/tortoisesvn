d699c17bb60fbf5cdc1468f48da46e97f846a67d
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2012-12-29 17:47:08 +0000

    add missing license headers
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23760 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SubWCRev/stdafx.h
+++ src/SubWCRev/stdafx.h
@@ -1,7 +1,20 @@
-// stdafx.h : include file for standard system include files,
-//  or project specific include files that are used frequently, but
-//      are changed infrequently
-//
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2004-2005, 2007-2008, 2010, 2012 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #pragma once
 
