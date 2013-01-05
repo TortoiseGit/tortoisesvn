40f740835efc742cd846df51bee5c5cc0f6e445e
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2013-01-05 08:49:56 +0000

    Fix copyright headers: * no space between year ranges (XXXX-XXXX) * one space after year lists (XXXX, XXXX)
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23787 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/Debug.cpp
+++ src/Utils/Debug.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2007,2012 - TortoiseSVN
+// Copyright (C) 2007, 2012 - TortoiseSVN 
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
