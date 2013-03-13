57df757744e0d082fbe2e26b68a99f89eefac7df
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2013-03-13 20:05:45 +0000

    To make sure Scintilla can show all chars, set it to UTF8 codepage unconditionally. Also fix the detection of the line encoding. Convert ANSI lines to UTF8 before putting them into Scintilla. Also add two test files. (Fixes issue #464) : TortoiseBlame does not detect utf8/ansi encodings correctly
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23992 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseBlame/TortoiseBlame.cpp
+++ src/TortoiseBlame/TortoiseBlame.cpp
@@ -230,6 +230,7 @@ BOOL TortoiseBlame::OpenFile(const TCHAR *fileName)
     SendEditor(SCI_SETSAVEPOINT);
     SendEditor(SCI_CANCEL);
     SendEditor(SCI_SETUNDOCOLLECTION, 0);
+    SendEditor(SCI_SETCODEPAGE, SC_CP_UTF8);
     ::ShowWindow(wEditor, SW_HIDE);
 
     FILE * File = NULL;
@@ -250,7 +251,6 @@ BOOL TortoiseBlame::OpenFile(const TCHAR *fileName)
 
     m_lowestRev = LONG_MAX;
     m_highestRev = 0;
-    bool bUTF8 = true;
     size_t len = 0;
     LONG linenumber = 0;
     svn_revnum_t rev = 0;
@@ -388,7 +388,8 @@ BOOL TortoiseBlame::OpenFile(const TCHAR *fileName)
             // check each line for illegal utf8 sequences. If one is found, we treat
             // the file as ASCII, otherwise we assume an UTF8 file.
             unsigned char * utf8CheckBuf = (unsigned char*)lineptr;
-            while ((bUTF8)&&(*utf8CheckBuf))
+            bool bUTF8 = false;
+            while (*utf8CheckBuf)
             {
                 if ((*utf8CheckBuf == 0xC0)||(*utf8CheckBuf == 0xC1)||(*utf8CheckBuf >= 0xF5))
                 {
@@ -397,6 +398,7 @@ BOOL TortoiseBlame::OpenFile(const TCHAR *fileName)
                 }
                 if ((*utf8CheckBuf & 0xE0)==0xC0)
                 {
+                    bUTF8 = true;
                     utf8CheckBuf++;
                     if (*utf8CheckBuf == 0)
                         break;
@@ -408,6 +410,7 @@ BOOL TortoiseBlame::OpenFile(const TCHAR *fileName)
                 }
                 if ((*utf8CheckBuf & 0xF0)==0xE0)
                 {
+                    bUTF8 = true;
                     utf8CheckBuf++;
                     if (*utf8CheckBuf == 0)
                         break;
@@ -427,6 +430,7 @@ BOOL TortoiseBlame::OpenFile(const TCHAR *fileName)
                 }
                 if ((*utf8CheckBuf & 0xF8)==0xF0)
                 {
+                    bUTF8 = true;
                     utf8CheckBuf++;
                     if (*utf8CheckBuf == 0)
                         break;
@@ -455,7 +459,13 @@ BOOL TortoiseBlame::OpenFile(const TCHAR *fileName)
 
                 utf8CheckBuf++;
             }
-            SendEditor(SCI_ADDTEXT, strlen(lineptr), reinterpret_cast<LPARAM>(static_cast<char *>(lineptr)));
+            if (!bUTF8)
+            {
+                std::string utf8line = CUnicodeUtils::StdAnsiToUTF8(lineptr);
+                SendEditor(SCI_ADDTEXT, utf8line.length(), reinterpret_cast<LPARAM>(utf8line.c_str()));
+            }
+            else
+                SendEditor(SCI_ADDTEXT, strlen(lineptr), reinterpret_cast<LPARAM>(static_cast<char *>(lineptr)));
         }
         if (len == 0)
             break;
@@ -502,9 +512,6 @@ BOOL TortoiseBlame::OpenFile(const TCHAR *fileName)
     };
 
     fclose(File);
-
-    if (bUTF8)
-        SendEditor(SCI_SETCODEPAGE, SC_CP_UTF8);
 
     SendEditor(SCI_SETUNDOCOLLECTION, 1);
     ::SetFocus(wEditor);
