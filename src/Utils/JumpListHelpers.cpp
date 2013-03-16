8cdd1ff851ec61b429816ea19f865e90354fce58
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2013-03-16 08:16:08 +0000

    remove unneeded variable assignments
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@24009 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/JumpListHelpers.cpp
+++ src/Utils/JumpListHelpers.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2009-2012 - TortoiseSVN
+// Copyright (C) 2009-2013 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -136,6 +136,6 @@ void DeleteJumpList(LPCTSTR appID)
     HRESULT hr = pcdl.CoCreateInstance(CLSID_DestinationList, NULL, CLSCTX_INPROC_SERVER);
     if (SUCCEEDED(hr))
     {
-        hr = pcdl->DeleteList(appID);
+        pcdl->DeleteList(appID);
     }
 }
--- src/Utils/ListViewAccServer.cpp
+++ src/Utils/ListViewAccServer.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2009-2012 - TortoiseSVN
+// Copyright (C) 2009-2013 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -115,6 +115,6 @@ void ListViewAccServer::ClearProvider( HWND hControl )
     if (hr == S_OK && pAccPropSvc)
     {
         MSAAPROPID propid = PROPID_ACC_HELP;
-        hr = pAccPropSvc->ClearHwndProps(hControl, (DWORD)OBJID_CLIENT, CHILDID_SELF, &propid, 1);
+        pAccPropSvc->ClearHwndProps(hControl, (DWORD)OBJID_CLIENT, CHILDID_SELF, &propid, 1);
     }
 }
