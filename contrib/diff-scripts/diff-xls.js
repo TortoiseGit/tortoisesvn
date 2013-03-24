b75b9961157635b9f5503e94f2272ab966869adb
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2013-03-24 08:54:56 +0000

    diff-xls.js: fix typo
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@24048 154be670-7969-47af-9ab0-c6fb378d2295

--- contrib/diff-scripts/diff-xls.js
+++ contrib/diff-scripts/diff-xls.js
@@ -116,7 +116,7 @@ objExcelApp.Visible = true;
 // Arrange windows
 if (objBaseWorkbook.ProtectWindows || objNewWorkbook.ProtectWindows)
 {
-    StoreWarning("Unable to arrange windows because one or both workbooks are protected.");
+    StoreWarning("Unable to arrange windows because one or both Workbooks are protected.");
 }
 else
 {
@@ -131,7 +131,7 @@ else
 
 if (!bFastMode && objNewWorkbook.ProtectWindows)
 {
-    StoreWarning("Fallback to fast mode bacause " + objNewWorkbook.Name + " is protected.");
+    StoreWarning("Fall back to fast mode because " + objNewWorkbook.Name + " is protected.");
     bFastMode = true;
 }
 
