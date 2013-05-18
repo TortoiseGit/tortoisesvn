cec7ab31779272cbd0acafc8c15522f3569df3e6
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2013-05-18 15:20:36 +0000

    Create a dummy private default constructor: the default constructor isn't used, and making it private ensures that it won't get used accidentally.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@24212 154be670-7969-47af-9ab0-c6fb378d2295

--- src/LogCache/LogCachePool.h
+++ src/LogCache/LogCachePool.h
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2007-2007 - TortoiseSVN
+// Copyright (C) 2007-2007, 2013 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -77,6 +77,7 @@ private:
 
     void AutoRemoveUnused();
 
+    CLogCachePool() {}
 public:
 
     /// construction / destruction
