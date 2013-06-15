213682da4f45b4ec42bfc5eb42c41c01717a2510
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2013-06-15 08:49:13 +0000

    Implement printing from TortoiseUDiff. (Closes issue #480) : Print ability for TortoiseUDiff
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@24387 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseUDiff/stdafx.h
+++ src/TortoiseUDiff/stdafx.h
@@ -7,7 +7,6 @@
 
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
-#define WIN32_LEAN_AND_MEAN     // Exclude rarely-used stuff from Windows headers
 #ifndef WINVER                  // Specifies that the minimum required platform is Windows Vista.
 #define WINVER 0x0600           // Change this to the appropriate value to target other versions of Windows.
 #endif
