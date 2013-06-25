TortoiseGit/tortoisesvn

commit 237a75d8da904ca981910a406fec6e8a57a2fde0

Validate the drawing area after the custom painting of the control. This shouldn't be necessary, but it appears some plugins can mess up the painting somehow. See this thread for more details: http://tortoisesvn.tigris.org/ds/viewMessage.do?dsForumId=4061&dsMessageId=3011957
git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@23876 154be670-7969-47af-9ab0-c6fb378d2295

src/SVN/SVNStatusListCtrl.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2012 - TortoiseSVN
+// Copyright (C) 2003-2013 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -5147,6 +5147,7 @@ void CSVNStatusListCtrl::OnPaint()
         }
         ReleaseDC(pDC);
     }
+    ValidateRect(NULL);
 }
 
 // prevent users from extending our hidden (size 0) columns

src/Utils/MiscUI/FilterEdit.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2007, 2009, 2011-2012 - TortoiseSVN
+// Copyright (C) 2007, 2009, 2011-2013 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -352,6 +352,7 @@ void CFilterEdit::OnPaint()
     Default();
 
     DrawDimText();
+    ValidateRect(NULL);
 
     return;
 }

src/Utils/MiscUI/HintCtrl.h
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2011 - TortoiseSVN
+// Copyright (C) 2011, 2013 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -93,6 +93,7 @@ protected:
             }
             ReleaseDC(pDC);
         }
+        ValidateRect(NULL);
     }
 
 private:

commit 10b9164f006da7e33481ecbd4b762c3006ec73f8

Only validate the update rectangle, not the whole window rectangle. This bug was introduced in r23876. (Fixes issue #494) : renaming in repo browser shows empty cell
git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@24450 154be670-7969-47af-9ab0-c6fb378d2295

src/SVN/SVNStatusListCtrl.cpp
@@ -5171,7 +5171,9 @@ void CSVNStatusListCtrl::OnPaint()
         }
         ReleaseDC(pDC);
     }
-    ValidateRect(NULL);
+    CRect rc;
+    GetUpdateRect(&rc, FALSE);
+    ValidateRect(rc);
 }
 
 // prevent users from extending our hidden (size 0) columns

src/Utils/MiscUI/FilterEdit.cpp
@@ -352,7 +352,9 @@ void CFilterEdit::OnPaint()
     Default();
 
     DrawDimText();
-    ValidateRect(NULL);
+    CRect rc;
+    GetUpdateRect(&rc, FALSE);
+    ValidateRect(rc);
 
     return;
 }

src/Utils/MiscUI/HintCtrl.h
@@ -93,7 +93,9 @@ protected:
             }
             ReleaseDC(pDC);
         }
-        ValidateRect(NULL);
+        CRect rc;
+        GetUpdateRect(&rc, FALSE);
+        ValidateRect(rc);
     }
 
 private: