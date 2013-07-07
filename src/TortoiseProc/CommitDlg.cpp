1895769c436d54372fe64d6ba58b124a2d90d54a
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2013-07-07 10:49:37 +0000

    Don't show that an update is available if the update check got disabled. (Fixes issue #509) : VersionCheck = false not working like it should
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@24505 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/CommitDlg.cpp
+++ src/TortoiseProc/CommitDlg.cpp
@@ -1552,6 +1552,8 @@ void CCommitDlg::UpdateCheckLinks()
 
 void CCommitDlg::VersionCheck()
 {
+    if (CRegDWORD(_T("Software\\TortoiseSVN\\VersionCheck"), TRUE) == FALSE)
+        return;
     CRegString regVer(_T("Software\\TortoiseSVN\\NewVersion"));
     CString vertemp = regVer;
     int major = _ttoi(vertemp);
