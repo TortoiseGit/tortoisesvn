88408d7f2d810bd39ab15942a676d6b61a65db30
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2013-08-16 06:10:08 +0000

    Fix line endings.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@24672 154be670-7969-47af-9ab0-c6fb378d2295

--- src/LogCache/Streams/StreamException.cpp
+++ src/LogCache/Streams/StreamException.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2009 - TortoiseSVN
+// Copyright (C) 2009, 2013 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
