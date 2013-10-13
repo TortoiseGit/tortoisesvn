f85027216779ced98c06105f5cd85c513f76c3cc
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2013-10-13 18:03:25 +0000

    Update coverity.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@24836 154be670-7969-47af-9ab0-c6fb378d2295

--- coverity.bat
+++ coverity.bat
@@ -1,4 +1,4 @@
-"D:\Development\cov-analysis-win64-6.5.1\bin\cov-build.exe" --dir cov-int TSVN-covbuild.bat
+"D:\Development\cov-analysis-win64-6.6.1\bin\cov-build.exe" --dir cov-int TSVN-covbuild.bat
 "%PROGRAMFILES%\7-zip\7z.exe" a -ttar TortoiseSVN.tar cov-int
 "%PROGRAMFILES%\7-zip\7z.exe" a -tgzip TortoiseSVN.tgz TortoiseSVN.tar
 del TortoiseSVN.tar
