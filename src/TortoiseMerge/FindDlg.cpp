7859112e1878f67c7db1d004675fc4bdc59577f1
ch3cooli@gmail.com <ch3cooli@gmail.com@154be670-7969-47af-9ab0-c6fb378d2295>
2013-10-17 15:17:48 +0000

    TortoiseMerge: Add Status label in FindDlg. (Updates issue #575) : TortoiseMerge: Support more search functions
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@24857 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseMerge/FindDlg.cpp
+++ src/TortoiseMerge/FindDlg.cpp
@@ -34,6 +34,7 @@ CFindDlg::CFindDlg(CWnd* pParent /*=NULL*/)
     , m_bLimitToDiffs(FALSE)
     , m_bWholeWord(FALSE)
     , m_FindMsg(0)
+    , m_clrFindStatus(RGB(0, 0, 255))
     , m_regMatchCase(L"Software\\TortoiseMerge\\FindMatchCase", FALSE)
     , m_regLimitToDiffs(L"Software\\TortoiseMerge\\FindLimitToDiffs", FALSE)
     , m_regWholeWord(L"Software\\TortoiseMerge\\FindWholeWord", FALSE)
@@ -51,12 +52,14 @@ void CFindDlg::DoDataExchange(CDataExchange* pDX)
     DDX_Check(pDX, IDC_LIMITTODIFFS, m_bLimitToDiffs);
     DDX_Check(pDX, IDC_WHOLEWORD, m_bWholeWord);
     DDX_Control(pDX, IDC_FINDCOMBO, m_FindCombo);
+    DDX_Control(pDX, IDC_FINDSTATUS, m_FindStatus);
 }
 
 
 BEGIN_MESSAGE_MAP(CFindDlg, CDialog)
     ON_CBN_EDITCHANGE(IDC_FINDCOMBO, &CFindDlg::OnCbnEditchangeFindcombo)
     ON_BN_CLICKED(IDC_COUNT, &CFindDlg::OnBnClickedCount)
+    ON_WM_CTLCOLOR()
 END_MESSAGE_MAP()
 
 
@@ -65,6 +68,7 @@ END_MESSAGE_MAP()
 void CFindDlg::OnCancel()
 {
     m_bTerminating = true;
+    SetStatusText(_T(""));
     if (m_pParent)
         m_pParent->SendMessage(m_FindMsg);
     else if (GetParent())
@@ -80,6 +84,7 @@ void CFindDlg::PostNcDestroy()
 void CFindDlg::OnOK()
 {
     UpdateData();
+    SetStatusText(_T(""));
     m_FindCombo.SaveHistory();
     m_regMatchCase = m_bMatchCase;
     m_regLimitToDiffs = m_bLimitToDiffs;
@@ -123,6 +128,7 @@ void CFindDlg::OnCbnEditchangeFindcombo()
 void CFindDlg::OnBnClickedCount()
 {
     UpdateData();
+    SetStatusText(_T(""));
     m_FindCombo.SaveHistory();
     m_regMatchCase = m_bMatchCase;
     m_regLimitToDiffs = m_bLimitToDiffs;
@@ -136,4 +142,25 @@ void CFindDlg::OnBnClickedCount()
     else if (GetParent())
         GetParent()->SendMessage(m_FindMsg, FindType::Count);
     m_bFindNext = false;
+}
+
+HBRUSH CFindDlg::OnCtlColor(CDC* pDC, CWnd *pWnd, UINT nCtlColor)
+{
+    switch (nCtlColor)
+    {
+    case CTLCOLOR_STATIC:
+        if (pWnd == &m_FindStatus)
+        {
+            pDC->SetTextColor(m_clrFindStatus);
+            return (HBRUSH)GetStockObject(WHITE_BRUSH);
+        }
+    default:
+        return CDialog::OnCtlColor(pDC, pWnd, nCtlColor);
+    }
+}
+
+void CFindDlg::SetStatusText(const CString& str, COLORREF color)
+{
+    m_clrFindStatus = color;
+    m_FindStatus.SetWindowText(str);
 }
