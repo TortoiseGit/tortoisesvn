f96c44d0a9737a3e26d9b037df6acc4260232ff8
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2013-12-19 19:23:48 +0000

    The context menus can call commands that access the property data: guard against access. (Fixes issue #601) : Crash fetching properties in CfM dialog
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@25060 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNStatusListCtrl.cpp
+++ src/SVN/SVNStatusListCtrl.cpp
@@ -2956,7 +2956,10 @@ void CSVNStatusListCtrl::OnContextMenuList(CWnd * pWnd, CPoint point)
     bool bShift = !!(GetAsyncKeyState(VK_SHIFT) & 0x8000);
 
     CAutoReadWeakLock readLock(m_guard);
+    CAutoReadWeakLock readLockProp(m_PropertyMapGuard);
     if (!readLock.IsAcquired())
+        return;
+    if (!readLockProp.IsAcquired())
         return;
     bool bInactiveItem = false;
     int selIndex = GetSelectionMark();
@@ -4089,7 +4092,10 @@ void CSVNStatusListCtrl::OnContextMenuHeader(CWnd * pWnd, CPoint point)
         point = rect.CenterPoint();
     }
     CAutoReadWeakLock readLock(m_guard);
+    CAutoReadWeakLock readLockProp(m_PropertyMapGuard);
     if (!readLock.IsAcquired())
+        return;
+    if (!readLockProp.IsAcquired())
         return;
     CMenu popup;
     if (!popup.CreatePopupMenu())
