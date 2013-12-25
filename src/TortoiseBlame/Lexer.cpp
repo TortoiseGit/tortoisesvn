668f63ca842debd2655b26d71742b7aaeaa0a261
ch3cooli@gmail.com <ch3cooli@gmail.com@154be670-7969-47af-9ab0-c6fb378d2295>
2013-12-25 12:41:17 +0000

    TortoiseBlame: Disable Scintilla C preprocessor tracking to avoid blue wavy lines. (Fixes issue #603) : TortoiseBlame: Blue wavy lines in C header files
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@25074 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseBlame/Lexer.cpp
+++ src/TortoiseBlame/Lexer.cpp
@@ -428,4 +428,5 @@ void TortoiseBlame::SetupCppLexer()
     SetAStyle(SCE_C_IDENTIFIER, RGB(0, 0, 0));
     SetAStyle(SCE_C_PREPROCESSOR, RGB(0x80, 0, 0));
     SetAStyle(SCE_C_OPERATOR, RGB(0x80, 0x80, 0));
+    SendEditor(SCI_SETPROPERTY, (WPARAM)"lexer.cpp.track.preprocessor", (LPARAM)"0");
 }
