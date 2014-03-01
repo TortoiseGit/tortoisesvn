22474703035acd26b542e69bc9a1014bbcdfbb60
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2014-03-01 12:21:29 +0000

    Use the escaped url to determine the relative url since the repo root url is also escaped. (Fixes issue #620) : merge message truncates url
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@25257 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/SVNProgressDlg.cpp
+++ src/TortoiseProc/SVNProgressDlg.cpp
@@ -4035,7 +4035,9 @@ void CSVNProgressDlg::GenerateMergeLogMessage()
 {
     CString sUUID = GetUUIDFromPath(m_targetPathList[0]);
     CString sRepositoryRoot = GetRepositoryRoot(m_targetPathList[0]);
-    CString relUrl = m_url.GetSVNPathString().Mid(sRepositoryRoot.GetLength()+1);
+    CString escapedUrl = CUnicodeUtils::GetUnicode(m_url.GetSVNApiPath(pool));
+    CString relUrl = escapedUrl.Mid(sRepositoryRoot.GetLength() + 1);
+    relUrl = CPathUtils::PathUnescape(relUrl);
     CString sSeparator = CRegString(L"Software\\TortoiseSVN\\MergeLogSeparator", L"........");
     CString sRevListRange;
     CString sRevList;
