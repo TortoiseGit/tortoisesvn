75dbdadfa4bd3e1cc59773204fbf8ff4111a8892
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2014-03-14 05:52:22 +0000

    Update coverity.bat.
    Move it in contrib and update it to work with VS2013 and the latest coverity version.
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@25304 154be670-7969-47af-9ab0-c6fb378d2295

--- contrib/coverity.bat
+++ contrib/coverity.bat
@@ -4,7 +4,7 @@ setlocal
 pushd %~dp0
 
 rem you can set the COVDIR variable to your coverity path
-if not defined COVDIR set "COVDIR=C:\cov-analysis-win32-6.6.1"
+if not defined COVDIR set "COVDIR=C:\cov-analysis-win32-7.0.2"
 if defined COVDIR if not exist "%COVDIR%" (
   echo.
   echo ERROR: Coverity not found in "%COVDIR%"
@@ -17,19 +17,19 @@ set "PERL_PATH=C:\Perl\perl\bin"
 set "PYTHON_PATH=C:\Python27"
 set "PATH=%NANT_PATH%;%PERL_PATH%;%PYTHON_PATH%;%PATH%"
 
-call "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 
 rem cleanup the cov-dir if it's present
 if exist "cov-int" rd /q /s "cov-int"
 
 rem we need to build the libraries before our files
-title nant clean ipv6 xp Subversion
-nant clean ipv6 xp Subversion
+title nant -buildfile:../default.build clean ipv6 Subversion
+nant -buildfile:../default.build clean ipv6 Subversion
 
 rem the actual coverity command
-title "%COVDIR%\bin\cov-build.exe" --dir "cov-int" nant clean ipv6 xp TortoiseSVN
-"%COVDIR%\bin\cov-build.exe" --dir "cov-int" nant clean ipv6 xp TortoiseSVN
-"%COVDIR%\bin\cov-build.exe" --dir "cov-int" nant clean xp Overlays
+title "%COVDIR%\bin\cov-build.exe" --dir "cov-int" nant -buildfile:../default.build clean ipv6 TortoiseSVN
+"%COVDIR%\bin\cov-build.exe" --dir "cov-int" nant -buildfile:../default.build clean ipv6 TortoiseSVN
+"%COVDIR%\bin\cov-build.exe" --dir "cov-int" nant -buildfile:../default.build clean Overlays
 
 if exist "TortoiseSVN.tar" del "TortoiseSVN.tar"
 if exist "TortoiseSVN.tgz" del "TortoiseSVN.tgz"
