fee8a99ee6e084459a6a67e89d2b9fc4989b611b
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2014-12-31 16:24:53 +0000

    based on https://code.google.com/p/tortoisegit/source/detail?r=fa7295b820be9ca368924b8129253df52c189c9d Allow to put URLs into "<>" and allow spaces and other chars there (as the RichEdit control does)
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26162 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/MiscUI/SciEdit.cpp
+++ src/Utils/MiscUI/SciEdit.cpp
@@ -1330,7 +1330,8 @@ bool CSciEdit::IsValidURLChar(unsigned char ch)
 {
     return isalnum(ch) ||
         ch == '_' || ch == '/' || ch == ';' || ch == '?' || ch == '&' || ch == '=' ||
-        ch == '%' || ch == ':' || ch == '.' || ch == '#' || ch == '-' || ch == '+';
+        ch == '%' || ch == ':' || ch == '.' || ch == '#' || ch == '-' || ch == '+' ||
+        ch == '|' || ch == '>' || ch == '<';
 }
 
 void CSciEdit::StyleURLs(int startstylepos, int endstylepos)
@@ -1351,7 +1352,7 @@ void CSciEdit::StyleURLs(int startstylepos, int endstylepos)
     CStringA msg = textbuffer.get();
 
     int starturl = -1;
-    for(int i = 0; i <= msg.GetLength(); )
+    for (int i = 0; i <= msg.GetLength(); AdvanceUTF8(msg, i))
     {
         if ((i < len) && IsValidURLChar(msg[i]))
         {
@@ -1360,11 +1361,26 @@ void CSciEdit::StyleURLs(int startstylepos, int endstylepos)
         }
         else
         {
-            if ((starturl >= 0) && IsUrl(msg.Mid(starturl, i - starturl)))
+            if (starturl >= 0)
             {
+                bool strip = true;
+                if (msg[starturl] == '<' && i < len) // try to detect and do not strip URLs put within <>
+                {
+                    while (msg[starturl] == '<' && starturl <= i) // strip leading '<'
+                        ++starturl;
+                    strip = false;
+                    i = starturl;
+                    while (i < len && msg[i] != '\r' && msg[i] != '\n' && msg[i] != '>') // find first '>' or new line after resetting i to start position
+                        AdvanceUTF8(msg, i);
+                }
+                if (!IsUrl(msg.Mid(starturl, i - starturl)))
+                {
+                    starturl = -1;
+                    continue;
+                }
                 ASSERT(startstylepos + i <= endstylepos);
                 int skipTrailing = 0;
-                while (i - skipTrailing - 1 > starturl && (msg[i - skipTrailing - 1] == '.' || msg[i - skipTrailing - 1] == '-' || msg[i - skipTrailing - 1] == '?' || msg[i - skipTrailing - 1] == ';' || msg[i - skipTrailing - 1] == ':'))
+                while (strip && i - skipTrailing - 1 > starturl && (msg[i - skipTrailing - 1] == '.' || msg[i - skipTrailing - 1] == '-' || msg[i - skipTrailing - 1] == '?' || msg[i - skipTrailing - 1] == ';' || msg[i - skipTrailing - 1] == ':' || msg[i - skipTrailing - 1] == '>' || msg[i - skipTrailing - 1] == '<'))
                     ++skipTrailing;
                 ASSERT(startstylepos + i - skipTrailing <= endstylepos);
                 Call(SCI_STARTSTYLING, startstylepos + starturl, STYLE_MASK);
@@ -1372,7 +1388,6 @@ void CSciEdit::StyleURLs(int startstylepos, int endstylepos)
             }
             starturl = -1;
         }
-        AdvanceUTF8(msg, i);
     }
 }
 
