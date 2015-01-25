f940b779be2c8aea43bfe0bab610d3f7a2e0ae2e
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2015-01-25 01:03:39 +0000

    Remove trailing whitespace.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26228 154be670-7969-47af-9ab0-c6fb378d2295

--- contrib/choco/install.ps1
+++ contrib/choco/install.ps1
@@ -1,5 +1,5 @@
 param($installPath, $toolsPath, $package, $project)
- 
+
 Write-Host 'This is not a package with dlls to reference in it.'
 Write-Host 'Please install using chocolatey'
 Write-Host "To get chocolatey just run 'Install-Package chocolatey' followed by 'Initialize-Chocolatey'"
