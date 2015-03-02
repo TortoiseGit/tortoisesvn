d4892ae493474b67d57b310659d0b95093aedbb6
XhmikosR <XhmikosR@154be670-7969-47af-9ab0-c6fb378d2295>
2015-03-02 17:18:56 +0000

    Utils/MiscUI/Picture.cpp: fix swprintf_s type specifier.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26329 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/MiscUI/Picture.cpp
+++ src/Utils/MiscUI/Picture.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2014 - TortoiseSVN
+// Copyright (C) 2003-2015 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -113,7 +113,7 @@ tstring CPicture::GetFileSizeAsText(bool bAbbrev /* = true */)
     if (bAbbrev)
         StrFormatByteSize(m_nSize, buf, _countof(buf));
     else
-        swprintf_s(buf, L"%ld Bytes", m_nSize);
+        swprintf_s(buf, L"%lu Bytes", m_nSize);
 
     return tstring(buf);
 }
