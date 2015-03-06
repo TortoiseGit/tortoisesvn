f38b7f68a3c9441380f2edca9756128042fe2c9f
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2015-03-06 18:28:48 +0000

    Only post the message if the graph window is active.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26346 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/RevisionGraph/RevisionGraphDlg.cpp
+++ src/TortoiseProc/RevisionGraph/RevisionGraphDlg.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2014 - TortoiseSVN
+// Copyright (C) 2003-2015 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -311,7 +311,8 @@ bool CRevisionGraphDlg::UpdateData()
     }
 
     CoUninitialize();
-    m_Graph.PostMessage (CRevisionGraphWnd::WM_WORKERTHREADDONE, 0, 0);
+    if (::IsWindow(m_Graph))
+        m_Graph.PostMessage (CRevisionGraphWnd::WM_WORKERTHREADDONE, 0, 0);
 
     return true;
 }
