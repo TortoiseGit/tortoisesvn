367b6019e9edf925227e29dfb3701103c9d12ba4
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2015-03-07 18:17:53 +0000

    Due to complaints from the TGit guys: add a copyright header stating that at least some code was used from their project.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26354 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/MiscUI/SciEdit.cpp
+++ src/Utils/MiscUI/SciEdit.cpp
@@ -1,6 +1,7 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
 // Copyright (C) 2003-2015 - TortoiseSVN
+// Copyright (C) 2015 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
