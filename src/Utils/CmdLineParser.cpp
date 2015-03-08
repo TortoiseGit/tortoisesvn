0fecfea1a6a8acb4331b701ab146c3c38a5cfa30
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2015-03-08 10:43:17 +0000

    fix wrong year.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26361 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/CmdLineParser.cpp
+++ src/Utils/CmdLineParser.cpp
@@ -1,7 +1,7 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
 // Copyright (C) 2003-2006, 2009, 2011, 2014-2015 - TortoiseSVN
-// Copyright (C) 2103 - TortoiseGit
+// Copyright (C) 2013 - TortoiseGit
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
