115ca62df3e506ac805e572285c24b0d279f3c3c
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2015-05-11 19:03:35 +0000

    Use a separate hash to read the already synched auth data: we don't need the hash, we only need to see if reading succeeds.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26505 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNAuthData.cpp
+++ src/SVN/SVNAuthData.cpp
@@ -310,8 +310,9 @@ bool SVNAuthData::ExportAuthData(const CString& targetpath, const CString& passw
 
         if (!overwrite)
         {
-            Err = svn_config_read_auth_data(&hash, std::get<0>(ad).c_str(), std::get<1>(ad).c_str(), targetpathA.c_str(), subpool);
-            if ((Err != nullptr) || (hash != nullptr))
+            apr_hash_t * hash2 = nullptr;
+            Err = svn_config_read_auth_data(&hash2, std::get<0>(ad).c_str(), std::get<1>(ad).c_str(), targetpathA.c_str(), subpool);
+            if ((Err != nullptr) || (hash2 != nullptr))
             {
                 svn_error_clear(Err);
                 continue;
@@ -377,8 +378,9 @@ bool SVNAuthData::ImportAuthData(const CString& importpath, const CString& passw
 
         if (!overwrite)
         {
-            Err = svn_config_read_auth_data(&hash, std::get<0>(ad).c_str(), std::get<1>(ad).c_str(), g_pConfigDir, subpool);
-            if ((Err != nullptr) || (hash != nullptr))
+            apr_hash_t * hash2 = nullptr;
+            Err = svn_config_read_auth_data(&hash2, std::get<0>(ad).c_str(), std::get<1>(ad).c_str(), g_pConfigDir, subpool);
+            if ((Err != nullptr) || (hash2 != nullptr))
             {
                 svn_error_clear(Err);
                 continue;
