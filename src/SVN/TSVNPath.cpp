be62f43321f72434a46dcc581fd7f50a87cb8e0b
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2015-05-12 19:07:18 +0000

    Don't call the shell delete function if there is nothing to delete. This fixes issue #692
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26511 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/TSVNPath.cpp
+++ src/SVN/TSVNPath.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2014 - TortoiseSVN
+// Copyright (C) 2003-2015 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -1082,6 +1082,8 @@ void CTSVNPathList::SortByPathname(bool bReverse /*= false*/)
 
 void CTSVNPathList::DeleteAllPaths(bool bTrash, bool bFilesOnly, HWND hErrorWnd)
 {
+    if (m_paths.empty())
+        return;
     PathVector::const_iterator it;
     SortByPathname (true); // nested ones first
 
@@ -1097,6 +1099,8 @@ void CTSVNPathList::DeleteAllPaths(bool bTrash, bool bFilesOnly, HWND hErrorWnd)
             sPaths += '\0';
         }
     }
+    if (sPaths.IsEmpty())
+        return;
     sPaths += '\0';
     sPaths += '\0';
     DeleteViaShell((LPCTSTR)sPaths, bTrash, hErrorWnd);
