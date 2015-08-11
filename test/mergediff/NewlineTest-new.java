ca95ee49f8dad232da5b5105c107124005298e38
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2015-08-11 19:33:26 +0000

    * Improve check for newlines: if a LFCR is found, assume LF and CRLF if at the start of the file. * add test files.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26688 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ test/mergediff/NewlineTest-new.java
@@ -0,0 +1,10 @@
+package someproject.model.decoder;
+
+public class TT2Decoder {
+    
+    int getTraceYear ()
+    {
+    	return trace_year; // year not available from trace header (only month+day+time), so assume current year. 
+    }
+    
+}
---
+++ test/mergediff/NewlineTest-old.java
@@ -0,0 +1,10 @@
+package someproject.model.decoder;
+
+public class TT2Decoder {
+    
+    int getTraceYear ()
+    {
+    	return trace_year; // TODO! from file header
+    }
+    
+}
