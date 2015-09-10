393df105690ebda72cce346e0ebcdf7fc1e6b239
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2015-09-10 17:17:21 +0000

    use the XP UI theme for TortoisePlink. Based on f255000ec9309e653865a96b11212233612963ab of TortoiseGit.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26769 154be670-7969-47af-9ab0-c6fb378d2295

--- ext/TortoisePlink/windows/TPlink/TortoisePlink.c
+++ ext/TortoisePlink/windows/TPlink/TortoisePlink.c
@@ -2,7 +2,10 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 int WinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow)
 {
+    InitCommonControls();
     main(__argc,__argv);
 }
