31cfc427354c490235c427e49d4d791a28f466fa
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2015-09-12 12:05:42 +0000

    fix linker error.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26770 154be670-7969-47af-9ab0-c6fb378d2295

--- ext/TortoisePlink/windows/TPlink/TortoisePlink.c
+++ ext/TortoisePlink/windows/TPlink/TortoisePlink.c
@@ -1,8 +1,11 @@
+#include <Windows.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <Commctrl.h>
 
 #pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(lib, "Crypt32.lib")
 
 int WinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow)
 {
