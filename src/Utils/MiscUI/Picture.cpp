9f53389702b42f9cb503d943dbd937b96ec6d650
csware <csware@154be670-7969-47af-9ab0-c6fb378d2295>
2015-09-27 13:05:54 +0000

    Correctly set width and height for 256x256 pixel icons
    Based on TortoiseGit revision a085c2c8f0ccb0d6573bc8e34638311385c46b78.
    
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26828 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/MiscUI/Picture.cpp
+++ src/Utils/MiscUI/Picture.cpp
@@ -220,9 +220,13 @@ bool CPicture::Load(tstring sFilePathName)
                                         bResult = true;
                                         for (int i=0; i<lpIconDir->idCount; ++i)
                                         {
+                                            if (lpIconDir->idEntries[i].bHeight == 0)
+                                                lpIconDir->idEntries[i].bHeight = 0xff;
+                                            if (lpIconDir->idEntries[i].bWidth == 0)
+                                                lpIconDir->idEntries[i].bWidth = 0xff;
                                             hIcons[i] = (HICON)LoadImage(NULL, sFilePathName.c_str(), IMAGE_ICON,
-                                                                         lpIconDir->idEntries[i].bWidth == 0 ? 256 : lpIconDir->idEntries[i].bWidth,
-                                                                         lpIconDir->idEntries[i].bHeight == 0 ? 256 : lpIconDir->idEntries[i].bHeight,
+                                                                         lpIconDir->idEntries[i].bWidth,
+                                                                         lpIconDir->idEntries[i].bHeight,
                                                                          LR_LOADFROMFILE);
                                             if (hIcons[i] == NULL)
                                             {
