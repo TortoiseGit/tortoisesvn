17f612ad66bdd7d8cd1d7e76ef8614623cb6f11e
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2015-10-08 17:16:46 +0000

    When in overlap mode, zoom both images at the same time.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26885 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseIDiff/PicWindow.cpp
+++ src/TortoiseIDiff/PicWindow.cpp
@@ -885,7 +885,7 @@ void CPicWindow::OnMouseWheel(short fwKeys, short zDelta)
         InvalidateRect(*this, NULL, FALSE);
         SetWindowPos(*this, NULL, 0, 0, 0, 0, SWP_FRAMECHANGED|SWP_NOSIZE|SWP_NOREPOSITION|SWP_NOMOVE);
         UpdateWindow(*this);
-        if (bLinkedPositions && pTheOtherPic)
+        if ((bLinkedPositions || bOverlap) && pTheOtherPic)
         {
             pTheOtherPic->nHScrollPos = nHScrollPos;
             pTheOtherPic->nVScrollPos = nVScrollPos;
@@ -951,6 +951,10 @@ void CPicWindow::SetZoom(int Zoom, bool centermouse, bool inzoom)
 
     if (pTheOtherPic && !inzoom)
     {
+        if (bOverlap)
+        {
+            pTheOtherPic->SetZoom(Zoom, centermouse, true);
+        }
         if (bFitHeights)
         {
             m_linkedHeight = 0;
