7ba2a5167280d074be182808a0882edc812158da
xhmikosr <xhmikosr@154be670-7969-47af-9ab0-c6fb378d2295>
2015-11-13 07:17:48 +0000

    www: improve img-defer.js
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@26986 154be670-7969-47af-9ab0-c6fb378d2295

--- www/source/assets/js/img-defer.js
+++ www/source/assets/js/img-defer.js
@@ -1,36 +1,38 @@
-// Source: https://www.feedthebot.com/pagespeed/defer-images.html
+// Original source: https://varvy.com/pagespeed/defer-images.html
 
 /* jshint browser: true */
 
 (function() {
     'use strict';
 
-    function loadOnLoad() {
+    function initOnLoad() {
 
-        var iframeEl = document.createElement('iframe');
-        iframeEl.setAttribute('src', 'https://www.openhub.net/p/tortoisesvn/widgets/project_users?style=blue');
-        iframeEl.scrolling = 'no';
-        iframeEl.frameborder = '0';
-        iframeEl.style.border = 'none';
-        iframeEl.style.width = '95px';
-        iframeEl.style.height = '115px';
-        document.getElementById('iframe-badge').appendChild(iframeEl);
+        var iframeElement = document.createElement('iframe');
+        iframeElement.setAttribute('src', 'https://www.openhub.net/p/tortoisesvn/widgets/project_users?style=blue');
+        iframeElement.scrolling = 'no';
+        iframeElement.frameborder = '0';
+        iframeElement.style.border = 'none';
+        iframeElement.style.width = '95px';
+        iframeElement.style.height = '115px';
+        document.getElementById('iframe-badge').appendChild(iframeElement);
 
-        var imgDefer = document.getElementsByTagName('img');
-        for (var i = 0; i < imgDefer.length; i++) {
-            if (imgDefer[i].getAttribute('data-src')) {
-                imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
+        var imgElement = document.getElementsByTagName('img');
+
+        for (var i = 0; i < imgElement.length; i++) {
+            if (imgElement[i].hasAttribute('data-src')) {
+                imgElement[i].setAttribute('src', imgElement[i].getAttribute('data-src'));
+                imgElement[i].removeAttribute('data-src');
             }
         }
 
     }
 
     if (window.addEventListener) {
-        window.addEventListener('load', loadOnLoad, false);
+        window.addEventListener('load', initOnLoad, false);
     } else if (window.attachEvent) {
-        window.attachEvent('onload', loadOnLoad);
+        window.attachEvent('onload', initOnLoad);
     } else {
-        window.onload = loadOnLoad;
+        window.onload = initOnLoad;
     }
 
 }());
