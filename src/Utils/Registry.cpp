f875b4662d0f3a18b0b781c2fa8a9d21eae5f477
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2015-12-13 20:36:31 +0000

    fix broken build (see r27042)
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@27047 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/Registry.cpp
+++ src/Utils/Registry.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2006, 2008-2010, 2012, 2014 - TortoiseSVN
+// Copyright (C) 2003-2006, 2008-2010, 2012, 2014-2015 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -180,7 +180,7 @@ bool CRegistryKey::getValues(CStringList& values)
         }
     }
 
-    return !subkeys.IsEmpty();
+    return !values.IsEmpty();
 }
 
 bool CRegistryKey::getSubKeys(CStringList& subkeys)
