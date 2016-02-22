3780c030073dddfaf52ceb55fded23ae46b2e1e2
ivan-z <ivan-z@154be670-7969-47af-9ab0-c6fb378d2295>
2016-02-22 15:42:54 +0000

    LogCache: Add test for single-byte corruption in StringDictionary storage.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@27190 154be670-7969-47af-9ab0-c6fb378d2295

--- src/LogCache/Tests/StringDictionaryTests.cpp
+++ src/LogCache/Tests/StringDictionaryTests.cpp
@@ -24,6 +24,8 @@
 #include "RootOutStream.h"
 #include "RootInStream.h"
 
+#include <fstream>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace LogCacheTests
@@ -89,6 +91,61 @@ namespace LogCacheTests
                 Assert::AreEqual((LogCache::index_t) 4, dict.Find("dict4"));
 
                 Assert::AreEqual((LogCache::index_t) 5, dict.size());
+            }
+        }
+
+        TEST_METHOD(SingleByteCorruptionTest)
+        {
+            for (int i = 0; i < 125; i++)
+            {
+                CTestTempFile tmpFile;
+                {
+                    CRootOutStream strm(tmpFile.GetFileName());
+                    LogCache::CStringDictionary dict;
+
+                    Assert::AreEqual((LogCache::index_t) 1, dict.AutoInsert("dict1"));
+                    Assert::AreEqual((LogCache::index_t) 2, dict.AutoInsert("dict2"));
+                    Assert::AreEqual((LogCache::index_t) 3, dict.AutoInsert("dict3"));
+                    Assert::AreEqual((LogCache::index_t) 4, dict.AutoInsert("dict4"));
+
+                    Assert::AreEqual((LogCache::index_t) 5, dict.size());
+
+                    // Save data to stream.
+                    strm << dict;
+                }
+
+                // Corrupt single byte in file.
+                {
+                    std::fstream file;
+                    file.open(tmpFile.GetFileName(), std::fstream::in | std::fstream::out | std::fstream::binary);
+                    file.seekp(i, std::ios::beg);
+                    char buf[1];
+                    file.read(buf, 1);
+                    buf[0] ^= 0x23;
+                    file.seekp(i, std::ios::beg);
+                    file.write(buf, 1);
+                    file.close();
+                }
+
+                try
+                {
+                    CRootInStream strm(tmpFile.GetFileName());
+                    LogCache::CStringDictionary dict;
+
+                    // Load data from stream.
+                    strm >> dict;
+
+                    LogCache::index_t idx;
+                    idx = dict.Find("dict1");
+                    idx = dict.Find("dict2");
+                    idx = dict.Find("dict3");
+                    idx = dict.Find("dict4");
+                }
+                catch(const std::exception & e)
+                {
+                    // Exceptions are excepcted when reading data from corrupted storage.
+                    Logger::WriteMessage(e.what());
+                }
             }
         }
     };
