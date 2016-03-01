4f911f3fc7c32191041257ccdf95ba6ef850a71f
ivan-z <ivan-z@154be670-7969-47af-9ab0-c6fb378d2295>
2016-03-01 07:27:28 +0000

    Follow-up to r27214: Update copyright year.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@27219 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/StringUtils.cpp
+++ src/Utils/StringUtils.cpp
@@ -1,7 +1,7 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
 // Copyright (C) 2016 - TortoiseGit
-// Copyright (C) 2003-2011, 2013-2015 - TortoiseSVN
+// Copyright (C) 2003-2011, 2013-2016 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
