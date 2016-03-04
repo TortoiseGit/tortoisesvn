a9856ec609e5c8aabfb8b2a3b6c5c4d095974030
ivan-z <ivan-z@154be670-7969-47af-9ab0-c6fb378d2295>
2016-03-04 07:39:05 +0000

    Cleanup Windows XP compatibility code.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@27227 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TSVNCache/TSVNCache.cpp
+++ src/TSVNCache/TSVNCache.cpp
@@ -31,7 +31,6 @@
 #include <ioevent.h>
 #include "svn_dso.h"
 #include "SmartHandle.h"
-#include "DllVersion.h"
 
 #include <ShellAPI.h>
 
@@ -155,18 +154,7 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*
     {
         SecureZeroMemory(&niData,sizeof(NOTIFYICONDATA));
 
-        DWORD dwMajor = 0;
-        DWORD dwMinor = 0;
-        GetShellVersion(&dwMajor, &dwMinor);
-        DWORD dwVersion = PACKVERSION(dwMajor, dwMinor);
-
-        if (dwVersion >= PACKVERSION(6,0))
-            niData.cbSize = sizeof(NOTIFYICONDATA);
-        else if (dwVersion >= PACKVERSION(5,0))
-            niData.cbSize = NOTIFYICONDATA_V2_SIZE;
-        else
-            niData.cbSize = NOTIFYICONDATA_V1_SIZE;
-
+        niData.cbSize = sizeof(NOTIFYICONDATA);
         niData.uID = TRAY_ID;       // own tray icon ID
         niData.hWnd  = hWndHidden;
         niData.uFlags = NIF_ICON|NIF_MESSAGE;
