d66ff49c52e1b80212615ac04fef1a138d0960b4
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2016-03-07 18:48:25 +0000

    Before fetching the status, remove all items from the control. This solves an issue where the icons are still shown on a refresh while the rest of the control shows the busy message. Note: this only affects a refresh, when starting the control would be empty anyway.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@27232 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SVN/SVNStatusListCtrl.cpp
+++ src/SVN/SVNStatusListCtrl.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2015 - TortoiseSVN
+// Copyright (C) 2003-2016 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -517,6 +517,7 @@ BOOL CSVNStatusListCtrl::GetStatus ( const CTSVNPathList& pathList
                                    , bool bShowUserProps /* = false */)
 {
     CAutoWriteLock locker(m_guard);
+    DeleteAllItems();
     int refetchcounter = 0;
     BOOL bRet = TRUE;
     m_bUpdate = bUpdate;
