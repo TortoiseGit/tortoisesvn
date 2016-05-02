831da38a4e3ed7f1c1775f5694073cb88dda8f0d
ivan-z <ivan-z@154be670-7969-47af-9ab0-c6fb378d2295>
2016-05-02 12:55:04 +0000

    Implement simple LRU cache class.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@27300 154be670-7969-47af-9ab0-c6fb378d2295

---
+++ src/Utils/LruCache.cpp
@@ -0,0 +1,67 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2016 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "stdafx.h"
+#include "LruCache.h"
+
+#if defined(_DEBUG)
+static class LruCacheTest
+{
+public:
+    LruCacheTest()
+    {
+        {
+            LruCache<int, int> cache(2);
+            cache.insert(1, 100);
+            cache.insert(2, 200);
+            // Emulate access to key '1'
+            cache.try_get(1);
+
+            // Add third entry. Key '2' should be evicted/
+            cache.insert(3, 300);
+
+            ATLASSERT(cache.try_get(1) != nullptr);
+            ATLASSERT(*cache.try_get(1) == 100);
+
+            ATLASSERT(cache.try_get(2) == nullptr);
+
+            ATLASSERT(cache.try_get(3) != nullptr);
+            ATLASSERT(*cache.try_get(3) == 300);
+
+            // Test LruCache.clear()
+            cache.clear();
+            ATLASSERT(cache.try_get(1) == nullptr);
+            ATLASSERT(cache.try_get(2) == nullptr);
+            ATLASSERT(cache.try_get(3) == nullptr);
+
+            cache.insert(1, 100);
+            ATLASSERT(cache.try_get(1) != nullptr);
+            ATLASSERT(*cache.try_get(1) == 100);
+        }
+
+        {   // Test empty cache behavior
+            LruCache<int, int> cache(5);
+            ATLASSERT(cache.try_get(1) == nullptr);
+            ATLASSERT(cache.try_get(2) == nullptr);
+            ATLASSERT(cache.try_get(3) == nullptr);
+        }
+    }
+} LruCacheTest;
+
+#endif
