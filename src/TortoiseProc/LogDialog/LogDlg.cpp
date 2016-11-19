835bf5413ac7901d32f6d2d106b139f444a9c175
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2016-11-19 09:09:52 +0000

    Pass the second selected revision, not twice the first.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@27565 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/LogDialog/LogDlg.cpp
+++ src/TortoiseProc/LogDialog/LogDlg.cpp
@@ -5867,7 +5867,7 @@ void CLogDlg::ExecuteBlameTwoMenuRevisions(ContextMenuInfoForRevisionsPtr& pCmi)
     else
     {
         CAppUtils::StartShowCompare(m_hWnd, CTSVNPath(pCmi->PathURL),
-                                    pCmi->RevSelected, CTSVNPath(pCmi->PathURL), pCmi->RevSelected,
+                                    pCmi->RevSelected2, CTSVNPath(pCmi->PathURL), pCmi->RevSelected,
                                     SVNRev(), m_LogRevision, false, L"", false, false, true, nodekind);
     }
 }
