724e8d3292378c60609892913c5c338c1c7d37c0
xhmikosr <xhmikosr@154be670-7969-47af-9ab0-c6fb378d2295>
2017-01-13 09:16:33 +0000

    www/source/assets/js/sf-accel.js: minor simplification.
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@27653 154be670-7969-47af-9ab0-c6fb378d2295

--- www/source/assets/js/sf-accel.js
+++ www/source/assets/js/sf-accel.js
@@ -10,22 +10,21 @@
         return;
     }
 
-    (function() {
-        var script = document.createElement('script');
-        script.src = '//sourceforge.net/accelerator/js?partner_id=61';
-        script.defer = true;
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(script, s);
+    var script = document.createElement('script');
+    script.src = '//sourceforge.net/accelerator/js?partner_id=61';
+    script.defer = true;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(script, s);
 
-        // Add custom class to our download links
-        for (var i = 0; i < linksNum; i++) {
-            var className = 'sourceforge_accelerator_link';
+    // Add custom class to our download links
+    for (var i = 0; i < linksNum; i++) {
+        var className = 'sourceforge_accelerator_link';
 
-            if (link[i].classList) {
-                link[i].classList.add(className);
-            } else {
-                link[i].className += ' ' + className;   // This can be removed for IE > 10
-            }
+        if (link[i].classList) {
+            link[i].classList.add(className);
+        } else {
+            link[i].className += ' ' + className;   // This can be removed for IE > 10
         }
-    })();
+    }
+
 })();
