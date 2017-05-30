269fa41ea32e7afd1b5d28085b3798c896a92227
steveking <steveking@154be670-7969-47af-9ab0-c6fb378d2295>
2017-05-30 19:34:22 +0000

    * rearrange includes to get the deprecated warnings * replace deprecated APIs with new ones
    git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@27848 154be670-7969-47af-9ab0-c6fb378d2295

--- src/SubWCRev/SubWCRevCOM.cpp
+++ src/SubWCRev/SubWCRevCOM.cpp
@@ -16,10 +16,22 @@
 // along with this program; if not, write to the Free Software Foundation,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "stdafx.h"
+
+#pragma warning(push)
+#include "apr_pools.h"
+#include "svn_error.h"
+#include "svn_client.h"
+#include "svn_dirent_uri.h"
+#include "svn_dso.h"
+#pragma warning(pop)
 #include <objbase.h>
 #include "SubWCRevCOM_h.h"
 #include "SubWCRevCOM_i.c"
 #include "SubWCRevCOM.h"
+
+#include "Register.h"
+#include "UnicodeUtils.h"
+#include <atlbase.h>
 
 #include <tchar.h>
 #include <windows.h>
@@ -27,17 +39,6 @@
 #include <Shellapi.h>
 #include <comutil.h>
 #include <memory>
-
-#pragma warning(push)
-#include "apr_pools.h"
-#include "svn_error.h"
-#include "svn_client.h"
-#include "svn_path.h"
-#include "svn_dso.h"
-#pragma warning(pop)
-#include "Register.h"
-#include "UnicodeUtils.h"
-#include <atlbase.h>
 
 STDAPI DllRegisterServer();
 STDAPI DllUnregisterServer();
@@ -215,7 +216,7 @@ HRESULT SubWCRev::GetWCInfoInternal(/*[in]*/ BSTR wcPath, /*[in]*/VARIANT_BOOL f
     }
 
     char *wc_utf8 = Utf16ToUtf8(wcPath, pool);
-    const char * internalpath = svn_path_internal_style (wc_utf8, pool);
+    const char * internalpath = svn_dirent_internal_style(wc_utf8, pool);
 
     apr_hash_t * config = nullptr;;
     svn_config_get_config(&(config), nullptr, pool);
