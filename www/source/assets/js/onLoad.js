1e9d3f8543e250beb4421211c658e3d6ec28f8d6
xhmikosr <xhmikosr@154be670-7969-47af-9ab0-c6fb378d2295>
2019-06-23 14:06:41 +0000

    www: move the OpenHub badge to the HTML.
    git-svn-id: http://svn.osdn.net/svnroot/tortoisesvn/trunk@28611 154be670-7969-47af-9ab0-c6fb378d2295

--- www/source/assets/js/onLoad.js
+++ www/source/assets/js/onLoad.js
@@ -4,16 +4,6 @@
     'use strict';
 
     function initOnLoad() {
-
-        var iframeElement = document.createElement('iframe');
-        iframeElement.setAttribute('src', 'https://www.openhub.net/p/tortoisesvn/widgets/project_users?style=blue');
-        iframeElement.scrolling = 'no';
-        iframeElement.frameborder = '0';
-        iframeElement.style.border = 'none';
-        iframeElement.style.width = '95px';
-        iframeElement.style.height = '115px';
-        document.getElementById('iframe-badge').appendChild(iframeElement);
-
         // Original source: https://varvy.com/pagespeed/defer-images.html
         var imgElement = document.getElementsByTagName('img');
 
@@ -24,7 +14,6 @@
                 imgElement[i].className += ' loaded';
             }
         }
-
     }
 
     if (window.addEventListener) {
