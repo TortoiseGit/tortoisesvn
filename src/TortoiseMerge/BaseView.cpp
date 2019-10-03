9fec865d17d5ba24bcf0f6fcb5118cbdda715c32
stefankueng <stefankueng@154be670-7969-47af-9ab0-c6fb378d2295>
2019-10-03 16:45:20 +0000

    Use Ctrl+M to toggle the "Mark/Unmark this change".
    git-svn-id: http://svn.osdn.net/svnroot/tortoisesvn/trunk@28667 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseMerge/BaseView.cpp
+++ src/TortoiseMerge/BaseView.cpp
@@ -3200,6 +3200,17 @@ void CBaseView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
         m_bInsertMode = !m_bInsertMode;
         UpdateCaret();
         break;
+    case 'M':
+        if (bControl && m_pwndRight)
+        {
+            int nFirstViewLine = 0;
+            int nLastViewLine = 0;
+            if (GetViewSelection(nFirstViewLine, nLastViewLine))
+            {
+                m_pwndRight->MarkBlock(!m_pwndRight->GetViewMarked(nFirstViewLine));
+            }
+        }
+        break;
 
     }
     CView::OnKeyDown(nChar, nRepCnt, nFlags);
@@ -3827,7 +3838,11 @@ void CBaseView::OnChar(UINT nChar, UINT nRepCnt, UINT nFlags)
     CView::OnChar(nChar, nRepCnt, nFlags);
 
     bool bShift = !!(GetKeyState(VK_SHIFT)&0x8000);
+    bool bControl = !!(GetKeyState(VK_CONTROL)&0x8000);
     bool bSkipSelectionClear = false;
+
+    if (bControl)
+        return;
 
     if (IsReadonly())
         return;
