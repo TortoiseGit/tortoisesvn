62833737aed2f1d9e1b531536003881c56866f1b
stefankueng <stefankueng@154be670-7969-47af-9ab0-c6fb378d2295>
2020-03-21 07:45:42 +0000

    fix compiler warning
    git-svn-id: http://svn.osdn.net/svnroot/tortoisesvn/trunk@28814 154be670-7969-47af-9ab0-c6fb378d2295

--- src/LogCache/Access/ILogIterator.h
+++ src/LogCache/Access/ILogIterator.h
@@ -1,6 +1,6 @@
-// TortoiseSVN - a Windows shell extension for easy version control
+﻿// TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2007, 2011 - TortoiseSVN
+// Copyright (C) 2007, 2011, 2020 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -44,6 +44,7 @@ class CDictionaryBasedTempPath;
 class ILogIterator
 {
 public:
+    virtual ~ILogIterator() {}
 
     /// data access
 
