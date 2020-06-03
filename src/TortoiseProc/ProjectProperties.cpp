6c1f5226c5011279ccaa9fe403049a5ea4031656
stefankueng <stefankueng@154be670-7969-47af-9ab0-c6fb378d2295>
2020-06-03 17:18:10 +0000

    Set the return value on init.
    git-svn-id: http://svn.osdn.net/svnroot/tortoisesvn/trunk@28895 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/ProjectProperties.cpp
+++ src/TortoiseProc/ProjectProperties.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2018 - TortoiseSVN
+// Copyright (C) 2003-2018, 2020 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -539,7 +539,7 @@ bool ProjectProperties::MightContainABugID()
 
 CString ProjectProperties::GetBugIDUrl(const CString& sBugID)
 {
-    CString ret;
+    CString ret = sUrl;
     if (sUrl.IsEmpty())
         return ret;
     if (!sMessage.IsEmpty() || !sCheckRe.IsEmpty())
