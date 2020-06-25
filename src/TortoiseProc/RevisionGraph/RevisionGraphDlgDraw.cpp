c36c8c6057409ee45eb1747ff8e450bfd2400fc0
stefankueng <stefankueng@154be670-7969-47af-9ab0-c6fb378d2295>
2020-06-25 17:56:09 +0000

    partly revert r28940: the commit contained dev changes that were not part of the expat update.
    git-svn-id: http://svn.osdn.net/svnroot/tortoisesvn/trunk@28941 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/RevisionGraph/RevisionGraphDlgDraw.cpp
+++ src/TortoiseProc/RevisionGraph/RevisionGraphDlgDraw.cpp
@@ -60,7 +60,7 @@ CFont* CRevisionGraphWnd::GetFont(BOOL bItalic /*= FALSE*/, BOOL bBold /*= FALSE
         m_lfBaseFont.lfHeight = -CDPIAware::Instance().PointsToPixels(GetSafeHwnd(), m_nFontSize);
         // use the empty font name, so GDI takes the first font which matches
         // the specs. Maybe this will help render chinese/japanese chars correctly.
-        wcsncpy_s(m_lfBaseFont.lfFaceName, L"Segoe UI", _countof(m_lfBaseFont.lfFaceName) - 1);
+        wcsncpy_s(m_lfBaseFont.lfFaceName, L"MS Shell Dlg 2", _countof(m_lfBaseFont.lfFaceName) - 1);
         if (!m_apFonts[nIndex]->CreateFontIndirect(&m_lfBaseFont))
         {
             delete m_apFonts[nIndex];
