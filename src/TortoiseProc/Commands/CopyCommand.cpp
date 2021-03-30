b1104ca385645eeea5e583ced3dcb00209d17d11
stefankueng <stefankueng@154be670-7969-47af-9ab0-c6fb378d2295>
2021-03-30 16:25:14 +0000

    use the advanced setting includeExternals when switching after a branch/tag
    git-svn-id: http://svn.osdn.net/svnroot/tortoisesvn/trunk@29097 154be670-7969-47af-9ab0-c6fb378d2295

--- src/TortoiseProc/Commands/CopyCommand.cpp
+++ src/TortoiseProc/Commands/CopyCommand.cpp
@@ -1,6 +1,6 @@
-// TortoiseSVN - a Windows shell extension for easy version control
+﻿// TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2007-2012, 2014 - TortoiseSVN
+// Copyright (C) 2007-2012, 2014, 2021 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -25,7 +25,7 @@
 
 bool CopyCommand::Execute()
 {
-    bool bRet = false;
+    bool    bRet = false;
     CString msg;
     if (parser.HasKey(L"logmsg"))
     {
@@ -37,51 +37,57 @@ bool CopyCommand::Execute()
         CStringUtils::ReadStringFromTextFile(logmsgfile, msg);
     }
 
-    BOOL repeat = FALSE;
+    BOOL     repeat = FALSE;
     CCopyDlg dlg;
 
-    dlg.m_path = cmdLinePath;
-    CString url = parser.GetVal(L"url");
-    CString logmessage = msg;
-    SVNRev copyRev = SVNRev::REV_HEAD;
-    BOOL doSwitch = parser.HasKey(L"switchaftercopy");
-    BOOL makeParents = parser.HasKey(L"makeparents");
+    dlg.m_path          = cmdLinePath;
+    CString url         = parser.GetVal(L"url");
+    CString logMessage  = msg;
+    SVNRev  copyRev     = SVNRev::REV_HEAD;
+    BOOL    doSwitch    = parser.HasKey(L"switchaftercopy");
+    BOOL    makeParents = parser.HasKey(L"makeparents");
     do
     {
-        repeat = FALSE;
-        dlg.m_URL = url;
-        dlg.m_sLogMessage = logmessage;
-        dlg.m_CopyRev = copyRev;
-        dlg.m_bDoSwitch = doSwitch;
+        repeat             = FALSE;
+        dlg.m_URL          = url;
+        dlg.m_sLogMessage  = logMessage;
+        dlg.m_CopyRev      = copyRev;
+        dlg.m_bDoSwitch    = doSwitch;
         dlg.m_bMakeParents = makeParents;
         if (dlg.DoModal() == IDOK)
         {
-            theApp.m_pMainWnd = NULL;
+            CRegDWORD updateExternals(L"Software\\TortoiseSVN\\IncludeExternals", true);
+            bool      ignoreExternals = false;
+            if (dlg.m_bDoSwitch)
+                ignoreExternals = static_cast<DWORD>(updateExternals) == 0;
+
+            theApp.m_pMainWnd = nullptr;
             CSVNProgressDlg progDlg;
             progDlg.SetCommand(CSVNProgressDlg::SVNProgress_Copy);
-            progDlg.SetAutoClose (parser);
+            progDlg.SetAutoClose(parser);
             DWORD options = dlg.m_bDoSwitch ? ProgOptSwitchAfterCopy : ProgOptNone;
             options |= dlg.m_bMakeParents ? ProgOptMakeParents : ProgOptNone;
+            options |= ignoreExternals ? ProgOptIgnoreExternals : ProgOptNone;
             progDlg.SetOptions(options);
             progDlg.SetPathList(pathList);
             progDlg.SetUrl(dlg.m_URL);
             progDlg.SetCommitMessage(dlg.m_sLogMessage);
             progDlg.SetRevision(dlg.m_CopyRev);
             progDlg.SetExternals(dlg.GetExternalsToTag());
-            url = dlg.m_URL;
-            logmessage = dlg.m_sLogMessage;
-            copyRev = dlg.m_CopyRev;
-            doSwitch = dlg.m_bDoSwitch;
+            url         = dlg.m_URL;
+            logMessage  = dlg.m_sLogMessage;
+            copyRev     = dlg.m_CopyRev;
+            doSwitch    = dlg.m_bDoSwitch;
             makeParents = dlg.m_bMakeParents;
             progDlg.DoModal();
             CRegDWORD err(L"Software\\TortoiseSVN\\ErrorOccurred", FALSE);
-            err = (DWORD)progDlg.DidErrorsOccur();
-            bRet = !progDlg.DidErrorsOccur();
-            repeat = progDlg.DidErrorsOccur();
+            err                   = static_cast<DWORD>(progDlg.DidErrorsOccur());
+            bRet                  = !progDlg.DidErrorsOccur();
+            repeat                = progDlg.DidErrorsOccur();
             CRegDWORD bFailRepeat = CRegDWORD(L"Software\\TortoiseSVN\\CommitReopen", FALSE);
-            if (DWORD(bFailRepeat) == FALSE)
-                repeat = false;     // do not repeat if the user chose not to in the settings.
+            if (static_cast<DWORD>(bFailRepeat) == FALSE)
+                repeat = false; // do not repeat if the user chose not to in the settings.
         }
-    } while(repeat);
+    } while (repeat);
     return bRet;
 }
