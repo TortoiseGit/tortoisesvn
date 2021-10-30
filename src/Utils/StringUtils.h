58af0ed7ca7ab7e7315a1cd31f5346310f180230
stefankueng <stefankueng@154be670-7969-47af-9ab0-c6fb378d2295>
2021-10-30 07:38:56 +0000

    extend the stringtok function to handle numerical containers
    git-svn-id: http://svn.osdn.net/svnroot/tortoisesvn/trunk@29279 154be670-7969-47af-9ab0-c6fb378d2295

--- src/Utils/StringUtils.h
+++ src/Utils/StringUtils.h
@@ -58,10 +58,10 @@ int wcswildcmp(const wchar_t* wild, const wchar_t* string);
 
 template <typename Container>
 void stringtok(Container& container, const std::wstring& in, bool trim,
-               const wchar_t* const delimiters = L"|", bool append = false)
+               const wchar_t* const delimiters = L"|", bool append = true)
 {
-    const std::string::size_type len = in.length();
-    std::string::size_type       i   = 0;
+    const std::wstring::size_type len = in.length();
+    std::wstring::size_type       i   = 0;
     if (!append)
         container.clear();
 
@@ -71,21 +71,37 @@ void stringtok(Container& container, const std::wstring& in, bool trim,
         {
             // eat leading whitespace
             i = in.find_first_not_of(delimiters, i);
-            if (i == std::string::npos)
+            if (i == std::wstring::npos)
                 return; // nothing left but white space
         }
 
         // find the end of the token
-        std::string::size_type j = in.find_first_of(delimiters, i);
+        std::wstring::size_type j = in.find_first_of(delimiters, i);
 
         // push token
-        if (j == std::string::npos)
+        if (j == std::wstring::npos)
         {
-            container.push_back(in.substr(i));
+            if constexpr (std::is_same_v<typename Container::value_type, std::wstring>)
+            {
+                container.push_back(in.substr(i));
+            }
+            else
+            {
+                container.push_back(static_cast<typename Container::value_type>(_wtoi64(in.substr(i).c_str())));
+            }
             return;
         }
         else
-            container.push_back(in.substr(i, j - i));
+        {
+            if constexpr (std::is_same_v<typename Container::value_type, std::wstring>)
+            {
+                container.push_back(in.substr(i, j - i));
+            }
+            else
+            {
+                container.push_back(static_cast<typename Container::value_type>(_wtoi64(in.substr(i, j - i).c_str())));
+            }
+        }
 
         // set up for next loop
         i = j + 1;
@@ -93,8 +109,61 @@ void stringtok(Container& container, const std::wstring& in, bool trim,
 }
 
 template <typename Container>
+void stringtokset(Container& container, const std::wstring& in, bool trim,
+                  const wchar_t* const delimiters = L"|", bool append = false)
+{
+    const std::wstring::size_type len = in.length();
+    std::wstring::size_type       i   = 0;
+    if (!append)
+        container.clear();
+
+    while (i < len)
+    {
+        if (trim)
+        {
+            // eat leading whitespace
+            i = in.find_first_not_of(delimiters, i);
+            if (i == std::wstring::npos)
+                return; // nothing left but white space
+        }
+
+        // find the end of the token
+        std::wstring::size_type j = in.find_first_of(delimiters, i);
+
+        // push token
+        if (j == std::wstring::npos)
+        {
+            if constexpr (std::is_same_v<typename Container::value_type, std::wstring>)
+            {
+                container.insert(in.substr(i));
+            }
+            else
+            {
+                container.insert(static_cast<typename Container::value_type>(_wtoi64(in.substr(i).c_str())));
+            }
+            return;
+        }
+        else
+        {
+            if constexpr (std::is_same_v<typename Container::value_type, std::wstring>)
+            {
+                container.insert(in.substr(i, j - i));
+            }
+            else
+            {
+                container.insert(static_cast<typename Container::value_type>(_wtoi64(in.substr(i, j - i).c_str())));
+            }
+        }
+
+        // set up for next loop
+        i = j + 1;
+    }
+}
+
+// append = true as the default: a default value should never lose data!
+template <typename Container>
 void stringtok(Container& container, const std::string& in, bool trim,
-               const char* const delimiters = "|", bool append = false)
+               const char* const delimiters = "|", bool append = true)
 {
     const std::string::size_type len = in.length();
     std::string::size_type       i   = 0;
@@ -117,11 +186,79 @@ void stringtok(Container& container, const std::string& in, bool trim,
         // push token
         if (j == std::string::npos)
         {
-            container.push_back(in.substr(i));
+            if constexpr (std::is_same_v<typename Container::value_type, std::string>)
+            {
+                container.push_back(in.substr(i));
+            }
+            else
+            {
+                container.push_back(static_cast<typename Container::value_type>(_atoi64(in.substr(i).c_str())));
+            }
             return;
         }
         else
-            container.push_back(in.substr(i, j - i));
+        {
+            if constexpr (std::is_same_v<typename Container::value_type, std::string>)
+            {
+                container.push_back(in.substr(i, j - i));
+            }
+            else
+            {
+                container.push_back(static_cast<typename Container::value_type>(_atoi64(in.substr(i, j - i).c_str())));
+            }
+        }
+
+        // set up for next loop
+        i = j + 1;
+    }
+}
+
+template <typename Container>
+void stringtokset(Container& container, const std::string& in, bool trim,
+                  const char* const delimiters = "|", bool append = false)
+{
+    const std::string::size_type len = in.length();
+    std::string::size_type       i   = 0;
+    if (!append)
+        container.clear();
+
+    while (i < len)
+    {
+        if (trim)
+        {
+            // eat leading whitespace
+            i = in.find_first_not_of(delimiters, i);
+            if (i == std::string::npos)
+                return; // nothing left but white space
+        }
+
+        // find the end of the token
+        std::string::size_type j = in.find_first_of(delimiters, i);
+
+        // push token
+        if (j == std::string::npos)
+        {
+            if constexpr (std::is_same_v<typename Container::value_type, std::string>)
+            {
+                container.insert(in.substr(i));
+            }
+            else
+            {
+                container.insert(static_cast<typename Container::value_type>(_atoi64(in.substr(i).c_str())));
+            }
+            return;
+        }
+        else
+        {
+            if constexpr (std::is_same_v<typename Container::value_type, std::string>)
+            {
+                container.insert(in.substr(i, j - i));
+            }
+            else
+            {
+                container.insert(static_cast<typename Container::value_type>(_atoi64(in.substr(i, j - i).c_str())));
+            }
+        }
 
         // set up for next loop
         i = j + 1;
